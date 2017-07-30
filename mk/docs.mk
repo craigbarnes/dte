cfcba6750a12390392af9062d5d0ebb2149ba811
Craig Barnes <Cr@igBarn.es>
2017-07-30 19:25:44 +0100

    Add "make check-docs" target for checking for dead links in README.md

--- mk/docs.mk
+++ mk/docs.mk
@@ -1,9 +1,18 @@
+FINDLINKS = sed -n 's|^.*\(https\?://[A-Za-z0-9_/.-]*\).*|\1|gp'
+CHECKURL = curl -sSI -w '%{http_code}  @1  %{redirect_url}\n' -o /dev/null @1
+NPROC = $(shell sh mk/nproc.sh)
+TTMAN = Documentation/ttman$(X)
+
+XARGS_P_FLAG = $(shell \
+    printf "1\n2" | xargs -P2 -I@ echo '@' >/dev/null 2>&1 && \
+    echo '-P$(NPROC)' \
+)
+
 man1 := Documentation/$(PROGRAM).1
 man7 := Documentation/$(PROGRAM)-syntax.7
 man  := $(man1) $(man7)
 html := $(addprefix public/, $(addsuffix .html, $(notdir $(man))))
 img  := public/screenshot.png
-TTMAN = Documentation/ttman$(X)
 
 quiet_cmd_ttman = TTMAN  $@
 cmd_ttman = mk/manvars.sh '$<' '$(PROGRAM)' | $(TTMAN) > $@
@@ -40,6 +49,9 @@ Documentation/%.o: Documentation/%.c
 public/:
 	@mkdir -p $@
 
+check-docs:
+	@$(FINDLINKS) README.md | xargs -I@1 $(XARGS_P_FLAG) $(CHECKURL)
+
 
 CLEANFILES += $(man) $(html) $(img) $(TTMAN) $(TTMAN).o
-.PHONY: docs man html img
+.PHONY: docs man html img check-docs
