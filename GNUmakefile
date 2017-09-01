967f7fe3618e14fe2d734dc6f14a2e77f9724f82
Craig Barnes <Cr@igBarn.es>
2017-09-01 03:19:43 +0100

    Move all build related macros/rules in GNUmakefile to mk/build.mk

--- GNUmakefile
+++ GNUmakefile
@@ -1,52 +1,16 @@
-CC ?= gcc
-LD = $(CC)
-CFLAGS ?= -g -O2
-LDFLAGS ?=
-HOST_CC ?= $(CC)
-HOST_LD ?= $(HOST_CC)
-HOST_CFLAGS ?= $(CFLAGS)
-HOST_LDFLAGS ?=
-INSTALL = install
-SED = sed
-RM = rm -f
+include mk/compat.mk
+-include Config.mk
+include mk/build.mk
+include mk/docs.mk
+-include mk/dev.mk
+
 prefix ?= /usr/local
 bindir ?= $(prefix)/bin
 datadir ?= $(prefix)/share
 mandir ?= $(datadir)/man
 
-# Enabled if supported by CC
-WARNINGS = \
-    -Wall -Wformat-security -Wmissing-prototypes -Wold-style-definition \
-    -Wwrite-strings -Wundef -Wshadow \
-    -Wextra -Wno-unused-parameter -Wno-sign-compare
-
-# More verbose build
-# V = 1
-
-# Disable automatic dependency calculation
-# NO_DEPS = 1
-
-# 0: Disable debugging.
-# 1: Enable BUG_ON() and light-weight sanity checks.
-# 2: Enable logging to $(DTE_HOME)/debug.log.
-# 3: Enable expensive sanity checks.
-DEBUG = 1
-
-# End of configuration =================================================
-
-editor_objects := $(addprefix src/, $(addsuffix .o, \
-    alias bind block buffer-iter buffer cconv change cmdline color \
-    command-mode commands common compiler completion config ctags ctype \
-    cursed decoder detect edit editor encoder encoding env error \
-    file-history file-location file-option filetype fork format-status \
-    frame git-open history hl indent input-special iter key \
-    load-save lock main move msg normal-mode obuf options \
-    parse-args parse-command path ptr-array regexp run screen \
-    screen-tabbar screen-view search-mode search selection spawn state \
-    strbuf syntax tabbar tag term-caps term uchar unicode view \
-    wbuf window xmalloc ))
-
-test_objects := src/test-main.o
+INSTALL = install
+RM = rm -f
 
 syntax_files := \
     awk c config css diff docker dte gitcommit gitrebase go html html+smarty \
@@ -58,27 +22,8 @@ color := $(addprefix share/color/, darkgray light light256)
 compiler:= $(addprefix share/compiler/, gcc go)
 config := $(addprefix share/, filetype option rc)
 syntax := $(addprefix share/syntax/, $(syntax_files))
-dte := dte$(X)
-
-OBJECTS := $(editor_objects) $(test_objects)
-
-PKGDATADIR = $(datadir)/dte
-VERSION = 1.3
-
-include mk/compat.mk
--include Config.mk
-include mk/build.mk
-include mk/docs.mk
--include mk/dev.mk
 
 all: $(dte) test man
-
-$(dte): $(editor_objects)
-test: $(filter-out src/main.o, $(editor_objects)) $(test_objects)
-
-$(dte) test:
-	$(E) LINK $@
-	$(Q) $(LD) $(LDFLAGS) $(BASIC_LDFLAGS) -o $@ $^ $(LIBS)
 
 install: all
 	$(INSTALL) -d -m755 $(DESTDIR)$(bindir)
@@ -101,8 +46,8 @@ tags:
 	ctags src/*.[ch]
 
 clean:
-	$(RM) .CFLAGS .VARS src/*.o src/bindings.inc $(dte) test $(CLEANFILES)
-	$(RM) -r $(dep_dirs)
+	$(RM) $(CLEANFILES)
+	$(RM) -r $(CLEANDIRS)
 
 distclean: clean
 	$(RM) tags
--- mk/build.mk
+++ mk/build.mk
@@ -1,33 +1,75 @@
-S_FLAG := $(findstring s,$(firstword -$(MAKEFLAGS)))$(filter -s,$(MAKEFLAGS))
-uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
-uname_O := $(shell sh -c 'uname -o 2>/dev/null || echo not')
-uname_R := $(shell sh -c 'uname -r 2>/dev/null || echo not')
+CC ?= gcc
+LD = $(CC)
+CFLAGS ?= -g -O2
+LDFLAGS ?=
+HOST_CC ?= $(CC)
+HOST_LD ?= $(HOST_CC)
+HOST_CFLAGS ?= $(CFLAGS)
+HOST_LDFLAGS ?=
 LIBS = -lcurses
-X =
+SED = sed
+
+# Enabled if supported by CC
+WARNINGS = \
+    -Wall -Wformat-security -Wmissing-prototypes -Wold-style-definition \
+    -Wwrite-strings -Wundef -Wshadow \
+    -Wextra -Wno-unused-parameter -Wno-sign-compare
+
+VERSION = 1.3
+PKGDATADIR = $(datadir)/dte
+EXE =
+dte = dte$(EXE)
 
 try-run = $(shell if ($(1)) >/dev/null 2>&1; then echo "$(2)"; else echo "$(3)"; fi)
 cc-option = $(call try-run, $(CC) $(1) -c -x c /dev/null -o /dev/null,$(1),$(2))
+uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
+uname_O := $(shell sh -c 'uname -o 2>/dev/null || echo not')
+uname_R := $(shell sh -c 'uname -r 2>/dev/null || echo not')
 
-# Avoid random internationalization problems
-export LC_ALL := C
-
-ifdef S_FLAG
+ifneq "$(findstring s,$(firstword -$(MAKEFLAGS)))$(filter -s,$(MAKEFLAGS))" ""
+  # Make "-s" flag was used (silent build)
   Q = @
   E = @:
 else ifeq "$(V)" "1"
+  # "V=1" variable was set (verbose build)
   Q =
   E = @:
 else
+  # Normal build
   Q = @
   E = @printf ' %7s  %s\n'
 endif
+
+# Disable automatic dependency calculation
+# NO_DEPS = 1
+
+# 0: Disable debugging.
+# 1: Enable BUG_ON() and light-weight sanity checks.
+# 2: Enable logging to $(DTE_HOME)/debug.log.
+# 3: Enable expensive sanity checks.
+DEBUG = 1
+
+editor_objects := $(addprefix src/, $(addsuffix .o, \
+    alias bind block buffer-iter buffer cconv change cmdline color \
+    command-mode commands common compiler completion config ctags ctype \
+    cursed decoder detect edit editor encoder encoding env error \
+    file-history file-location file-option filetype fork format-status \
+    frame git-open history hl indent input-special iter key \
+    load-save lock main move msg normal-mode obuf options \
+    parse-args parse-command path ptr-array regexp run screen \
+    screen-tabbar screen-view search-mode search selection spawn state \
+    strbuf syntax tabbar tag term-caps term uchar unicode view \
+    wbuf window xmalloc ))
+
+test_objects := src/test-main.o
+OBJECTS := $(editor_objects) $(test_objects)
 
 ifeq "$(uname_S)" "Darwin"
   LIBS += -liconv
 endif
 ifeq "$(uname_O)" "Cygwin"
   LIBS += -liconv
-  X = .exe
+  EXE = .exe
 endif
 ifeq "$(uname_S)" "FreeBSD"
   # libc of FreeBSD 10.0 includes iconv
@@ -90,10 +132,15 @@ $(dep_dirs):
 	@mkdir -p $@
 endif
 
-src/editor.o: BASIC_CFLAGS += -DVERSION=\"$(VERSION)\" -DPKGDATADIR=\"$(PKGDATADIR)\"
-
+$(dte): $(editor_objects)
+test: $(filter-out src/main.o, $(editor_objects)) $(test_objects)
 src/main.o: src/bindings.inc
 src/editor.o: .VARS
+src/editor.o: BASIC_CFLAGS += -DVERSION=\"$(VERSION)\" -DPKGDATADIR=\"$(PKGDATADIR)\"
+
+$(dte) test:
+	$(E) LINK $@
+	$(Q) $(LD) $(LDFLAGS) $(BASIC_LDFLAGS) -o $@ $^ $(LIBS)
 
 $(OBJECTS): src/%.o: src/%.c .CFLAGS | $(missing_dep_dirs)
 	$(E) CC $@
@@ -111,5 +158,7 @@ src/bindings.inc: share/binding/builtin mk/rc2c.sed
 	$(Q) $(SED) -f mk/rc2c.sed $< >> $@
 
 
+CLEANFILES += $(dte) test $(OBJECTS) src/bindings.inc .VARS .CFLAGS
+CLEANDIRS += $(dep_dirs)
 .PHONY: FORCE
-.SECONDARY: $(missing_dep_dirs)
+.SECONDARY: $(dep_dirs)
