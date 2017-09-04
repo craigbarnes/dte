ccc1b849cb5006a211e08bf81378ff62c0dc1e3d
Craig Barnes <Cr@igBarn.es>
2017-09-04 03:22:20 +0100

    Add script for installing OS package dependencies
    ...and also update .gitlab-ci.yml to make use of it.

---
+++ mk/install-deps.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+linux_distro() {
+    case $(. /etc/os-release && echo "$ID") in
+    alpine)
+        apk --update add make gcc binutils ncurses ncurses-dev libc-dev;;
+    arch)
+        pacman -S --needed --noconfirm make gcc ncurses;;
+    centos|fedora)
+        yum -y install make gcc binutils ncurses-devel;;
+    debian|ubuntu)
+        apt-get -qy install make gcc binutils libncurses5-dev;;
+    *)
+        echo 'Unrecognized Linux distro; install GNU Make, GCC and ncurses manually';;
+    esac
+}
+
+case $(uname) in
+Linux)
+    linux_distro;;
+OpenBSD)
+    pkg_add -IU gmake gcc;;
+FreeBSD)
+    pkg install gmake gcc;; # TODO: test this
+Darwin)
+    ;;
+*)
+    echo 'Unrecognized OS; install GNU Make, GCC and ncurses manually';;
+esac
