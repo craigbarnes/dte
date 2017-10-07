1d07b5f601a9184aa648204ebbaaa37f9a5868f9
Craig Barnes <Cr@igBarn.es>
2017-10-07 23:55:44 +0100

    Print origin of user-overridable variables in "make vars" output

--- mk/util.mk
+++ mk/util.mk
@@ -2,14 +2,14 @@ KERNEL := $(shell sh -c 'uname -s 2>/dev/null || echo not')
 OS := $(shell sh -c 'uname -o 2>/dev/null || echo not')
 DISTRO = $(shell . /etc/os-release && echo "$$NAME")
 STREQ = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
-PRINTVAR = printf '\033[1m%-11s\033[0m= %s\n' '$(1)' '$(strip $($(1)))'
-
-USERVARS = \
-    VERSION CC CFLAGS LDFLAGS LDLIBS DEBUG PKGDATADIR KERNEL \
-    $(if $(call STREQ, $(KERNEL), Linux), DISTRO)
+PRINTVAR = printf '\033[1m%-11s\033[0m= %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
+PRINTVARX = $(call PRINTVAR,$(1), \033[32m(%s)\033[0m, '$(origin $(1))')
+USERVARS = CC CFLAGS LDFLAGS LDLIBS DEBUG PKGDATADIR
+AUTOVARS = VERSION KERNEL $(if $(call STREQ, $(KERNEL), Linux), DISTRO)
 
 vars:
-	@$(foreach VAR, $(USERVARS), $(call PRINTVAR,$(VAR));)
+	@$(foreach VAR, $(AUTOVARS), $(call PRINTVAR,$(VAR));)
+	@$(foreach VAR, $(USERVARS), $(call PRINTVARX,$(VAR));)
 
 
 .PHONY: vars
