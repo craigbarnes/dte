55399b4d2068848c74c9db4456b6fafc6e449d28
Craig Barnes <Cr@igBarn.es>
2017-10-11 15:41:07 +0100

    Show value of "TERM" variable in "make vars" output

--- mk/util.mk
+++ mk/util.mk
@@ -1,13 +1,13 @@
 KERNEL := $(shell sh -c 'uname -s 2>/dev/null || echo not')
 OS := $(shell sh -c 'uname -o 2>/dev/null || echo not')
-DISTRO = $(shell . /etc/os-release && echo "$$NAME")
+DISTRO = $(shell . /etc/os-release && echo "$$PRETTY_NAME")
 ARCH = $(shell uname -m 2>/dev/null)
 NPROC = $(shell sh mk/nproc.sh)
 STREQ = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
 PRINTVAR = printf '\033[1m%-11s\033[0m= %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
 PRINTVARX = $(call PRINTVAR,$(1), \033[32m(%s)\033[0m, '$(origin $(1))')
 USERVARS = CC CFLAGS LDFLAGS LDLIBS DEBUG PKGDATADIR
-AUTOVARS = VERSION KERNEL $(if $(call STREQ,$(KERNEL),Linux), DISTRO) ARCH NPROC
+AUTOVARS = VERSION KERNEL $(if $(call STREQ,$(KERNEL),Linux), DISTRO) ARCH NPROC TERM
 
 vars:
 	@$(foreach VAR, $(AUTOVARS), $(call PRINTVAR,$(VAR));)
