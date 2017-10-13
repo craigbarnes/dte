97b8d74277ee17a1ae0ecf17a55a86c9d880f117
Craig Barnes <Cr@igBarn.es>
2017-10-13 17:12:12 +0100

    Show value of "MAKE_VERSION" and "SHELL" variables in "make vars" output

--- mk/util.mk
+++ mk/util.mk
@@ -6,18 +6,20 @@ NPROC = $(shell sh mk/nproc.sh)
 TPUT = $(shell sh -c 'command -v tput')
 TPUT-V = $(if $(TPUT), $(shell $(TPUT) -V 2>/dev/null))
 STREQ = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
-PRINTVAR = printf '\033[1m%-11s\033[0m= %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
+PRINTVAR = printf '\033[1m%13s\033[0m = %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
 PRINTVARX = $(call PRINTVAR,$(1), \033[32m(%s)\033[0m, '$(origin $(1))')
 USERVARS = CC CFLAGS LDFLAGS LDLIBS DEBUG PKGDATADIR
 
 AUTOVARS = \
     VERSION KERNEL \
     $(if $(call STREQ,$(KERNEL),Linux), DISTRO) \
-    ARCH NPROC TERM TPUT TPUT-V
+    ARCH NPROC TERM TPUT TPUT-V MAKE_VERSION SHELL
 
 vars:
+	@echo
 	@$(foreach VAR, $(AUTOVARS), $(call PRINTVAR,$(VAR));)
 	@$(foreach VAR, $(USERVARS), $(call PRINTVARX,$(VAR));)
+	@echo
 
 
 .PHONY: vars
