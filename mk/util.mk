968e3ca9ad10c6054e1c170d662057be8e18fbec
Craig Barnes <Cr@igBarn.es>
2017-10-13 02:55:29 +0100

    Show output of "command -v tput" and "tput -V" when running "make vars"

--- mk/util.mk
+++ mk/util.mk
@@ -3,11 +3,17 @@ OS := $(shell sh -c 'uname -o 2>/dev/null || echo not')
 DISTRO = $(shell . /etc/os-release && echo "$$NAME $$VERSION_ID")
 ARCH = $(shell uname -m 2>/dev/null)
 NPROC = $(shell sh mk/nproc.sh)
+TPUT = $(shell sh -c 'command -v tput')
+TPUT-V = $(if $(TPUT), $(shell $(TPUT) -V 2>/dev/null))
 STREQ = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
 PRINTVAR = printf '\033[1m%-11s\033[0m= %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
 PRINTVARX = $(call PRINTVAR,$(1), \033[32m(%s)\033[0m, '$(origin $(1))')
 USERVARS = CC CFLAGS LDFLAGS LDLIBS DEBUG PKGDATADIR
-AUTOVARS = VERSION KERNEL $(if $(call STREQ,$(KERNEL),Linux), DISTRO) ARCH NPROC TERM
+
+AUTOVARS = \
+    VERSION KERNEL \
+    $(if $(call STREQ,$(KERNEL),Linux), DISTRO) \
+    ARCH NPROC TERM TPUT TPUT-V
 
 vars:
 	@$(foreach VAR, $(AUTOVARS), $(call PRINTVAR,$(VAR));)
