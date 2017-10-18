dc8e6dc04d0d52103e84b5c474b6dbe3b11dbe1b
Craig Barnes <Cr@igBarn.es>
2017-10-18 07:28:46 +0100

    Add GCC "nonnull" attribute to all functions in src/strbuf.h

--- src/strbuf.h
+++ src/strbuf.h
@@ -1,7 +1,8 @@
 #ifndef STRBUF_H
 #define STRBUF_H
 
-#include <stdlib.h>
+#include <stddef.h>
+#include "macros.h"
 
 typedef struct {
     unsigned char *buffer;
@@ -15,29 +16,29 @@ typedef struct {
     .len = 0 \
 }
 
-static inline void strbuf_init(StringBuffer *buf)
+static inline NONNULL_ARGS void strbuf_init(StringBuffer *buf)
 {
     buf->buffer = NULL;
     buf->alloc = 0;
     buf->len = 0;
 }
 
-static inline void strbuf_clear(StringBuffer *buf)
+static inline NONNULL_ARGS void strbuf_clear(StringBuffer *buf)
 {
     buf->len = 0;
 }
 
-void strbuf_grow(StringBuffer *buf, long more);
-void strbuf_free(StringBuffer *buf);
-void strbuf_add_byte(StringBuffer *buf, unsigned char byte);
-long strbuf_add_ch(StringBuffer *buf, unsigned int u);
-long strbuf_insert_ch(StringBuffer *buf, long pos, unsigned int u);
-void strbuf_add_str(StringBuffer *buf, const char *str);
-void strbuf_add_buf(StringBuffer *buf, const char *ptr, long len);
-char *strbuf_steal(StringBuffer *buf, long *len);
-char *strbuf_steal_cstring(StringBuffer *buf);
-char *strbuf_cstring(StringBuffer *buf);
-void strbuf_make_space(StringBuffer *buf, long pos, long len);
-void strbuf_remove(StringBuffer *buf, long pos, long len);
+void strbuf_grow(StringBuffer *buf, long more) NONNULL_ARGS;
+void strbuf_free(StringBuffer *buf) NONNULL_ARGS;
+void strbuf_add_byte(StringBuffer *buf, unsigned char byte) NONNULL_ARGS;
+long strbuf_add_ch(StringBuffer *buf, unsigned int u) NONNULL_ARGS;
+long strbuf_insert_ch(StringBuffer *buf, long pos, unsigned int u) NONNULL_ARGS;
+void strbuf_add_str(StringBuffer *buf, const char *str) NONNULL_ARGS;
+void strbuf_add_buf(StringBuffer *buf, const char *ptr, long len) NONNULL_ARGS;
+char *strbuf_steal(StringBuffer *buf, long *len) NONNULL_ARGS;
+char *strbuf_steal_cstring(StringBuffer *buf) NONNULL_ARGS;
+char *strbuf_cstring(StringBuffer *buf) NONNULL_ARGS;
+void strbuf_make_space(StringBuffer *buf, long pos, long len) NONNULL_ARGS;
+void strbuf_remove(StringBuffer *buf, long pos, long len) NONNULL_ARGS;
 
 #endif
