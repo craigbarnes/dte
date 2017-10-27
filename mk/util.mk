1fb2760f36e47b9ee0f2868e0ba6e555d305dfb9
Craig Barnes <Cr@igBarn.es>
2017-10-27 23:29:00 +0100

    Show _POSIX_VERSION and _XOPEN_VERSION in "make vars" output

--- mk/util.mk
+++ mk/util.mk
@@ -3,18 +3,21 @@ OS := $(shell sh -c 'uname -o 2>/dev/null || echo not')
 DISTRO = $(shell . /etc/os-release && echo "$$NAME $$VERSION_ID")
 ARCH = $(shell uname -m 2>/dev/null)
 NPROC = $(shell sh mk/nproc.sh)
+_POSIX_VERSION = $(shell getconf _POSIX_VERSION 2>/dev/null)
+_XOPEN_VERSION = $(shell getconf _XOPEN_VERSION 2>/dev/null)
 TPUT = $(shell sh -c 'command -v tput')
 TPUT-V = $(if $(TPUT), $(shell $(TPUT) -V 2>/dev/null))
 CC-VERSION = $(shell $(CC) --version 2>/dev/null | head -n1)
 STREQ = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
-PRINTVAR = printf '\033[1m%13s\033[0m = %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
+PRINTVAR = printf '\033[1m%15s\033[0m = %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
 PRINTVARX = $(call PRINTVAR,$(1), \033[32m(%s)\033[0m, '$(origin $(1))')
 USERVARS = CC CFLAGS LDFLAGS LDLIBS DEBUG
 
 AUTOVARS = \
     VERSION KERNEL \
     $(if $(call STREQ,$(KERNEL),Linux), DISTRO) \
-    ARCH NPROC TERM TPUT TPUT-V MAKE_VERSION SHELL CC-VERSION
+    ARCH NPROC _POSIX_VERSION _XOPEN_VERSION \
+    TERM TPUT TPUT-V MAKE_VERSION SHELL CC-VERSION
 
 vars:
 	@echo
