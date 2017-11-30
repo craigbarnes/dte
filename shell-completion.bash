ff085cb98dfb2b94dd5af55bacd8948daf48dde6
Craig Barnes <Cr@igBarn.es>
2017-11-30 12:50:33 +0000

    Add bash completion script

---
+++ shell-completion.bash
@@ -0,0 +1,25 @@
+_dte() {
+    local cur prev words cword split
+    _init_completion -s || return
+
+    if test "$cur" = '-'; then
+        COMPREPLY=($(compgen -W "-h -B -R -V -c -t -r -b" -- "$cur"))
+        return
+    fi
+
+    case "$prev" in
+    -b)
+        COMPREPLY=($(compgen -W "$(dte -B)" -- "$cur"))
+        return;;
+    -c)
+        COMPREPLY=()
+        return;;
+    -t)
+        COMPREPLY=($(dte -T "$cur" | head -n50000))
+        return;;
+    esac
+
+    COMPREPLY=($(compgen -o default -- "$cur"))
+}
+
+complete -F _dte dte
