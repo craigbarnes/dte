3c181630b0e3bbf35f22b184f486eea6b19dee5a
Craig Barnes <Cr@igBarn.es>
2018-02-04 22:27:39 +0000

    Fix typo in src/file-history.h

--- src/file-history.h
+++ src/file-history.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 void add_file_history(int row, int col, const char *filename);
-void load_file_history(const char *filname);
+void load_file_history(const char *filename);
 void save_file_history(const char *filename);
 bool find_file_in_history(const char *filename, int *row, int *col);
 
