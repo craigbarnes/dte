879d8be5f7fbca25dcd0a8a3b2f96f5e25209a30
Craig Barnes <Cr@igBarn.es>
2018-04-03 20:10:21 +0100

    Add utility script for showing sizes of symbols in object files

---
+++ tools/symsize.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script filters output from "nm" to show the sizes of symbols
+# (in KiB) in an object file, sorted from largest to smallest.
+
+nm -Ptd $@ | awk '
+{
+    name = $1
+    type = $2
+    size = $4
+
+    if (size < 128) {
+        next
+    }
+
+    ksize = sprintf("%.2f", $4 / 1024)
+    printf(" %s %6sK  %s\n", type, ksize, name)
+}
+' | sort -k2 -nr
