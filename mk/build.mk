15f8a6190e24f30c65d2a3c1101f25aebfb6c61b
Craig Barnes <Cr@igBarn.es>
2018-04-08 04:08:39 +0100

    Add syntax highlighter for texmf.cnf files

--- mk/build.mk
+++ mk/build.mk
@@ -19,7 +19,7 @@ WARNINGS_EXTRA = \
 BUILTIN_SYNTAX_FILES := \
     awk c config css d diff docker dte gitcommit gitrebase go html \
     ini java javascript lua mail make markdown meson nginx perl \
-    php python robotstxt roff ruby sh sql tex vala xml
+    php python robotstxt roff ruby sh sql tex texmfcnf vala xml
 
 BUILTIN_CONFIGS := $(addprefix config/, \
     rc filetype \
