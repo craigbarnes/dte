85c63df75b9f9257ca8215cf4fe9223766b7f27c
Craig Barnes <Cr@igBarn.es>
2018-06-12 01:07:46 +0100

    Add unit tests for key_to_string() function

--- test/test_main.c
+++ test/test_main.c
@@ -21,6 +21,9 @@ static void fail(const char *format, ...)
     failed += 1;
 }
 
+#define FOR_EACH_I(i, array) \
+    for (size_t i = 0; i < ARRAY_COUNT(array); i++)
+
 #define EXPECT_EQ(a, b) do { \
     if ((a) != (b)) { \
         fail ( \
@@ -146,6 +149,49 @@ static void test_parse_xterm_key_sequence(void)
     EXPECT_EQ(size, 0);
 }
 
+static void test_key_to_string(void)
+{
+    static const struct key_to_string_test {
+        const char *str;
+        Key key;
+    } tests[] = {
+        {"a", 'a'},
+        {"Z", 'Z'},
+        {"0", '0'},
+        {"{", '{'},
+        {"space", ' '},
+        {"enter", KEY_ENTER},
+        {"tab", '\t'},
+        {"insert", KEY_INSERT},
+        {"delete", KEY_DELETE},
+        {"home", KEY_HOME},
+        {"end", KEY_END},
+        {"pgup", KEY_PAGE_UP},
+        {"pgdown", KEY_PAGE_DOWN},
+        {"left", KEY_LEFT},
+        {"right", KEY_RIGHT},
+        {"up", KEY_UP},
+        {"down", KEY_DOWN},
+        {"C-A", MOD_CTRL | 'A'},
+        {"M-S-{", MOD_META | MOD_SHIFT | '{'},
+        {"C-S-A", MOD_CTRL | MOD_SHIFT | 'A'},
+        {"F1", KEY_F1},
+        {"F12", KEY_F12},
+        {"M-enter", MOD_META | KEY_ENTER},
+        {"M-space", MOD_META | ' '},
+        {"S-tab", MOD_SHIFT | '\t'},
+        {"C-M-S-F12", MOD_CTRL | MOD_META | MOD_SHIFT | KEY_F12},
+        {"C-M-S-up", MOD_CTRL | MOD_META | MOD_SHIFT | KEY_UP},
+        {"C-M-delete", MOD_CTRL | MOD_META | KEY_DELETE},
+        {"C-home", MOD_CTRL | KEY_HOME},
+    };
+    FOR_EACH_I(i, tests) {
+        char *str = key_to_string(tests[i].key);
+        EXPECT_STREQ(str, tests[i].str);
+        free(str);
+    }
+}
+
 int main(void)
 {
     init_editor_state();
@@ -154,6 +200,7 @@ int main(void)
     test_detect_encoding_from_bom();
     test_find_ft();
     test_parse_xterm_key_sequence();
+    test_key_to_string();
 
     return failed ? 1 : 0;
 }
