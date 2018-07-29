8ab7171fbc9d9600b1ec6741c308eddbe098e80e
Craig Barnes <Cr@igBarn.es>
2018-07-29 16:47:17 +0100

    Fix mk/version.sh
    If a git-generated tarball was unpacked into a git checkout directory,
    the "git describe" command would return the version string for the
    git repo, even when inside the unpacked tarball directory.

--- mk/version.sh
+++ mk/version.sh
@@ -5,21 +5,19 @@
 set -eu
 export VPREFIX="$1"
 
-git_describe_ver=$(git describe --match="v$VPREFIX" 2>/dev/null || true)
-
-if test -n "$git_describe_ver"; then
-    echo "${git_describe_ver#v}"
-    exit 0
-fi
-
 # These values are filled automatically for git-archive(1) tarballs.
 # See: "export-subst" in gitattributes(5).
 distinfo_commit_full='$Format:%H$'
 distinfo_commit_short='$Format:%h$'
 distinfo_author_date='$Format:%ai$'
-
 if expr "$distinfo_commit_short" : '[0-9a-f]\{7,40\}$' >/dev/null; then
     echo "${VPREFIX}-g${distinfo_commit_short}-dist"
+    exit 0
+fi
+
+git_describe_ver=$(git describe --match="v$VPREFIX" 2>/dev/null || true)
+if test -n "$git_describe_ver"; then
+    echo "${git_describe_ver#v}"
     exit 0
 fi
 
