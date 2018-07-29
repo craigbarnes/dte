ea4a7de7190406c44997b4a3c484b7b87017051c
Craig Barnes <Cr@igBarn.es>
2018-07-29 16:39:43 +0100

    Remove .distinfo file
    There's no need to clutter the base directory with obscure files. The
    distinfo values can be inserted directly into mk/version.sh instead.

--- mk/version.sh
+++ mk/version.sh
@@ -12,14 +12,14 @@ if test -n "$git_describe_ver"; then
     exit 0
 fi
 
-distinfo_ver=$(awk '
-    NR==1 && /^[a-f0-9]{40}$/ {
-        print ENVIRON["VPREFIX"] "-" substr($0,0,12) "-dist"
-    }
-' .distinfo)
+# These values are filled automatically for git-archive(1) tarballs.
+# See: "export-subst" in gitattributes(5).
+distinfo_commit_full='$Format:%H$'
+distinfo_commit_short='$Format:%h$'
+distinfo_author_date='$Format:%ai$'
 
-if test -n "$distinfo_ver"; then
-    echo "$distinfo_ver"
+if expr "$distinfo_commit_short" : '[0-9a-f]\{7,40\}$' >/dev/null; then
+    echo "${VPREFIX}-g${distinfo_commit_short}-dist"
     exit 0
 fi
 
