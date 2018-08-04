06141970c5b85df91b6dddeed47f446452ed28e4
Craig Barnes <Cr@igBarn.es>
2018-08-04 02:40:02 +0100

    Move syntax-lint.awk script to tools/ directory

--- mk/check.mk
+++
@@ -1,9 +0,0 @@
-SYNTAX_LINT = $(AWK) -f test/syntax-lint.awk
-
-check-syntax-files:
-	$(E) LINT 'config/syntax/*'
-	$(Q) $(SYNTAX_LINT) $(addprefix config/syntax/, $(BUILTIN_SYNTAX_FILES))
-	$(Q) ! $(SYNTAX_LINT) test/data/syntax-lint.dterc 2>/dev/null
-
-
-.PHONY: check-syntax-files
--- mk/dev.mk
+++ mk/dev.mk
@@ -1,10 +1,16 @@
 DIST_VERSIONS = 1.7 1.6 1.5 1.4 1.3 1.2 1.1 1.0
 DIST_ALL = $(addprefix dte-, $(addsuffix .tar.gz, $(DIST_VERSIONS)))
 GIT_HOOKS = $(addprefix .git/hooks/, commit-msg pre-commit)
+SYNTAX_LINT = $(AWK) -f tools/syntax-lint.awk
 
 dist: $(firstword $(DIST_ALL))
 dist-all: $(DIST_ALL)
 git-hooks: $(GIT_HOOKS)
+
+check-syntax-files:
+	$(E) LINT 'config/syntax/*'
+	$(Q) $(SYNTAX_LINT) $(addprefix config/syntax/, $(BUILTIN_SYNTAX_FILES))
+	$(Q) ! $(SYNTAX_LINT) test/data/syntax-lint.dterc 2>/dev/null
 
 check-dist: dist-all
 	@sha256sum -c mk/sha256sums.txt
@@ -43,4 +49,7 @@ coverage-report:
 
 
 CLEANFILES += dte-*.tar.gz
-.PHONY: dist dist-all check-dist git-hooks show-sizes coverage-report
+
+.PHONY: \
+    check-syntax-files check-dist dist dist-all git-hooks \
+    show-sizes coverage-report
