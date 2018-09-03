dfe902af73cdc14ae8e101333d194b9cc9e3b978
Craig Barnes <Cr@igBarn.es>
2018-09-03 19:08:02 +0100

    Clean up src/util/ascii.c

--- src/util/ascii.c
+++ src/util/ascii.c
@@ -1,6 +1,3 @@
-// Sane, locale-independent ASCII ctype (borrowed from git).
-// No surprises and works with signed and unsigned chars.
-
 #include "ascii.h"
 
 enum {
@@ -8,23 +5,21 @@ enum {
     L = ASCII_LOWER,
     U = ASCII_UPPER,
     D = ASCII_DIGIT,
-    x = ASCII_LOWER | ASCII_HEX_LOWER,
-    X = ASCII_UPPER | ASCII_HEX_UPPER,
 };
 
-const unsigned char dte_ascii_table[256] = {
+const unsigned char ascii_table[256] = {
     ['\t'] = S, ['\n'] = S, ['\r'] = S, [' '] = S,
 
     ['0'] = D, ['1'] = D, ['2'] = D, ['3'] = D, ['4'] = D,
     ['5'] = D, ['6'] = D, ['7'] = D, ['8'] = D, ['9'] = D,
 
-    ['A'] = X, ['B'] = X, ['C'] = X, ['D'] = X, ['E'] = X, ['F'] = X,
+    ['A'] = U, ['B'] = U, ['C'] = U, ['D'] = U, ['E'] = U, ['F'] = U,
     ['G'] = U, ['H'] = U, ['I'] = U, ['J'] = U, ['K'] = U, ['L'] = U,
     ['M'] = U, ['N'] = U, ['O'] = U, ['P'] = U, ['Q'] = U, ['R'] = U,
     ['S'] = U, ['T'] = U, ['U'] = U, ['V'] = U, ['W'] = U, ['X'] = U,
     ['Y'] = U, ['Z'] = U,
 
-    ['a'] = x, ['b'] = x, ['c'] = x, ['d'] = x, ['e'] = x, ['f'] = x,
+    ['a'] = L, ['b'] = L, ['c'] = L, ['d'] = L, ['e'] = L, ['f'] = L,
     ['g'] = L, ['h'] = L, ['i'] = L, ['j'] = L, ['k'] = L, ['l'] = L,
     ['m'] = L, ['n'] = L, ['o'] = L, ['p'] = L, ['q'] = L, ['r'] = L,
     ['s'] = L, ['t'] = L, ['u'] = L, ['v'] = L, ['w'] = L, ['x'] = L,
@@ -33,12 +28,17 @@ const unsigned char dte_ascii_table[256] = {
 
 int hex_decode(int ch)
 {
-    if (sane_istest(ch, ASCII_DIGIT)) {
+    switch (ch) {
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
         return ch - '0';
-    } else if (sane_istest(ch, ASCII_HEX_LOWER)) {
+    case 'a': case 'b': case 'c': case 'd': case 'e':
+    case 'f':
         return ch - 'a' + 10;
-    } else if (sane_istest(ch, ASCII_HEX_UPPER)) {
+    case 'A': case 'B': case 'C': case 'D': case 'E':
+    case 'F':
         return ch - 'A' + 10;
+    default:
+        return -1;
     }
-    return -1;
 }
--- src/util/ascii.h
+++ src/util/ascii.h
@@ -4,23 +4,20 @@
 #include <stdbool.h>
 #include "macros.h"
 
-extern const unsigned char dte_ascii_table[256];
+extern const unsigned char ascii_table[256];
 
 #define ASCII_SPACE 0x01
 #define ASCII_DIGIT 0x02
 #define ASCII_LOWER 0x04
 #define ASCII_UPPER 0x08
-#define ASCII_HEX_LOWER 0x10
-#define ASCII_HEX_UPPER 0x20
 
-#define sane_istest(x,mask) ((dte_ascii_table[(unsigned char)(x)] & (mask)) != 0)
-#define ascii_isspace(x) sane_istest(x, ASCII_SPACE)
-#define ascii_isdigit(x) sane_istest(x, ASCII_DIGIT)
-#define ascii_islower(x) sane_istest(x, ASCII_LOWER)
-#define ascii_isupper(x) sane_istest(x, ASCII_UPPER)
-#define ascii_isalpha(x) sane_istest(x, ASCII_LOWER | ASCII_UPPER)
-#define ascii_isalnum(x) sane_istest(x, ASCII_LOWER | ASCII_UPPER | ASCII_DIGIT)
-#define ascii_isxdigit(x) sane_istext(x, ASCII_DIGIT | ASCII_HEX_LOWER | ASCII_HEX_UPPER)
+#define ascii_test(x, mask) ((ascii_table[(unsigned char)(x)] & (mask)) != 0)
+#define ascii_isspace(x) ascii_test(x, ASCII_SPACE)
+#define ascii_isdigit(x) ascii_test(x, ASCII_DIGIT)
+#define ascii_islower(x) ascii_test(x, ASCII_LOWER)
+#define ascii_isupper(x) ascii_test(x, ASCII_UPPER)
+#define ascii_isalpha(x) ascii_test(x, ASCII_LOWER | ASCII_UPPER)
+#define ascii_isalnum(x) ascii_test(x, ASCII_LOWER | ASCII_UPPER | ASCII_DIGIT)
 
 static inline int PURE ascii_tolower(int x)
 {
@@ -43,6 +40,6 @@ static inline bool PURE is_word_byte(unsigned char byte)
     return ascii_isalnum(byte) || byte == '_' || byte > 0x7f;
 }
 
-int hex_decode(int ch) PURE;
+int hex_decode(int ch) CONST_FN;
 
 #endif
