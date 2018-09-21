89e7605ca49e719ea15cfa61d75c7dc871e38769
Craig Barnes <Cr@igBarn.es>
2018-09-21 22:00:05 +0100

    Add "make gen-unicode-tables" target
    For conveniently fetching the required UCD files (with curl) and
    generating the tables in a single command.

--- mk/gen.mk
+++ mk/gen.mk
@@ -1,9 +1,13 @@
+FETCH = curl -LSs -o $@
+UCD_FILES = .cache/UnicodeData.txt .cache/EastAsianWidth.txt
+LUA_DEP = $(if $(call streq,$(USE_LUA),static), $(LUA))
+
 define LPERF_GEN
   $(E) GEN $(1).c
   $(Q) $(LUA) mk/lperf.lua $(1).lua $(1).c
 endef
 
-gen: $(if $(call streq,$(USE_LUA),static), $(LUA))
+gen: $(LUA_DEP)
 	$(call LPERF_GEN, src/lookup/extensions)
 	$(call LPERF_GEN, src/lookup/basenames)
 	$(call LPERF_GEN, src/lookup/pathnames)
@@ -12,5 +16,18 @@ gen: $(if $(call streq,$(USE_LUA),static), $(LUA))
 	$(E) GEN src/terminal/xterm-keys.c
 	$(Q) $(LUA) src/terminal/xterm-keys.lua > src/terminal/xterm-keys.c
 
+gen-unicode-tables: build/unicode-tables.c
 
-.PHONY: gen
+build/unicode-tables.c: tools/gen-unicode-tables.lua $(UCD_FILES) | $(LUA_DEP)
+	$(E) GEN $@
+	$(Q) $(LUA) $< $(UCD_FILES) > $@
+
+$(UCD_FILES): | .cache/
+	$(E) FETCH $@
+	$(Q) $(FETCH) https://unicode.org/Public/11.0.0/ucd/$(@F)
+
+.cache/:
+	@mkdir -p $@
+
+
+.PHONY: gen gen-unicode-tables
