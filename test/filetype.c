craigbarnes/dte

commit 3ffaaccc6d6b727b718961f7b63afa1ce8a0107d

Improve mk/lperf.lua generator script
* The output now includes a key/value table, similar to gperf(1)
* The generated C code should be less reliant on compiler optimization
* The binary size is basically the same as before (as compiled with GCC)

mk/lperf.lua
@@ -15,79 +15,118 @@ end
 
 local function make_trie(keys)
     local trie = {n = 0}
-    for seq, key in pairs(keys) do
-        local n = #seq
-        if n > trie.n then
-            trie.n = n
+    for key, value in pairs(keys) do
+        local key_length = #key
+        if key_length > trie.n then
+            trie.n = key_length
         end
-        if not trie[n] then
-            trie[n] = {}
+
+        local node = trie[key_length]
+        if not node then
+            node = {type = "branch", child_count = 0}
+            trie[key_length] = node
         end
-        local node = trie[n]
-        for i = 1, n do
-            local byte = assert(seq:sub(i, i):byte())
-            assert(byte >= (" "):byte() and byte <= ("~"):byte())
-            if i == n then
-                -- Leaf node
-                assert(node[byte] == nil, "Clashing sequence: " .. seq)
-                node[byte] = key
-                break
-            elseif type(node[byte]) ~= "table" then
-                assert(node[byte] == nil, "Clashing sequence: " .. seq)
-                node[byte] = {}
+
+        for i = 1, key_length do
+            local byte = assert(key:sub(i, i):byte())
+            local min, max = node.min, node.max
+            if not min or byte < min then
+                node.min = byte
             end
-            node = node[byte]
+            if not max or byte > max then
+                node.max = byte
+            end
+
+            if i == key_length then
+                assert(node[byte] == nil)
+                node[byte] = {type = "leaf", key = key, value = value}
+                node.child_count = node.child_count + 1
+            else
+                if not node[byte] then
+                    node[byte] = {type = "branch", child_count = 0}
+                    node.child_count = node.child_count + 1
+                end
+                node = node[byte]
+            end
         end
     end
     return trie
 end
 
 local function compress(node)
-    local buf, n = {}, 0
-    while type(node) == "table" do
-        local k = next(node)
-        if next(node, k) then
-            if n > 1 then
-                return concat(buf), node
+    local skip = 0
+    while node.type == "branch" do
+        if node.child_count > 1 then
+            if skip > 1 then
+                return skip, node
             end
             return false
         end
+        skip = skip + 1
+        node = node[node.min]
+    end
+    return skip, node
+end
+
+local function make_index(keys)
+    local index, n, longest = {}, 0, 0
+    for k in pairs(keys) do
         n = n + 1
-        buf[n] = char(k)
-        node = node[k]
+        index[n] = k
+        local klen = #k
+        if klen > longest then
+            longest = klen
+        end
     end
-    return concat(buf), node
+    table.sort(index)
+    for i = 1, n do
+        local key = index[i]
+        index[key] = i
+    end
+    index.n = n
+    index.longest_key = longest
+    return index
 end
 
-local function memcmp(offset, str)
-    local length = #str
-    if length == 1 then
-        return ("(s[%u] != '%s')"):format(offset, str)
-    elseif offset == 0 then
-        return ('memcmp(s, "%s", %u)'):format(str, length)
+local function write_index(index, buf, defs)
+    local key_type
+    if index.longest_key <= 8 then
+        key_type = "const char key[8]"
+    elseif index.longest_key <= 16 then
+        key_type = "const char key[16]"
+    else
+        key_type = "const char *const key"
     end
-    return ('memcmp(s + %d, "%s", %u)'):format(offset, str, length)
+    local n = #index
+    buf:write (
+        "static const struct {\n",
+        "    ", key_type, ";\n",
+        "    const ", defs.return_type, " val;\n",
+        "} ", defs.function_name, "_table[", tostring(n), "] = {\n"
+    )
+    for i = 1, n do
+        local k = index[i]
+        buf:write('    {"', k, '", ', defs.keys[k], "},\n")
+    end
+    buf:write("};\n\n")
 end
 
-local function write_trie(node, buf, default_return)
+local function write_trie(node, buf, defs, index)
+    local default_return = defs.default_return
     local indents = Indent.new(4)
     local function serialize(node, level, indent_depth)
         local indent = indents[indent_depth]
-        if type(node) == "table" then
-            local s, n = compress(node)
-            if s then
-                if type(n) == "table" then
-                    buf:write (
-                        indent, "if (", memcmp(level, s), ") {\n",
-                        indent, "    return ", default_return, ";\n",
-                        indent, "}\n"
-                    )
+        if node.type == "branch" then
+            local skip, n = compress(node)
+            if skip then
+                if n.type == "branch" then
                     node = n
-                    level = level + #s
+                    level = level + skip
                 else
+                    assert(n.type == "leaf")
                     buf:write (
-                        indent, "return ", memcmp(level, s), " ? ",
-                        default_return, " : ", n, ";\n"
+                        indent, "idx = ", index[n.key] - 1, "; // ", n.key, "\n",
+                        indent, "goto compare;\n"
                     )
                     return
                 end
@@ -101,9 +140,10 @@ local function write_trie(node, buf, default_return)
                     serialize(v, level + 1, indent_depth + 1)
                 end
             end
-            buf:write(indent, "}\n", indent, "return ", default_return, ";\n")
+            buf:write(indent, "}\n", indent, "break;\n")
         else
-            buf:write(indent, "return ", node, ";\n")
+            assert(node.type == "leaf")
+            buf:write(indent, "return ", node.value, ";\n")
         end
     end
 
@@ -134,13 +174,27 @@ local keys = assert(defs.keys, "No keys defined")
 local fname = assert(defs.function_name, "No function_name defined")
 local rtype = assert(defs.return_type, "No return_type defined")
 local rdefault = assert(defs.default_return, "No default_return defined")
-
-local headers = "#include <stddef.h>\n#include <string.h>\n\n"
-local proto = "%sstatic %s %s(const char *s, size_t len)\n{\n"
-local prelude = proto:format(defs.includes and headers or "", rtype, fname)
-
+local index = assert(make_index(keys))
 local trie = assert(make_trie(keys))
-output:write(prelude)
-write_trie(trie, output, rdefault)
-output:write("}\n")
+
+local prelude = [[
+static %s %s(const char *s, size_t len)
+{
+    size_t idx;
+    const char *key;
+    %s val;
+]]
+
+local postlude = [[
+compare:
+    key = %s_table[idx].key;
+    val = %s_table[idx].val;
+    return memcmp(s, key, len) ? %s : val;
+}
+]]
+
+write_index(index, output, defs)
+output:write(prelude:format(rtype, fname, rtype))
+write_trie(trie, output, defs, index)
+output:write(postlude:format(fname, fname, rdefault))
 output:flush()

src/lookup/basenames.c
@@ -1,245 +1,403 @@
+static const struct {
+    const char *const key;
+    const FileTypeEnum val;
+} filetype_from_basename_table[74] = {
+    {".bash_logout", SHELL},
+    {".bash_profile", SHELL},
+    {".bashrc", SHELL},
+    {".clang-format", YAML},
+    {".cshrc", SHELL},
+    {".drirc", XML},
+    {".editorconfig", INI},
+    {".emacs", EMACSLISP},
+    {".gemrc", YAML},
+    {".gitattributes", CONFIG},
+    {".gitconfig", INI},
+    {".gitmodules", INI},
+    {".gnus", EMACSLISP},
+    {".indent.pro", INDENT},
+    {".inputrc", INPUTRC},
+    {".jshintrc", JSON},
+    {".luacheckrc", LUA},
+    {".luacov", LUA},
+    {".profile", SHELL},
+    {".zlogin", SHELL},
+    {".zlogout", SHELL},
+    {".zprofile", SHELL},
+    {".zshenv", SHELL},
+    {".zshrc", SHELL},
+    {"APKBUILD", SHELL},
+    {"BSDmakefile", MAKE},
+    {"BUILD.bazel", PYTHON},
+    {"CMakeLists.txt", CMAKE},
+    {"COMMIT_EDITMSG", GITCOMMIT},
+    {"Capfile", RUBY},
+    {"Cargo.lock", TOML},
+    {"Dockerfile", DOCKER},
+    {"Doxyfile", DOXYGEN},
+    {"GNUmakefile", MAKE},
+    {"Gemfile", RUBY},
+    {"Gemfile.lock", RUBY},
+    {"Kbuild", MAKE},
+    {"Makefile", MAKE},
+    {"Makefile.am", MAKE},
+    {"Makefile.in", MAKE},
+    {"PKGBUILD", SHELL},
+    {"Project.ede", EMACSLISP},
+    {"Rakefile", RUBY},
+    {"Vagrantfile", RUBY},
+    {"bash_logout", SHELL},
+    {"bash_profile", SHELL},
+    {"bashrc", SHELL},
+    {"config.ld", LUA},
+    {"configure.ac", M4},
+    {"cshrc", SHELL},
+    {"drirc", XML},
+    {"git-rebase-todo", GITREBASE},
+    {"gitattributes", CONFIG},
+    {"gitconfig", INI},
+    {"inputrc", INPUTRC},
+    {"krb5.conf", INI},
+    {"makefile", MAKE},
+    {"meson.build", MESON},
+    {"meson_options.txt", MESON},
+    {"mimeapps.list", INI},
+    {"mkinitcpio.conf", SHELL},
+    {"nginx.conf", NGINX},
+    {"pacman.conf", INI},
+    {"profile", SHELL},
+    {"robots.txt", ROBOTSTXT},
+    {"rockspec.in", LUA},
+    {"terminalrc", INI},
+    {"texmf.cnf", TEXMFCNF},
+    {"yum.conf", INI},
+    {"zlogin", SHELL},
+    {"zlogout", SHELL},
+    {"zprofile", SHELL},
+    {"zshenv", SHELL},
+    {"zshrc", SHELL},
+};
+
 static FileTypeEnum filetype_from_basename(const char *s, size_t len)
 {
+    size_t idx;
+    const char *key;
+    FileTypeEnum val;
     switch (len) {
     case 5:
         switch (s[0]) {
         case '.':
-            return memcmp(s + 1, "gnus", 4) ? 0 : EMACSLISP;
+            idx = 12; // .gnus
+            goto compare;
         case 'c':
-            return memcmp(s + 1, "shrc", 4) ? 0 : SHELL;
+            idx = 49; // cshrc
+            goto compare;
         case 'd':
-            return memcmp(s + 1, "rirc", 4) ? 0 : XML;
+            idx = 50; // drirc
+            goto compare;
         case 'z':
-            return memcmp(s + 1, "shrc", 4) ? 0 : SHELL;
+            idx = 73; // zshrc
+            goto compare;
         }
-        return 0;
+        break;
     case 6:
         switch (s[0]) {
         case '.':
             switch (s[1]) {
             case 'c':
-                return memcmp(s + 2, "shrc", 4) ? 0 : SHELL;
+                idx = 4; // .cshrc
+                goto compare;
             case 'd':
-                return memcmp(s + 2, "rirc", 4) ? 0 : XML;
+                idx = 5; // .drirc
+                goto compare;
             case 'e':
-                return memcmp(s + 2, "macs", 4) ? 0 : EMACSLISP;
+                idx = 7; // .emacs
+                goto compare;
             case 'g':
-                return memcmp(s + 2, "emrc", 4) ? 0 : YAML;
+                idx = 8; // .gemrc
+                goto compare;
             case 'z':
-                return memcmp(s + 2, "shrc", 4) ? 0 : SHELL;
+                idx = 23; // .zshrc
+                goto compare;
             }
-            return 0;
+            break;
         case 'K':
-            return memcmp(s + 1, "build", 5) ? 0 : MAKE;
+            idx = 36; // Kbuild
+            goto compare;
         case 'b':
-            return memcmp(s + 1, "ashrc", 5) ? 0 : SHELL;
+            idx = 46; // bashrc
+            goto compare;
         case 'z':
             switch (s[1]) {
             case 'l':
-                return memcmp(s + 2, "ogin", 4) ? 0 : SHELL;
+                idx = 69; // zlogin
+                goto compare;
             case 's':
-                return memcmp(s + 2, "henv", 4) ? 0 : SHELL;
+                idx = 72; // zshenv
+                goto compare;
             }
-            return 0;
+            break;
         }
-        return 0;
+        break;
     case 7:
         switch (s[0]) {
         case '.':
             switch (s[1]) {
             case 'b':
-                return memcmp(s + 2, "ashrc", 5) ? 0 : SHELL;
+                idx = 2; // .bashrc
+                goto compare;
             case 'l':
-                return memcmp(s + 2, "uacov", 5) ? 0 : LUA;
+                idx = 17; // .luacov
+                goto compare;
             case 'z':
                 switch (s[2]) {
                 case 'l':
-                    return memcmp(s + 3, "ogin", 4) ? 0 : SHELL;
+                    idx = 19; // .zlogin
+                    goto compare;
                 case 's':
-                    return memcmp(s + 3, "henv", 4) ? 0 : SHELL;
+                    idx = 22; // .zshenv
+                    goto compare;
                 }
-                return 0;
+                break;
             }
-            return 0;
+            break;
         case 'C':
-            return memcmp(s + 1, "apfile", 6) ? 0 : RUBY;
+            idx = 29; // Capfile
+            goto compare;
         case 'G':
-            return memcmp(s + 1, "emfile", 6) ? 0 : RUBY;
+            idx = 34; // Gemfile
+            goto compare;
         case 'i':
-            return memcmp(s + 1, "nputrc", 6) ? 0 : INPUTRC;
+            idx = 54; // inputrc
+            goto compare;
         case 'p':
-            return memcmp(s + 1, "rofile", 6) ? 0 : SHELL;
+            idx = 63; // profile
+            goto compare;
         case 'z':
-            return memcmp(s + 1, "logout", 6) ? 0 : SHELL;
+            idx = 70; // zlogout
+            goto compare;
         }
-        return 0;
+        break;
     case 8:
         switch (s[0]) {
         case '.':
             switch (s[1]) {
             case 'i':
-                return memcmp(s + 2, "nputrc", 6) ? 0 : INPUTRC;
+                idx = 14; // .inputrc
+                goto compare;
             case 'p':
-                return memcmp(s + 2, "rofile", 6) ? 0 : SHELL;
+                idx = 18; // .profile
+                goto compare;
             case 'z':
-                return memcmp(s + 2, "logout", 6) ? 0 : SHELL;
+                idx = 20; // .zlogout
+                goto compare;
             }
-            return 0;
+            break;
         case 'A':
-            return memcmp(s + 1, "PKBUILD", 7) ? 0 : SHELL;
+            idx = 24; // APKBUILD
+            goto compare;
         case 'D':
-            return memcmp(s + 1, "oxyfile", 7) ? 0 : DOXYGEN;
+            idx = 32; // Doxyfile
+            goto compare;
         case 'M':
-            return memcmp(s + 1, "akefile", 7) ? 0 : MAKE;
+            idx = 37; // Makefile
+            goto compare;
         case 'P':
-            return memcmp(s + 1, "KGBUILD", 7) ? 0 : SHELL;
+            idx = 40; // PKGBUILD
+            goto compare;
         case 'R':
-            return memcmp(s + 1, "akefile", 7) ? 0 : RUBY;
+            idx = 42; // Rakefile
+            goto compare;
         case 'm':
-            return memcmp(s + 1, "akefile", 7) ? 0 : MAKE;
+            idx = 56; // makefile
+            goto compare;
         case 'y':
-            return memcmp(s + 1, "um.conf", 7) ? 0 : INI;
+            idx = 68; // yum.conf
+            goto compare;
         case 'z':
-            return memcmp(s + 1, "profile", 7) ? 0 : SHELL;
+            idx = 71; // zprofile
+            goto compare;
         }
-        return 0;
+        break;
     case 9:
         switch (s[0]) {
         case '.':
             switch (s[1]) {
             case 'j':
-                return memcmp(s + 2, "shintrc", 7) ? 0 : JSON;
+                idx = 15; // .jshintrc
+                goto compare;
             case 'z':
-                return memcmp(s + 2, "profile", 7) ? 0 : SHELL;
+                idx = 21; // .zprofile
+                goto compare;
             }
-            return 0;
+            break;
         case 'c':
-            return memcmp(s + 1, "onfig.ld", 8) ? 0 : LUA;
+            idx = 47; // config.ld
+            goto compare;
         case 'g':
-            return memcmp(s + 1, "itconfig", 8) ? 0 : INI;
+            idx = 53; // gitconfig
+            goto compare;
         case 'k':
-            return memcmp(s + 1, "rb5.conf", 8) ? 0 : INI;
+            idx = 55; // krb5.conf
+            goto compare;
         case 't':
-            return memcmp(s + 1, "exmf.cnf", 8) ? 0 : TEXMFCNF;
+            idx = 67; // texmf.cnf
+            goto compare;
         }
-        return 0;
+        break;
     case 10:
         switch (s[0]) {
         case '.':
-            return memcmp(s + 1, "gitconfig", 9) ? 0 : INI;
+            idx = 10; // .gitconfig
+            goto compare;
         case 'C':
-            return memcmp(s + 1, "argo.lock", 9) ? 0 : TOML;
+            idx = 30; // Cargo.lock
+            goto compare;
         case 'D':
-            return memcmp(s + 1, "ockerfile", 9) ? 0 : DOCKER;
+            idx = 31; // Dockerfile
+            goto compare;
         case 'n':
-            return memcmp(s + 1, "ginx.conf", 9) ? 0 : NGINX;
+            idx = 61; // nginx.conf
+            goto compare;
         case 'r':
-            return memcmp(s + 1, "obots.txt", 9) ? 0 : ROBOTSTXT;
+            idx = 64; // robots.txt
+            goto compare;
         case 't':
-            return memcmp(s + 1, "erminalrc", 9) ? 0 : INI;
+            idx = 66; // terminalrc
+            goto compare;
         }
-        return 0;
+        break;
     case 11:
         switch (s[0]) {
         case '.':
             switch (s[1]) {
             case 'g':
-                return memcmp(s + 2, "itmodules", 9) ? 0 : INI;
+                idx = 11; // .gitmodules
+                goto compare;
             case 'i':
-                return memcmp(s + 2, "ndent.pro", 9) ? 0 : INDENT;
+                idx = 13; // .indent.pro
+                goto compare;
             case 'l':
-                return memcmp(s + 2, "uacheckrc", 9) ? 0 : LUA;
+                idx = 16; // .luacheckrc
+                goto compare;
             }
-            return 0;
+            break;
         case 'B':
             switch (s[1]) {
             case 'S':
-                return memcmp(s + 2, "Dmakefile", 9) ? 0 : MAKE;
+                idx = 25; // BSDmakefile
+                goto compare;
             case 'U':
-                return memcmp(s + 2, "ILD.bazel", 9) ? 0 : PYTHON;
+                idx = 26; // BUILD.bazel
+                goto compare;
             }
-            return 0;
+            break;
         case 'G':
-            return memcmp(s + 1, "NUmakefile", 10) ? 0 : MAKE;
+            idx = 33; // GNUmakefile
+            goto compare;
         case 'M':
-            if (memcmp(s + 1, "akefile.", 8)) {
-                return 0;
-            }
             switch (s[9]) {
             case 'a':
-                return (s[10] != 'm') ? 0 : MAKE;
+                idx = 38; // Makefile.am
+                goto compare;
             case 'i':
-                return (s[10] != 'n') ? 0 : MAKE;
+                idx = 39; // Makefile.in
+                goto compare;
             }
-            return 0;
+            break;
         case 'P':
-            return memcmp(s + 1, "roject.ede", 10) ? 0 : EMACSLISP;
+            idx = 41; // Project.ede
+            goto compare;
         case 'V':
-            return memcmp(s + 1, "agrantfile", 10) ? 0 : RUBY;
+            idx = 43; // Vagrantfile
+            goto compare;
         case 'b':
-            return memcmp(s + 1, "ash_logout", 10) ? 0 : SHELL;
+            idx = 44; // bash_logout
+            goto compare;
         case 'm':
-            return memcmp(s + 1, "eson.build", 10) ? 0 : MESON;
+            idx = 57; // meson.build
+            goto compare;
         case 'p':
-            return memcmp(s + 1, "acman.conf", 10) ? 0 : INI;
+            idx = 62; // pacman.conf
+            goto compare;
         case 'r':
-            return memcmp(s + 1, "ockspec.in", 10) ? 0 : LUA;
+            idx = 65; // rockspec.in
+            goto compare;
         }
-        return 0;
+        break;
     case 12:
         switch (s[0]) {
         case '.':
-            return memcmp(s + 1, "bash_logout", 11) ? 0 : SHELL;
+            idx = 0; // .bash_logout
+            goto compare;
         case 'G':
-            return memcmp(s + 1, "emfile.lock", 11) ? 0 : RUBY;
+            idx = 35; // Gemfile.lock
+            goto compare;
         case 'b':
-            return memcmp(s + 1, "ash_profile", 11) ? 0 : SHELL;
+            idx = 45; // bash_profile
+            goto compare;
         case 'c':
-            return memcmp(s + 1, "onfigure.ac", 11) ? 0 : M4;
+            idx = 48; // configure.ac
+            goto compare;
         }
-        return 0;
+        break;
     case 13:
         switch (s[0]) {
         case '.':
             switch (s[1]) {
             case 'b':
-                return memcmp(s + 2, "ash_profile", 11) ? 0 : SHELL;
+                idx = 1; // .bash_profile
+                goto compare;
             case 'c':
-                return memcmp(s + 2, "lang-format", 11) ? 0 : YAML;
+                idx = 3; // .clang-format
+                goto compare;
             case 'e':
-                return memcmp(s + 2, "ditorconfig", 11) ? 0 : INI;
+                idx = 6; // .editorconfig
+                goto compare;
             }
-            return 0;
+            break;
         case 'g':
-            return memcmp(s + 1, "itattributes", 12) ? 0 : CONFIG;
+            idx = 52; // gitattributes
+            goto compare;
         case 'm':
-            return memcmp(s + 1, "imeapps.list", 12) ? 0 : INI;
+            idx = 59; // mimeapps.list
+            goto compare;
         }
-        return 0;
+        break;
     case 14:
         switch (s[0]) {
         case '.':
-            return memcmp(s + 1, "gitattributes", 13) ? 0 : CONFIG;
+            idx = 9; // .gitattributes
+            goto compare;
         case 'C':
             switch (s[1]) {
             case 'M':
-                return memcmp(s + 2, "akeLists.txt", 12) ? 0 : CMAKE;
+                idx = 27; // CMakeLists.txt
+                goto compare;
             case 'O':
-                return memcmp(s + 2, "MMIT_EDITMSG", 12) ? 0 : GITCOMMIT;
+                idx = 28; // COMMIT_EDITMSG
+                goto compare;
             }
-            return 0;
+            break;
         }
-        return 0;
+        break;
     case 15:
         switch (s[0]) {
         case 'g':
-            return memcmp(s + 1, "it-rebase-todo", 14) ? 0 : GITREBASE;
+            idx = 51; // git-rebase-todo
+            goto compare;
         case 'm':
-            return memcmp(s + 1, "kinitcpio.conf", 14) ? 0 : SHELL;
+            idx = 60; // mkinitcpio.conf
+            goto compare;
         }
-        return 0;
+        break;
     case 17:
-        return memcmp(s, "meson_options.txt", 17) ? 0 : MESON;
+        idx = 58; // meson_options.txt
+        goto compare;
     }
     return 0;
+compare:
+    key = filetype_from_basename_table[idx].key;
+    val = filetype_from_basename_table[idx].val;
+    return memcmp(s, key, len) ? 0 : val;
 }

commit c6d6dbe50593b7acd2b0ce7371a5bc52594a72bb

Fix bug in mk/lperf.lua that was introduced in commit 3ffaaccc6d6b7
The call to memcmp() that checks compressed substrings can't be
omitted because a return statement within the switch tree would
allow non-matching bytes through as if they were a positive match.

mk/lperf.lua
@@ -50,18 +50,20 @@ local function make_trie(keys)
 end
 
 local function compress(node)
-    local skip = 0
+    local buf, n = {}, 0
     while node.type == "branch" do
         if node.child_count > 1 then
-            if skip > 1 then
-                return skip, node
+            if n > 1 then
+                return concat(buf), node
             end
             return false
         end
-        skip = skip + 1
-        node = node[node.min]
+        local k = node.min
+        n = n + 1
+        buf[n] = char(k)
+        node = node[k]
     end
-    return skip, node
+    return concat(buf), node
 end
 
 local function make_index(keys)
@@ -108,17 +110,32 @@ local function write_index(index, buf, defs)
     buf:write("};\n\n")
 end
 
+local function memcmp(offset, str)
+    local length = #str
+    if length == 1 then
+        return ("(s[%u] != '%s')"):format(offset, str)
+    elseif offset == 0 then
+        return ('memcmp(s, "%s", %u)'):format(str, length)
+    end
+    return ('memcmp(s + %d, "%s", %u)'):format(offset, str, length)
+end
+
 local function write_trie(node, buf, defs, index)
     local default_return = defs.default_return
     local indents = Indent.new(4)
     local function serialize(node, level, indent_depth)
         local indent = indents[indent_depth]
         if node.type == "branch" then
-            local skip, n = compress(node)
-            if skip then
+            local s, n = compress(node)
+            if s then
                 if n.type == "branch" then
+                    buf:write (
+                        "\n", indent, "if (", memcmp(level, s), ") {\n",
+                        indent, "    return ", default_return, ";\n",
+                        indent, "}"
+                    )
                     node = n
-                    level = level + skip
+                    level = level + #s
                 else
                     assert(n.type == "leaf")
                     buf:write(" CMP(", index[n.key] - 1, "); // ", n.key, "\n")

src/lookup/basenames.c
@@ -196,6 +196,9 @@ static FileTypeEnum filetype_from_basename(const char *s, size_t len)
             break;
         case 'G': CMP(33); // GNUmakefile
         case 'M':
+            if (memcmp(s + 1, "akefile.", 8)) {
+                return 0;
+            }
             switch (s[9]) {
             case 'a': CMP(38); // Makefile.am
             case 'i': CMP(39); // Makefile.in

src/lookup/extensions.c
@@ -549,6 +549,9 @@ static FileTypeEnum filetype_from_extension(const char *s, size_t len)
         case 'g':
             switch (s[1]) {
             case 'l':
+                if (memcmp(s + 2, "sl", 2)) {
+                    return 0;
+                }
                 switch (s[4]) {
                 case 'f': return GLSL;
                 case 'v': return GLSL;

src/lookup/ignored-exts.c
@@ -40,6 +40,9 @@ static bool is_ignored_extension(const char *s, size_t len)
     case 7:
         switch (s[0]) {
         case 'p':
+            if (memcmp(s + 1, "ac", 2)) {
+                return false;
+            }
             switch (s[3]) {
             case 'o': CMP(7); // pacorig
             case 's': CMP(8); // pacsave

src/lookup/pathnames.c
@@ -16,6 +16,9 @@ static FileTypeEnum filetype_from_pathname(const char *s, size_t len)
     FileTypeEnum val;
     switch (len) {
     case 10:
+        if (memcmp(s, "/etc/", 5)) {
+            return 0;
+        }
         switch (s[5]) {
         case 'f': CMP(1); // /etc/fstab
         case 'h': CMP(2); // /etc/hosts

test/filetype.c
@@ -20,6 +20,7 @@ static void test_find_ft_filename(void)
         {"file.rb", "ruby"},
         {"/etc/hosts", "config"},
         {"/etc/fstab", "config"},
+        {"/var/fstab", NULL},
         {"/boot/grub/menu.lst", "config"},
         {"/etc/krb5.conf", "ini"},
         {"/etc/ld.so.conf", "config"},
@@ -36,6 +37,17 @@ static void test_find_ft_filename(void)
         {".gitmodules", "ini"},
         {".jshintrc", "json"},
         {".zshrc", "sh"},
+        {"Makefile.am", "make"},
+        {"Makefile.in", "make"},
+        {"Makefile.i_", NULL},
+        {"Makefile.a_", NULL},
+        {"Makefile._m", NULL},
+        {"M_______.am", NULL},
+        {"file.glslf", "glsl"},
+        {"file.glslv", "glsl"},
+        {"file.gl_lv", NULL},
+        {"file.gls_v", NULL},
+        {"file.gl__v", NULL},
         {"._", NULL},
         {".1234", NULL},
         {".12345", NULL},
@@ -44,7 +56,6 @@ static void test_find_ft_filename(void)
         {"", NULL},
         {"/", NULL},
         {"/etc../etc.c.old/c.old", NULL},
-        // Ignored extensions
         {"test.c.bak", "c"},
         {"test.c.new", "c"},
         {"test.c.old~", "c"},
@@ -57,11 +68,12 @@ static void test_find_ft_filename(void)
         {"test.c.rpmnew", "c"},
         {"test.c.rpmsave", "c"},
     };
+    const size_t start_line = __LINE__ - 1 - ARRAY_COUNT(tests);
     const StringView empty_line = STRING_VIEW_INIT;
     FOR_EACH_I(i, tests) {
         const struct ft_filename_test *t = &tests[i];
         const char *result = find_ft(t->filename, empty_line);
-        EXPECT_STREQ(result, t->expected_filetype);
+        IEXPECT_STREQ(start_line + i, result, t->expected_filetype);
     }
 }
 

test/test.h
@@ -17,6 +17,7 @@
 #define EXPECT_FALSE(x) EXPECT_EQ(x, 0)
 
 #define IEXPECT_EQ(line, a, b) expect_eq(__FILE__, line, a, b)
+#define IEXPECT_STREQ(line, s1, s2) expect_streq(__FILE__, line, s1, s2)
 
 extern unsigned int failed;
 