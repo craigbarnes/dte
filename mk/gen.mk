4d8ccf89fc2c0c72cf2968038ff448f43fbadb72
Craig Barnes <Cr@igBarn.es>
2018-09-28 08:56:07 +0100

    Remove all remaining traces of mk/lperf.lua
    Using trees of switch statements may run very fast, but the jump tables
    produced by the compiler bloat the binary a lot. The new binary search
    tables are more than fast enough and are considerably smaller and easier
    to maintain.

--- mk/gen.mk
+++ mk/gen.mk
@@ -1,15 +1,8 @@
 FETCH = curl -LSs -o $@
-LPERF = $(LUA) mk/lperf.lua
 LUA_DEP = $(filter build/lua/lua, $(LUA))
 UCD_FILES = .cache/UnicodeData.txt .cache/EastAsianWidth.txt
 
-LPERF_TARGETS = $(addprefix gen-lookup-, ignored-exts)
-
-gen: $(LPERF_TARGETS) gen-xterm-keys
-
-$(LPERF_TARGETS): gen-lookup-%: $(LUA_DEP)
-	$(E) GEN src/lookup/$*.c
-	$(Q) $(LPERF) src/lookup/$*.lua src/lookup/$*.c
+gen: gen-xterm-keys
 
 gen-xterm-keys: $(LUA_DEP)
 	$(E) GEN src/terminal/xterm-keys.c
@@ -27,4 +20,4 @@ $(UCD_FILES): | .cache/
 	@mkdir -p $@
 
 
-.PHONY: gen gen-xterm-keys gen-wcwidth $(LPERF_TARGETS)
+.PHONY: gen gen-xterm-keys gen-wcwidth
--- src/lookup/ignored-exts.c
+++ src/lookup/ignored-exts.c
@@ -1,65 +1,33 @@
-static const struct {
-    const char key[16];
-    const bool val;
-} is_ignored_extension_table[11] = {
-    {"bak", true},
-    {"dpkg-dist", true},
-    {"dpkg-old", true},
-    {"new", true},
-    {"old", true},
-    {"orig", true},
-    {"pacnew", true},
-    {"pacorig", true},
-    {"pacsave", true},
-    {"rpmnew", true},
-    {"rpmsave", true},
-};
-
-#define CMP(i) idx = i; goto compare
-#define CMPN(i) idx = i; goto compare_last_char
-#define KEY is_ignored_extension_table[idx].key
-#define VAL is_ignored_extension_table[idx].val
-
 static bool is_ignored_extension(const char *s, size_t len)
 {
-    size_t idx;
     switch (len) {
     case 3:
         switch (s[0]) {
-        case 'b': CMP(0); // bak
-        case 'n': CMP(3); // new
-        case 'o': CMP(4); // old
+        case 'b': return !memcmp(s, "bak", len);
+        case 'n': return !memcmp(s, "new", len);
+        case 'o': return !memcmp(s, "old", len);
         }
         break;
-    case 4: CMP(5); // orig
+    case 4: return !memcmp(s, "orig", len);
     case 6:
         switch (s[0]) {
-        case 'p': CMP(6); // pacnew
-        case 'r': CMP(9); // rpmnew
+        case 'p': return !memcmp(s, "pacnew", len);
+        case 'r': return !memcmp(s, "rpmnew", len);
         }
         break;
     case 7:
         switch (s[0]) {
+        case 'r': return !memcmp(s, "rpmsave", len);
         case 'p':
             switch (s[3]) {
-            case 'o': CMP(7); // pacorig
-            case 's': CMP(8); // pacsave
+            case 'o': return !memcmp(s, "pacorig", len);
+            case 's': return !memcmp(s, "pacsave", len);
             }
             break;
-        case 'r': CMP(10); // rpmsave
         }
         break;
-    case 8: CMP(2); // dpkg-old
-    case 9: CMP(1); // dpkg-dist
+    case 8: return !memcmp(s, "dpkg-old", len);
+    case 9: return !memcmp(s, "dpkg-dist", len);
     }
     return false;
-compare:
-    return (memcmp(s, KEY, len) == 0) ? VAL : false;
-compare_last_char:
-    return (s[len - 1] == KEY[len - 1]) ? VAL : false;
 }
-
-#undef CMP
-#undef CMPN
-#undef KEY
-#undef VAL
