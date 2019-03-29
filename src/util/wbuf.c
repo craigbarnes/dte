91841c49dbd5faa18cd19eb4d83453b4439b4fcb
Craig Barnes <Cr@igBarn.es>
2019-03-29 04:33:38 +0000

    Optimize fast path of wbuf_write() function slightly
    If the number of bytes to be written is larger than the buffer,
    it's always going to be larger than the available space -- so the
    two conditions can be nested. This means one less check to perform
    when the buffer has enough space and one less XFLUSH() in the
    generated code.

--- src/util/wbuf.c
+++ src/util/wbuf.c
@@ -32,14 +32,13 @@ ssize_t wbuf_write(WriteBuffer *wbuf, const char *buf, size_t count)
 {
     if (count > wbuf_avail(wbuf)) {
         XFLUSH(wbuf);
-    }
-    if (count >= sizeof(wbuf->buf)) {
-        XFLUSH(wbuf);
-        ssize_t rc = xwrite(wbuf->fd, buf, count);
-        if (unlikely(rc < 0)) {
-            return rc;
+        if (unlikely(count >= sizeof(wbuf->buf))) {
+            ssize_t rc = xwrite(wbuf->fd, buf, count);
+            if (unlikely(rc < 0)) {
+                return rc;
+            }
+            return 0;
         }
-        return 0;
     }
     memcpy(wbuf->buf + wbuf->fill, buf, count);
     wbuf->fill += count;
