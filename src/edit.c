craigbarnes/dte

commit d553c64f60a3ebffd0d1f8754b46f2363ebefcee

Clean up code in format_paragraph() by using a designated initializer

src/edit.c
@@ -796,13 +796,14 @@ void format_paragraph(int text_width)
     char *sel = block_iter_get_bytes(&view->cursor, len);
     size_t indent_width = get_indent_width(sel, len);
 
-    ParagraphFormatter pf;
-    string_init(&pf.buf);
-    pf.indent = make_indent(indent_width);
-    pf.indent_len = pf.indent ? strlen(pf.indent) : 0;
-    pf.indent_width = indent_width;
-    pf.cur_width = 0;
-    pf.text_width = text_width;
+    ParagraphFormatter pf = {
+        .buf = STRING_INIT,
+        .indent = make_indent(indent_width),
+        .indent_len = pf.indent ? strlen(pf.indent) : 0,
+        .indent_width = indent_width,
+        .cur_width = 0,
+        .text_width = text_width
+    };
 
     size_t i = 0;
     while (1) {

commit d06b6eed81bcffad747a7e93d2efd84abaf41323

Fix uninitialized read in format_paragraph() function
This bug was introduced in commit d553c64f60a3e.

src/edit.c
@@ -795,11 +795,12 @@ void format_paragraph(int text_width)
 
     char *sel = block_iter_get_bytes(&view->cursor, len);
     size_t indent_width = get_indent_width(sel, len);
+    char *indent = make_indent(indent_width);
 
     ParagraphFormatter pf = {
         .buf = STRING_INIT,
-        .indent = make_indent(indent_width),
-        .indent_len = pf.indent ? strlen(pf.indent) : 0,
+        .indent = indent,
+        .indent_len = indent ? strlen(indent) : 0,
         .indent_width = indent_width,
         .cur_width = 0,
         .text_width = text_width