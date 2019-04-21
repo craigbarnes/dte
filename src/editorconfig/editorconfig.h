4d06e9910d7abb2557da50cd56465eb8d6ec0fa2
Craig Barnes <Cr@igBarn.es>
2019-04-21 06:48:34 +0100

    Fix namespacing in src/editorconfig/editorconfig.h

--- src/editorconfig/editorconfig.h
+++ src/editorconfig/editorconfig.h
@@ -7,13 +7,13 @@ typedef enum {
     INDENT_STYLE_UNSPECIFIED,
     INDENT_STYLE_TAB,
     INDENT_STYLE_SPACE,
-} IndentStyle;
+} EditorConfigIndentStyle;
 
 typedef struct {
     unsigned int indent_size;
     unsigned int tab_width;
     unsigned int max_line_length;
-    IndentStyle indent_style;
+    EditorConfigIndentStyle indent_style;
     bool indent_size_is_tab;
 } EditorConfigOptions;
 
