373fe2377451e0532df353ecb73b2129789f7ea9
Craig Barnes <Cr@igBarn.es>
2019-04-22 01:33:16 +0100

    Minor style fixes in src/screen-status.c

--- src/screen-status.c
+++ src/screen-status.c
@@ -89,82 +89,82 @@ static void sf_format(Formatter *f, char *buf, size_t size, const char *format)
         if (ch != '%') {
             add_separator(f);
             add_ch(f, ch);
-        } else {
-            ch = *format++;
-            switch (ch) {
-            case 'f':
-                add_status_str(f, buffer_filename(v->buffer));
+            continue;
+        }
+        ch = *format++;
+        switch (ch) {
+        case 'f':
+            add_status_str(f, buffer_filename(v->buffer));
+            break;
+        case 'm':
+            if (buffer_modified(v->buffer)) {
+                add_status_str(f, "*");
+            }
+            break;
+        case 'r':
+            if (v->buffer->readonly) {
+                add_status_str(f, "RO");
+            }
+            break;
+        case 'y':
+            add_status_format(f, "%d", v->cy + 1);
+            break;
+        case 'Y':
+            add_status_format(f, "%zu", v->buffer->nl);
+            break;
+        case 'x':
+            add_status_format(f, "%d", v->cx_display + 1);
+            break;
+        case 'X':
+            add_status_format(f, "%d", v->cx_char + 1);
+            if (v->cx_display != v->cx_char) {
+                add_status_format(f, "-%d", v->cx_display + 1);
+            }
+            break;
+        case 'p':
+            add_status_pos(f);
+            break;
+        case 'E':
+            add_status_str(f, encoding_to_string(&v->buffer->encoding));
+            break;
+        case 'M': {
+            if (f->misc_status != NULL) {
+                add_status_str(f, f->misc_status);
+            }
+            break;
+        }
+        case 'n':
+            switch (v->buffer->newline) {
+            case NEWLINE_UNIX:
+                add_status_str(f, "LF");
                 break;
-            case 'm':
-                if (buffer_modified(v->buffer)) {
-                    add_status_str(f, "*");
-                }
-                break;
-            case 'r':
-                if (v->buffer->readonly) {
-                    add_status_str(f, "RO");
-                }
-                break;
-            case 'y':
-                add_status_format(f, "%d", v->cy + 1);
-                break;
-            case 'Y':
-                add_status_format(f, "%zu", v->buffer->nl);
-                break;
-            case 'x':
-                add_status_format(f, "%d", v->cx_display + 1);
-                break;
-            case 'X':
-                add_status_format(f, "%d", v->cx_char + 1);
-                if (v->cx_display != v->cx_char) {
-                    add_status_format(f, "-%d", v->cx_display + 1);
-                }
-                break;
-            case 'p':
-                add_status_pos(f);
-                break;
-            case 'E':
-                add_status_str(f, encoding_to_string(&v->buffer->encoding));
-                break;
-            case 'M': {
-                if (f->misc_status != NULL) {
-                    add_status_str(f, f->misc_status);
-                }
+            case NEWLINE_DOS:
+                add_status_str(f, "CRLF");
                 break;
             }
-            case 'n':
-                switch (v->buffer->newline) {
-                case NEWLINE_UNIX:
-                    add_status_str(f, "LF");
-                    break;
-                case NEWLINE_DOS:
-                    add_status_str(f, "CRLF");
-                    break;
+            break;
+        case 's':
+            f->separator = true;
+            break;
+        case 't':
+            add_status_str(f, v->buffer->options.filetype);
+            break;
+        case 'u':
+            if (got_char) {
+                if (u_is_unicode(u)) {
+                    add_status_format(f, "U+%04X", u);
+                } else {
+                    add_status_str(f, "Invalid");
                 }
-                break;
-            case 's':
-                f->separator = true;
-                break;
-            case 't':
-                add_status_str(f, v->buffer->options.filetype);
-                break;
-            case 'u':
-                if (got_char) {
-                    if (u_is_unicode(u)) {
-                        add_status_format(f, "U+%04X", u);
-                    } else {
-                        add_status_str(f, "Invalid");
-                    }
-                }
-                break;
-            case '%':
-                add_separator(f);
-                add_ch(f, ch);
-                break;
-            case '\0':
-                f->buf[f->pos] = '\0';
-                return;
             }
+            break;
+        case '%':
+            add_separator(f);
+            add_ch(f, ch);
+            break;
+        case '\0':
+            f->buf[f->pos] = '\0';
+            return;
         }
     }
     f->buf[f->pos] = '\0';
