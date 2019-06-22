201023da224bd483c41da5d8f03977c7cb9e1eb8
Craig Barnes <Cr@igBarn.es>
2019-06-22 03:02:02 +0100

    Minor coding style tweaks in src/util/list.h

--- src/util/list.h
+++ src/util/list.h
@@ -36,8 +36,8 @@ static inline void list_del(ListHead *entry)
 {
     entry->next->prev = entry->prev;
     entry->prev->next = entry->next;
-    entry->next = (void *)0x00100100;
-    entry->prev = (void *)0x00200200;
+    entry->next = (void*)0x00100100;
+    entry->prev = (void*)0x00200200;
 }
 
 static inline bool list_empty(const ListHead *const head)
@@ -47,15 +47,13 @@ static inline bool list_empty(const ListHead *const head)
 
 /**
  * container_of - cast a member of a structure out to the containing structure
- *
  * @ptr: the pointer to the member.
  * @type: the type of the container struct this is embedded in.
  * @member: the name of the member within the struct.
- *
  */
 #define container_of(ptr, type, member) __extension__ ({ \
-    const __typeof__( ((type *)0)->member ) *__mptr = (ptr); \
-    (type *)( (char *)__mptr - offsetof(type,member) ); \
+    const __typeof__(((type*)0)->member) *mptr = (ptr); \
+    (type*)((char*)mptr - offsetof(type, member)); \
 })
 
 /**
