craigbarnes/dte

commit 1437a76a211e1a64e5206be1d5aaa373e4b8d59e

Simplify Encoding type by using interned strings for encoding names

src/encoding/encoding.c
@@ -1,5 +1,8 @@
+#include <stdlib.h>
 #include "encoding.h"
 #include "../util/ascii.h"
+#include "../util/intern.h"
+#include "../util/xmalloc.h"
 
 static const char encoding_names[][16] = {
     [UTF8] = "UTF-8",
@@ -49,10 +52,28 @@ EncodingType lookup_encoding(const char *name)
     return UNKNOWN_ENCODING;
 }
 
-const char *encoding_type_to_string(EncodingType type)
+static const char *encoding_type_to_string(EncodingType type)
 {
     if (type < NR_ENCODING_TYPES && type != UNKNOWN_ENCODING) {
         return encoding_names[type];
     }
     return NULL;
+}
+
+Encoding encoding_from_name(const char *name)
+{
+    const EncodingType type = lookup_encoding(name);
+    const char *normalized_name;
+    if (type == UNKNOWN_ENCODING) {
+        char *upper = xstrdup_toupper(name);
+        normalized_name = str_intern(upper);
+        free(upper);
+    } else {
+        normalized_name = str_intern(encoding_type_to_string(type));
+    }
+
+    return (Encoding) {
+        .type = type,
+        .name = normalized_name
+    };
 }

src/encoding/encoding.h
@@ -1,9 +1,7 @@
 #ifndef ENCODING_ENCODING_H
 #define ENCODING_ENCODING_H
 
-#include <stdlib.h>
 #include "../util/macros.h"
-#include "../util/xmalloc.h"
 
 typedef enum {
     UTF8,
@@ -25,49 +23,11 @@ typedef enum {
 
 typedef struct {
     EncodingType type;
-    // If type is UNKNOWN_ENCODING, this is an encoding name compatible
-    // with iconv_open(3). Otherwise it's NULL and the name string can
-    // obtained using encoding_type_to_string(type).
-    char *name;
+    // An interned encoding name compatible with iconv_open(3)
+    const char *name;
 } Encoding;
 
+Encoding encoding_from_name(const char *name) NONNULL_ARGS;
 EncodingType lookup_encoding(const char *name) NONNULL_ARGS;
-const char *encoding_type_to_string(EncodingType type);
-
-NONNULL_ARGS
-static inline Encoding encoding_from_name(const char *name)
-{
-    const EncodingType type = lookup_encoding(name);
-    Encoding e = {
-        .type = type,
-        .name = (type == UNKNOWN_ENCODING) ? xstrdup_toupper(name) : NULL
-    };
-    return e;
-}
-
-NONNULL_ARGS
-static inline const char *encoding_to_string(const Encoding *e)
-{
-    if (e->type == UNKNOWN_ENCODING) {
-        return e->name;
-    } else {
-        return encoding_type_to_string(e->type);
-    }
-}
-
-NONNULL_ARGS
-static inline Encoding encoding_clone(const Encoding *e)
-{
-    Encoding clone = {
-        .type = e->type,
-        .name = (e->type == UNKNOWN_ENCODING) ? xstrdup(e->name) : NULL
-    };
-    return clone;
-}
-
-static inline void free_encoding(Encoding *e)
-{
-    free(e->name);
-}
 
 #endif

src/load-save.c
@@ -103,7 +103,7 @@ static int decode_and_add_blocks (
         size -= bom_len;
     }
 
-    FileDecoder *dec = new_file_decoder(encoding_to_string(&b->encoding), buf, size);
+    FileDecoder *dec = new_file_decoder(b->encoding.name, buf, size);
     if (dec == NULL) {
         return -1;
     }
@@ -134,7 +134,7 @@ static int decode_and_add_blocks (
         if (dec->encoding) {
             b->encoding = encoding_from_name(dec->encoding);
         } else {
-            b->encoding = encoding_clone(&editor.charset);
+            b->encoding = editor.charset;
         }
     }
 
@@ -265,7 +265,7 @@ int load_buffer(Buffer *b, bool must_exist, const char *filename)
     }
 
     if (b->encoding.type == ENCODING_AUTODETECT) {
-        b->encoding = encoding_clone(&editor.charset);
+        b->encoding = editor.charset;
     }
 
     return 0;

src/buffer.c
@@ -64,7 +64,7 @@ Buffer *buffer_new(const Encoding *encoding)
     b->newline = editor.options.newline;
 
     if (encoding) {
-        b->encoding = encoding_clone(encoding);
+        b->encoding = *encoding;
     } else {
         b->encoding.type = ENCODING_AUTODETECT;
     }
@@ -112,7 +112,6 @@ void free_buffer(Buffer *b)
     free(b->views.ptrs);
     free(b->display_filename);
     free(b->abs_filename);
-    free_encoding(&b->encoding);
     free(b);
 }
 

src/commands.c
@@ -683,7 +683,6 @@ static void cmd_open(const char *pf, char **args)
         pf++;
     }
 
-    bool dealloc_encoding = true;
     Encoding encoding = {
         .type = ENCODING_AUTODETECT,
         .name = NULL
@@ -715,10 +714,7 @@ static void cmd_open(const char *pf, char **args)
     if (!paths[0]) {
         window_open_new_file(window);
         if (requested_encoding) {
-            free_encoding(&buffer->encoding);
             buffer->encoding = encoding;
-            // buffer now owns encoding -- don't free it below
-            dealloc_encoding = false;
         }
     } else if (!paths[1]) {
         // Previous view is remembered when opening single file
@@ -729,9 +725,6 @@ static void cmd_open(const char *pf, char **args)
         window_open_files(window, paths, &encoding);
     }
 
-    if (dealloc_encoding) {
-        free_encoding(&encoding);
-    }
     if (use_glob) {
         globfree(&globbuf);
     }
@@ -1012,7 +1005,6 @@ static void cmd_save(const char *pf, char **args)
 {
     char *absolute = buffer->abs_filename;
     Encoding encoding = buffer->encoding;
-    bool encoding_alloced = false;
     const char *requested_encoding = NULL;
     bool force = false;
     bool prompt = false;
@@ -1051,7 +1043,6 @@ static void cmd_save(const char *pf, char **args)
             return;
         }
         encoding = encoding_from_name(requested_encoding);
-        encoding_alloced = true;
     }
 
     // The encoding_from_name() call above may have allocated memory,
@@ -1175,8 +1166,7 @@ static void cmd_save(const char *pf, char **args)
     buffer->saved_change = buffer->cur_change;
     buffer->readonly = false;
     buffer->newline = newline;
-    if (encoding_alloced) {
-        free_encoding(&buffer->encoding);
+    if (requested_encoding) {
         buffer->encoding = encoding;
     }
 
@@ -1209,9 +1199,6 @@ error:
     }
     if (absolute != buffer->abs_filename) {
         free(absolute);
-    }
-    if (encoding_alloced) {
-        free_encoding(&encoding);
     }
 }
 

src/encoding/decoder.c
@@ -2,6 +2,7 @@
 #include "decoder.h"
 #include "convert.h"
 #include "../editor.h"
+#include "../util/intern.h"
 #include "../util/utf8.h"
 #include "../util/xmalloc.h"
 
@@ -93,7 +94,7 @@ static int set_encoding(FileDecoder *dec, const char *encoding)
         }
         dec->read_line = decode_and_read_line;
     }
-    dec->encoding = xstrdup(encoding);
+    dec->encoding = str_intern(encoding);
     return 0;
 }
 
@@ -118,7 +119,7 @@ static bool detect(FileDecoder *dec, const unsigned char *line, ssize_t len)
                 }
             } else {
                 // Assume encoding is same as locale
-                encoding = encoding_to_string(&editor.charset);
+                encoding = editor.charset.name;
             }
             if (set_encoding(dec, encoding)) {
                 // FIXME: error message?
@@ -187,7 +188,6 @@ void free_file_decoder(FileDecoder *dec)
     if (dec->cconv != NULL) {
         cconv_free(dec->cconv);
     }
-    free(dec->encoding);
     free(dec);
 }
 

src/encoding/decoder.h
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 
 typedef struct FileDecoder {
-    char *encoding;
+    const char *encoding;
     const unsigned char *ibuf;
     ssize_t ipos, isize;
     struct cconv *cconv;

src/encoding/encoder.c
@@ -14,7 +14,7 @@ FileEncoder *new_file_encoder(const Encoding *encoding, LineEndingType nls, int 
     enc->fd = fd;
 
     if (encoding->type == UTF8) {
-        enc->cconv = cconv_from_utf8(encoding_to_string(encoding));
+        enc->cconv = cconv_from_utf8(encoding->name);
         if (enc->cconv == NULL) {
             free(enc);
             return NULL;

commit c44ebbf397e29b3c5ff7e91cd441bae26b9be4a3

Set Encoding::name properly when file encoding is detected from BOM
This fixes a NULL pointer dereference bug introduced in commit
1437a76a211e1a64e5206be1d5aaa373e4b8d59e.

src/encoding/encoding.c
@@ -52,10 +52,10 @@ EncodingType lookup_encoding(const char *name)
     return UNKNOWN_ENCODING;
 }
 
-static const char *encoding_type_to_string(EncodingType type)
+const char *encoding_type_to_string(EncodingType type)
 {
     if (type < NR_ENCODING_TYPES && type != UNKNOWN_ENCODING) {
-        return encoding_names[type];
+        return str_intern(encoding_names[type]);
     }
     return NULL;
 }
@@ -69,7 +69,7 @@ Encoding encoding_from_name(const char *name)
         normalized_name = str_intern(upper);
         free(upper);
     } else {
-        normalized_name = str_intern(encoding_type_to_string(type));
+        normalized_name = encoding_type_to_string(type);
     }
 
     return (Encoding) {

src/encoding/encoding.h
@@ -27,6 +27,7 @@ typedef struct {
     const char *name;
 } Encoding;
 
+const char *encoding_type_to_string(EncodingType type);
 Encoding encoding_from_name(const char *name) NONNULL_ARGS;
 EncodingType lookup_encoding(const char *name) NONNULL_ARGS;
 

src/load-save.c
@@ -136,6 +136,8 @@ static int decode_and_add_blocks (
         } else {
             b->encoding = editor.charset;
         }
+    } else {
+        b->encoding.name = encoding_type_to_string(b->encoding.type);
     }
 
     free_file_decoder(dec);