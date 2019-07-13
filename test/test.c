32273a9ce3a9895bf5a8cca1b0609dde208722c8
Craig Barnes <Cr@igBarn.es>
2019-07-13 19:07:10 +0100

    Use "%jd" instead of PRIdMAX format specifier in test/test.c

--- test/test.c
+++ test/test.c
@@ -22,13 +22,9 @@ static void test_fail(const char *file, int line, const char *format, ...)
 void expect_streq(const char *file, int line, const char *s1, const char *s2)
 {
     if (unlikely(!xstreq(s1, s2))) {
-        test_fail (
-            file,
-            line,
-            "Strings not equal: '%s', '%s'",
-            s1 ? s1 : "(null)",
-            s2 ? s2 : "(null)"
-        );
+        s1 = s1 ? s1 : "(null)";
+        s2 = s2 ? s2 : "(null)";
+        test_fail(file, line, "Strings not equal: '%s', '%s'", s1, s2);
     }
 }
 
@@ -42,26 +38,14 @@ void expect_ptreq(const char *file, int line, const void *p1, const void *p2)
 void expect_eq(const char *file, int line, intmax_t a, intmax_t b)
 {
     if (unlikely(a != b)) {
-        test_fail (
-            file,
-            line,
-            "Values not equal: %" PRIdMAX ", %" PRIdMAX,
-            a,
-            b
-        );
+        test_fail(file, line, "Values not equal: %jd, %jd", a, b);
     }
 }
 
 void expect_uint_eq(const char *file, int line, uintmax_t a, uintmax_t b)
 {
     if (unlikely(a != b)) {
-        test_fail (
-            file,
-            line,
-            "Values not equal: %" PRIuMAX ", %" PRIuMAX,
-            a,
-            b
-        );
+        test_fail(file, line, "Values not equal: %ju, %ju", a, b);
     }
 }
 
@@ -82,41 +66,25 @@ void expect_nonnull(const char *file, int line, const void *ptr)
 void iexpect_streq(const char *file, int line, size_t i, const char *s1, const char *s2)
 {
     if (unlikely(!xstreq(s1, s2))) {
-        test_fail (
-            file,
-            line,
-            "Test #%zu: strings not equal: '%s', '%s'",
-            i + 1,
-            s1 ? s1 : "(null)",
-            s2 ? s2 : "(null)"
-        );
+        s1 = s1 ? s1 : "(null)";
+        s2 = s2 ? s2 : "(null)";
+        i++;
+        test_fail(file, line, "Test #%zu: strings not equal: '%s', '%s'", i, s1, s2);
     }
 }
 
 void iexpect_eq(const char *file, int line, size_t i, intmax_t a, intmax_t b)
 {
     if (unlikely(a != b)) {
-        test_fail (
-            file,
-            line,
-            "Test #%zu: values not equal: %" PRIdMAX ", %" PRIdMAX,
-            i + 1,
-            a,
-            b
-        );
+        i++;
+        test_fail(file, line, "Test #%zu: values not equal: %jd, %jd", i, a, b);
     }
 }
 
 void assert_eq(const char *file, int line, intmax_t a, intmax_t b)
 {
     if (unlikely(a != b)) {
-        test_fail (
-            file,
-            line,
-            "ERROR: Values not equal: %" PRIdMAX ", %" PRIdMAX,
-            a,
-            b
-        );
+        test_fail(file, line, "ERROR: Values not equal: %jd, %jd", a, b);
         abort();
     }
 }
