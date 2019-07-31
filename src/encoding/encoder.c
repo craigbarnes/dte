craigbarnes/dte

commit a5488f1416e1ccc8da0b5e428ee4c4e6d3b3fb11

Convert some functions to take Encoding* arguments instead of strings

src/encoding/encoder.c
@@ -6,15 +6,15 @@
 #include "../util/xmalloc.h"
 #include "../util/xreadwrite.h"
 
-FileEncoder *new_file_encoder(const char *encoding, LineEndingType nls, int fd)
+FileEncoder *new_file_encoder(const Encoding *encoding, LineEndingType nls, int fd)
 {
     FileEncoder *enc = xnew0(FileEncoder, 1);
 
     enc->nls = nls;
     enc->fd = fd;
 
-    if (strcmp(encoding, "UTF-8") != 0) {
-        enc->cconv = cconv_from_utf8(encoding);
+    if (encoding->type == UTF8) {
+        enc->cconv = cconv_from_utf8(encoding_to_string(encoding));
         if (enc->cconv == NULL) {
             free(enc);
             return NULL;

src/buffer.c
@@ -49,7 +49,7 @@ const char *buffer_filename(const Buffer *b)
     return b->display_filename;
 }
 
-Buffer *buffer_new(const char *encoding)
+Buffer *buffer_new(const Encoding *encoding)
 {
     static int id;
 
@@ -59,8 +59,9 @@ Buffer *buffer_new(const char *encoding)
     b->saved_change = &b->change_head;
     b->id = ++id;
     b->newline = editor.options.newline;
+
     if (encoding) {
-        b->encoding = encoding_from_name(encoding);
+        b->encoding = encoding_clone(encoding);
     } else {
         b->encoding.type = ENCODING_AUTODETECT;
     }
@@ -76,7 +77,7 @@ Buffer *buffer_new(const char *encoding)
 
 Buffer *open_empty_buffer(void)
 {
-    Buffer *b = buffer_new(editor.charset);
+    Buffer *b = buffer_new(&editor.charset);
 
     // At least one block required
     Block *blk = block_new(1);

src/buffer.h
@@ -80,7 +80,7 @@ void update_short_filename_cwd(Buffer *b, const char *cwd);
 void update_short_filename(Buffer *b);
 Buffer *find_buffer(const char *abs_filename);
 Buffer *find_buffer_by_id(unsigned int id);
-Buffer *buffer_new(const char *encoding);
+Buffer *buffer_new(const Encoding *encoding);
 Buffer *open_empty_buffer(void);
 void free_buffer(Buffer *b);
 bool buffer_detect_filetype(Buffer *b);

src/commands.c
@@ -715,11 +715,11 @@ static void cmd_open(const char *pf, char **args)
         }
     } else if (!paths[1]) {
         // Previous view is remembered when opening single file
-        window_open_file(window, paths[0], encoding_to_string(&encoding));
+        window_open_file(window, paths[0], &encoding);
     } else {
         // It makes no sense to remember previous view when opening
         // multiple files
-        window_open_files(window, paths, encoding_to_string(&encoding));
+        window_open_files(window, paths, &encoding);
     }
 
     if (dealloc_encoding) {
@@ -1147,7 +1147,7 @@ static void cmd_save(const char *pf, char **args)
         // Allow chmod 755 etc.
         buffer->st.st_mode = st.st_mode;
     }
-    if (save_buffer(buffer, absolute, encoding_to_string(&encoding), newline)) {
+    if (save_buffer(buffer, absolute, &encoding, newline)) {
         goto error;
     }
 

src/editor.c
@@ -94,8 +94,8 @@ void init_editor_state(void)
     }
 
     setlocale(LC_CTYPE, "");
-    editor.charset = nl_langinfo(CODESET);
-    editor.term_utf8 = streq(editor.charset, "UTF-8");
+    editor.charset = encoding_from_name(nl_langinfo(CODESET));
+    editor.term_utf8 = (editor.charset.type == UTF8);
 
     editor.options.statusline_left = xstrdup(" %f%s%m%r%s%M");
     editor.options.statusline_right = xstrdup(" %y,%X   %u   %E %n %t   %p ");

src/editor.h
@@ -5,6 +5,7 @@
 #include "cmdline.h"
 #include "mode.h"
 #include "options.h"
+#include "encoding/encoding.h"
 #include "terminal/color.h"
 #include "util/macros.h"
 #include "util/ptr-array.h"
@@ -30,7 +31,7 @@ typedef struct {
     GlobalOptions options;
     const char *home_dir;
     const char *user_config_dir;
-    const char *charset;
+    Encoding charset;
     const char *pager;
     bool child_controls_terminal;
     bool everything_changed;

src/encoding/decoder.c
@@ -46,7 +46,7 @@ static bool detect(FileDecoder *dec, const unsigned char *line, ssize_t len)
                 encoding = "ISO-8859-1";
             } else {
                 // Assuming locale's encoding
-                encoding = editor.charset;
+                encoding = encoding_to_string(&editor.charset);
             }
             if (set_encoding(dec, encoding)) {
                 // FIXME: error message?

src/encoding/encoder.h
@@ -2,6 +2,8 @@
 #define ENCODING_ENCODER_H
 
 #include <sys/types.h>
+#include "../util/macros.h"
+#include "../encoding/encoding.h"
 
 typedef enum {
     NEWLINE_UNIX,
@@ -16,8 +18,8 @@ typedef struct {
     int fd;
 } FileEncoder;
 
-FileEncoder *new_file_encoder(const char *encoding, LineEndingType nls, int fd);
+FileEncoder *new_file_encoder(const Encoding *encoding, LineEndingType nls, int fd) NONNULL_ARGS;
 void free_file_encoder(FileEncoder *enc);
-ssize_t file_encoder_write(FileEncoder *enc, const unsigned char *buf, ssize_t size);
+ssize_t file_encoder_write(FileEncoder *enc, const unsigned char *buf, ssize_t size) NONNULL_ARGS;
 
 #endif

commit c7752942d354f4b0c7e44ae78f1b05e8f0970a2c

Fix inverted logic bug in new_file_encoder() function
This was causing all files to be saved as UTF-8, even when a
different encoding was requested.

The bug was originally introduced in commit
a5488f1416e1ccc8da0b5e428ee4c4e6d3b3fb11.

src/encoding/encoder.c
@@ -9,17 +9,17 @@
 FileEncoder *new_file_encoder(const Encoding *encoding, LineEndingType nls, int fd)
 {
     FileEncoder *enc = xnew0(FileEncoder, 1);
-
     enc->nls = nls;
     enc->fd = fd;
 
-    if (encoding->type == UTF8) {
+    if (encoding->type != UTF8) {
         enc->cconv = cconv_from_utf8(encoding->name);
         if (enc->cconv == NULL) {
             free(enc);
             return NULL;
         }
     }
+
     return enc;
 }
 
@@ -37,13 +37,12 @@ static ssize_t unix_to_dos (
     const unsigned char *buf,
     ssize_t size
 ) {
-    ssize_t s, d;
-
     if (enc->nsize < size * 2) {
         enc->nsize = size * 2;
         xrenew(enc->nbuf, enc->nsize);
     }
 
+    ssize_t s, d;
     for (s = 0, d = 0; s < size; s++) {
         unsigned char ch = buf[s];
         if (ch == '\n') {
@@ -51,6 +50,7 @@ static ssize_t unix_to_dos (
         }
         enc->nbuf[d++] = ch;
     }
+
     return d;
 }
 