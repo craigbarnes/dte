9ab56f6a4b1d0a0719d840d1383a168b5769bfbb
Craig Barnes <Cr@igBarn.es>
2019-09-17 23:22:34 +0100

    More fixes for previous commit
    * IEXPECT_EQ() shouldn't be used with an int i argument
    * isascii() isn't available on some systems

--- test/util.c
+++ test/util.c
@@ -216,10 +216,9 @@ static void test_ascii(void)
         EXPECT_EQ(!!ascii_isdigit(i), !!isdigit(i));
         EXPECT_EQ(!!ascii_isblank(i), !!isblank(i));
         EXPECT_EQ(!!ascii_isprint(i), !!isprint(i));
-        EXPECT_EQ(!!ascii_isascii(i), !!isascii(i));
         EXPECT_EQ(!!ascii_isxdigit(i), !!isxdigit(i));
         if (i != '\v' && i != '\f') {
-            IEXPECT_EQ(!!ascii_isspace(i), !!isspace(i));
+            EXPECT_EQ(!!ascii_isspace(i), !!isspace(i));
         }
     }
     setlocale(LC_CTYPE, saved_locale);
