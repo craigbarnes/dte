c94b773a4b6c289a98016170e7c95e0e31c89ef7
Craig Barnes <Cr@igBarn.es>
2019-10-05 05:38:30 +0100

    Use lookup table instead of switch in command_parse_error_to_string()

--- src/command-parse.c
+++ src/command-parse.c
@@ -276,15 +276,12 @@ bool parse_commands(PointerArray *array, const char *cmd, CommandParseError *err
 
 const char *command_parse_error_to_string(CommandParseError err)
 {
-    switch (err) {
-    case CMDERR_UNCLOSED_SINGLE_QUOTE:
-        return "Missing '";
-    case CMDERR_UNCLOSED_DOUBLE_QUOTE:
-        return "Missing \"";
-    case CMDERR_UNEXPECTED_EOF:
-        return "Unexpected EOF";
-    case CMDERR_NONE:
-        break;
-    }
-    return NULL;
+    static const char error_strings[][16] = {
+        [CMDERR_UNCLOSED_SINGLE_QUOTE] = "Missing '",
+        [CMDERR_UNCLOSED_DOUBLE_QUOTE] = "Missing \"",
+        [CMDERR_UNEXPECTED_EOF] = "Unexpected EOF",
+    };
+    BUG_ON(err <= CMDERR_NONE);
+    BUG_ON(err >= ARRAY_COUNT(error_strings));
+    return error_strings[err];
 }
