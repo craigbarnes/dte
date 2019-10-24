f4ec44fd0ee0f19d88939f469641ad9f6eb4d6e6
Craig Barnes <Cr@igBarn.es>
2019-10-24 05:01:07 +0100

    Add some unit tests for bitset_contains() function

--- test/syntax.c
+++ test/syntax.c
@@ -2,20 +2,77 @@
 #include "test.h"
 #include "../src/syntax/bitset.h"
 
-static void test_bitset_invert(void)
+static void test_bitset(void)
 {
-    BitSet set = {0};
+    BitSet set;
     ASSERT_EQ(sizeof(set), 32);
     ASSERT_EQ(ARRAY_COUNT(set), 32);
 
     memset(set, 0, sizeof(set));
-    for (size_t i = 0; i < ARRAY_COUNT(set); i++) {
-        EXPECT_UINT_EQ(set[i], 0);
-    }
+    EXPECT_FALSE(bitset_contains(set, '0'));
+    EXPECT_FALSE(bitset_contains(set, 'a'));
+    EXPECT_FALSE(bitset_contains(set, 'z'));
+    EXPECT_FALSE(bitset_contains(set, '!'));
+    EXPECT_FALSE(bitset_contains(set, '\0'));
+
+    bitset_add_pattern(set, "0-9a-fxy");
+    EXPECT_TRUE(bitset_contains(set, '0'));
+    EXPECT_TRUE(bitset_contains(set, '8'));
+    EXPECT_TRUE(bitset_contains(set, '9'));
+    EXPECT_TRUE(bitset_contains(set, 'a'));
+    EXPECT_TRUE(bitset_contains(set, 'b'));
+    EXPECT_TRUE(bitset_contains(set, 'f'));
+    EXPECT_TRUE(bitset_contains(set, 'x'));
+    EXPECT_TRUE(bitset_contains(set, 'y'));
+    EXPECT_FALSE(bitset_contains(set, 'g'));
+    EXPECT_FALSE(bitset_contains(set, 'z'));
+    EXPECT_FALSE(bitset_contains(set, 'A'));
+    EXPECT_FALSE(bitset_contains(set, 'F'));
+    EXPECT_FALSE(bitset_contains(set, 'X'));
+    EXPECT_FALSE(bitset_contains(set, 'Z'));
+    EXPECT_FALSE(bitset_contains(set, '{'));
+    EXPECT_FALSE(bitset_contains(set, '`'));
+    EXPECT_FALSE(bitset_contains(set, '/'));
+    EXPECT_FALSE(bitset_contains(set, ':'));
+    EXPECT_FALSE(bitset_contains(set, '\0'));
 
     bitset_invert(set);
-    for (size_t i = 0; i < ARRAY_COUNT(set); i++) {
-        EXPECT_UINT_EQ(set[i], UINT8_MAX);
+    EXPECT_FALSE(bitset_contains(set, '0'));
+    EXPECT_FALSE(bitset_contains(set, '8'));
+    EXPECT_FALSE(bitset_contains(set, '9'));
+    EXPECT_FALSE(bitset_contains(set, 'a'));
+    EXPECT_FALSE(bitset_contains(set, 'b'));
+    EXPECT_FALSE(bitset_contains(set, 'f'));
+    EXPECT_FALSE(bitset_contains(set, 'x'));
+    EXPECT_FALSE(bitset_contains(set, 'y'));
+    EXPECT_TRUE(bitset_contains(set, 'g'));
+    EXPECT_TRUE(bitset_contains(set, 'z'));
+    EXPECT_TRUE(bitset_contains(set, 'A'));
+    EXPECT_TRUE(bitset_contains(set, 'F'));
+    EXPECT_TRUE(bitset_contains(set, 'X'));
+    EXPECT_TRUE(bitset_contains(set, 'Z'));
+    EXPECT_TRUE(bitset_contains(set, '{'));
+    EXPECT_TRUE(bitset_contains(set, '`'));
+    EXPECT_TRUE(bitset_contains(set, '/'));
+    EXPECT_TRUE(bitset_contains(set, ':'));
+    EXPECT_TRUE(bitset_contains(set, '\0'));
+
+    memset(set, 0, sizeof(set));
+    FOR_EACH_I(i, set) {
+        EXPECT_UINT_EQ(set[i], 0);
+    }
+    bitset_invert(set);
+    FOR_EACH_I(i, set) {
+        EXPECT_UINT_EQ(set[i], 255);
+    }
+
+    memset(set, 170, sizeof(set));
+    FOR_EACH_I(i, set) {
+        EXPECT_UINT_EQ(set[i], 170);
+    }
+    bitset_invert(set);
+    FOR_EACH_I(i, set) {
+        EXPECT_UINT_EQ(set[i], 85);
     }
 }
 
@@ -23,5 +80,5 @@ DISABLE_WARNING("-Wmissing-prototypes")
 
 void test_syntax(void)
 {
-    test_bitset_invert();
+    test_bitset();
 }
