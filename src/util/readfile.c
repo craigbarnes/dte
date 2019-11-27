1fc14a3dc494db85087149ef4ca5be429de818ac
Craig Barnes <Cr@igBarn.es>
2019-11-27 12:23:55 +0000

    Remove unused stat_read_file() function

--- src/util/readfile.c
+++ src/util/readfile.c
@@ -2,30 +2,34 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include "readfile.h"
 #include "xmalloc.h"
 #include "xreadwrite.h"
 #include "../debug.h"
 
-ssize_t stat_read_file(const char *filename, char **bufp, struct stat *st)
+ssize_t read_file(const char *filename, char **bufp)
 {
-    int fd = open(filename, O_RDONLY);
     *bufp = NULL;
+    int fd = open(filename, O_RDONLY);
     if (fd == -1) {
         return -1;
     }
-    if (fstat(fd, st) == -1) {
+
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
         close(fd);
         return -1;
     }
-    if (S_ISDIR(st->st_mode)) {
+    if (S_ISDIR(st.st_mode)) {
         close(fd);
         errno = EISDIR;
         return -1;
     }
-    char *buf = xmalloc(st->st_size + 1);
-    ssize_t r = xread(fd, buf, st->st_size);
+
+    char *buf = xmalloc(st.st_size + 1);
+    ssize_t r = xread(fd, buf, st.st_size);
     close(fd);
     if (r > 0) {
         buf[r] = '\0';
