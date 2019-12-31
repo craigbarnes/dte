db72bc1057d63ff1d7e697b6e0b0b41e28378214
Craig Barnes <Cr@igBarn.es>
2019-12-31 00:12:09 +0000

    Add more unit tests for detect_encoding_from_bom() function

--- test/encoding.c
+++ test/encoding.c
@@ -9,13 +9,23 @@ static void test_detect_encoding_from_bom(void)
         const unsigned char *text;
         size_t size;
     } tests[] = {
-        {UTF8, "\xef\xbb\xbfHello", 8},
-        {UTF32BE, "\x00\x00\xfe\xffHello", 9},
-        {UTF32LE, "\xff\xfe\x00\x00Hello", 9},
-        {UTF16BE, "\xfe\xffHello", 7},
-        {UTF16LE, "\xff\xfeHello", 7},
-        {UNKNOWN_ENCODING, "\x00\xef\xbb\xbfHello", 9},
-        {UNKNOWN_ENCODING, "\xef\xbb", 2},
+        {UTF8, STRN("\xef\xbb\xbfHello")},
+        {UTF32BE, STRN("\x00\x00\xfe\xffHello")},
+        {UTF32LE, STRN("\xff\xfe\x00\x00Hello")},
+        {UTF16BE, STRN("\xfe\xffHello")},
+        {UTF16LE, STRN("\xff\xfeHello")},
+        {UTF16LE, STRN("\xff\xfe")},
+        {UNKNOWN_ENCODING, STRN("\xff\xff\x00\x00Hello")},
+        {UNKNOWN_ENCODING, STRN("\x00\xef\xbb\xbfHello")},
+        {UNKNOWN_ENCODING, STRN("\xef\xbb")},
+        {UNKNOWN_ENCODING, STRN("\xef\xbb#")},
+        {UNKNOWN_ENCODING, STRN("\xee\xbb\xbf")},
+        {UNKNOWN_ENCODING, STRN("\xff\xbb\xbf")},
+        {UNKNOWN_ENCODING, STRN("\xbf\xbb\xef")},
+        {UNKNOWN_ENCODING, STRN("\x00\x00\xfe")},
+        {UNKNOWN_ENCODING, STRN("\x00")},
+        {UNKNOWN_ENCODING, "", 0},
+        {UNKNOWN_ENCODING, NULL, 0},
     };
     FOR_EACH_I(i, tests) {
         const struct bom_test *t = &tests[i];
