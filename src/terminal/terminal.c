craigbarnes/dte

commit 23daf151dc4ba9d32eeb3c45a215ed44f8c5423d

Clean up terminal mode setting code

src/terminal/terminal.c
@@ -1,8 +1,11 @@
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "terminal.h"
 #include "ecma48.h"
+#include "mode.h"
 #include "terminfo.h"
 #include "xterm.h"
 #include "../debug.h"
@@ -87,6 +90,10 @@ void term_init(void)
         } else {
             term_init_fail("'DTE_FORCE_TERMINFO' set but terminfo not linked");
         }
+    }
+
+    if (!term_mode_init()) {
+        term_init_fail("tcgetattr: %s", strerror(errno));
     }
 
     switch (get_term_type(term)) {

src/editor.c
@@ -13,6 +13,7 @@
 #include "screen.h"
 #include "search.h"
 #include "terminal/input.h"
+#include "terminal/mode.h"
 #include "terminal/output.h"
 #include "terminal/terminal.h"
 #include "util/ascii.h"
@@ -282,7 +283,7 @@ static void ui_end(void)
     terminal.put_control_code(terminal.control_codes.keypad_off);
 
     term_output_flush();
-    terminal.cooked();
+    term_cooked();
 }
 
 void suspend(void)

src/main.c
@@ -18,6 +18,7 @@
 #include "syntax/state.h"
 #include "syntax/syntax.h"
 #include "terminal/input.h"
+#include "terminal/mode.h"
 #include "terminal/output.h"
 #include "terminal/terminal.h"
 #include "util/str-util.h"
@@ -38,7 +39,7 @@ static void handle_sigcont(int UNUSED_ARG(signum))
         !editor.child_controls_terminal
         && editor.status != EDITOR_INITIALIZING
     ) {
-        terminal.raw();
+        term_raw();
         editor.resize();
     }
 }
@@ -130,7 +131,7 @@ static int dump_builtin_config(const char *const name)
 
 static void showkey_loop(void)
 {
-    terminal.raw();
+    term_raw();
     terminal.put_control_code(terminal.control_codes.init);
     terminal.put_control_code(terminal.control_codes.keypad_on);
     term_add_literal("Press any key combination, or use Ctrl+D to exit\r\n");
@@ -159,7 +160,7 @@ static void showkey_loop(void)
     terminal.put_control_code(terminal.control_codes.keypad_off);
     terminal.put_control_code(terminal.control_codes.deinit);
     term_output_flush();
-    terminal.cooked();
+    term_cooked();
 }
 
 static const char usage[] =
@@ -337,7 +338,7 @@ loop_break:
     // Initialize terminal but don't update screen yet. Also display
     // "Press any key to continue" prompt if there were any errors
     // during reading configuration files.
-    terminal.raw();
+    term_raw();
     if (get_nr_errors()) {
         any_key();
         clear_error();

src/spawn.c
@@ -9,6 +9,7 @@
 #include "error.h"
 #include "msg.h"
 #include "regexp.h"
+#include "terminal/mode.h"
 #include "terminal/terminal.h"
 #include "util/exec.h"
 #include "util/string.h"
@@ -181,7 +182,7 @@ static void yield_terminal(void)
 
 static void resume_terminal(bool prompt)
 {
-    terminal.raw();
+    term_raw();
     if (prompt) {
         any_key();
     }

src/terminal/ecma48.c
@@ -83,8 +83,6 @@ Terminal terminal = {
     .color_type = TERM_8_COLOR,
     .width = 80,
     .height = 24,
-    .raw = &term_raw,
-    .cooked = &term_cooked,
     .parse_key_sequence = &xterm_parse_key,
     .put_control_code = &term_add_string_view,
     .clear_screen = &ecma48_clear_screen,

src/terminal/mode.c
@@ -1,19 +1,36 @@
 #include <errno.h>
-#include <stdbool.h>
 #include <termios.h>
 #include <unistd.h>
 #include "mode.h"
 #include "../debug.h"
 #include "../util/macros.h"
 
-static struct termios termios_save;
+static bool initialized;
+static struct termios raw, cooked;
 
-static void xtcgetattr(struct termios *t)
+bool term_mode_init(void)
 {
-    int ret = tcgetattr(STDIN_FILENO, t);
-    if (unlikely(ret != 0)) {
-        fatal_error("tcgetattr", errno);
+    BUG_ON(initialized);
+    if (unlikely(tcgetattr(STDIN_FILENO, &cooked) != 0)) {
+        return false;
     }
+
+    // Set up "raw" mode (roughly equivalent to cfmakeraw(3) on Linux/BSD)
+    raw = cooked;
+    raw.c_iflag &= ~(
+        ICRNL | IXON | IXOFF
+        | IGNBRK | BRKINT | PARMRK
+        | ISTRIP | INLCR | IGNCR
+    );
+    raw.c_oflag &= ~OPOST;
+    raw.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    raw.c_cflag &= ~(CSIZE | PARENB);
+    raw.c_cflag |= CS8;
+    raw.c_cc[VMIN] = 1;
+    raw.c_cc[VTIME] = 0;
+
+    initialized = true;
+    return true;
 }
 
 static void xtcsetattr(const struct termios *t)
@@ -30,35 +47,14 @@ static void xtcsetattr(const struct termios *t)
 
 void term_raw(void)
 {
-    static bool saved;
-    struct termios termios;
-    xtcgetattr(&termios);
-    if (!saved) {
-        termios_save = termios;
-        saved = true;
+    if (initialized) {
+        xtcsetattr(&raw);
     }
-
-    // Enter "raw" mode (roughly equivalent to cfmakeraw(3) on Linux/BSD)
-    termios.c_iflag &= ~(
-        ICRNL | IXON | IXOFF
-        | IGNBRK | BRKINT | PARMRK
-        | ISTRIP | INLCR | IGNCR
-    );
-    termios.c_oflag &= ~OPOST;
-    termios.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-    termios.c_cflag &= ~(CSIZE | PARENB);
-    termios.c_cflag |= CS8;
-
-    // Read at least 1 char on each read()
-    termios.c_cc[VMIN] = 1;
-
-    // Read blocks until there are MIN(VMIN, requested) bytes available
-    termios.c_cc[VTIME] = 0;
-
-    xtcsetattr(&termios);
 }
 
 void term_cooked(void)
 {
-    xtcsetattr(&termios_save);
+    if (initialized) {
+        xtcsetattr(&cooked);
+    }
 }

src/terminal/mode.h
@@ -1,6 +1,9 @@
 #ifndef TERMINAL_MODE_H
 #define TERMINAL_MODE_H
 
+#include <stdbool.h>
+
+bool term_mode_init(void);
 void term_raw(void);
 void term_cooked(void);
 

src/terminal/terminal.h
@@ -35,8 +35,6 @@ typedef struct {
     void (*set_color)(const TermColor *color);
     void (*move_cursor)(int x, int y);
     void (*repeat_byte)(char ch, size_t count);
-    void (*raw)(void);
-    void (*cooked)(void);
     void (*save_title)(void);
     void (*restore_title)(void);
     void (*set_title)(const char *title);

commit 9d6650e6672e77482f4643b539c9444442ba4fa1

Call term_mode_init() properly when DTE_FORCE_TERMINFO is set
This fixes a bug that was introduced in commit
23daf151dc4ba9d32eeb3c45a215ed44f8c5423d.

src/terminal/terminal.c
@@ -84,16 +84,16 @@ void term_init(void)
         term_init_fail("'TERM' not set");
     }
 
+    if (!term_mode_init()) {
+        term_init_fail("tcgetattr: %s", strerror(errno));
+    }
+
     if (getenv("DTE_FORCE_TERMINFO")) {
         if (term_init_terminfo(term)) {
             return;
         } else {
             term_init_fail("'DTE_FORCE_TERMINFO' set but terminfo not linked");
         }
-    }
-
-    if (!term_mode_init()) {
-        term_init_fail("tcgetattr: %s", strerror(errno));
     }
 
     switch (get_term_type(term)) {