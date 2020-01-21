craigbarnes/dte

commit 043413ca6401be0b527dd6cb247b3874bccca9b8

Clean up UI init/deinit/resize routines
* Avoid sending unnecessary smcup/smkx sequences on SIGWINCH
* Send xterm init/deinit sequences on suspend/resume terminal
* Allow disabling UI routines (e.g. for running tests) without
  using no-op function pointers

src/main.c
@@ -49,7 +49,7 @@ static void handle_sigcont(int UNUSED_ARG(signum))
         && editor.status != EDITOR_INITIALIZING
     ) {
         term_raw();
-        editor.resize();
+        ui_start();
     }
 }
 
@@ -412,10 +412,7 @@ loop_break:
     }
 
     set_view(window->views.ptrs[0]);
-
-    if (command || tag) {
-        editor.resize();
-    }
+    ui_start();
 
     if (command) {
         handle_command(&commands, command);
@@ -438,14 +435,10 @@ loop_break:
         remove_view(window->views.ptrs[0]);
     }
 
-    terminal.put_control_code(terminal.control_codes.init);
-    editor.resize();
-
     main_loop();
 
     terminal.restore_title();
-    editor.ui_end();
-    terminal.put_control_code(terminal.control_codes.deinit);
+    ui_end();
     term_output_flush();
 
     // Unlock files and add files to file history

src/debug.c
@@ -18,7 +18,7 @@ void term_cleanup(void)
         return;
     }
     if (!editor.child_controls_terminal) {
-        editor.ui_end();
+        ui_end();
     }
     if (terminal.control_codes.deinit.length) {
         xwrite (

src/editor.c
@@ -26,9 +26,6 @@
 
 static volatile sig_atomic_t terminal_resized;
 
-static void resize(void);
-static void ui_end(void);
-
 EditorState editor = {
     .status = EDITOR_INITIALIZING,
     .input_mode = INPUT_NORMAL,
@@ -39,8 +36,6 @@ EditorState editor = {
     .command_history = PTR_ARRAY_INIT,
     .version = version,
     .cmdline_x = 0,
-    .resize = resize,
-    .ui_end = ui_end,
     .cmdline = {
         .buf = STRING_INIT,
         .pos = 0,
@@ -253,29 +248,38 @@ void handle_sigwinch(int UNUSED_ARG(signum))
     terminal_resized = true;
 }
 
-static void resize(void)
+void ui_resize(void)
 {
+    if (editor.status == EDITOR_INITIALIZING) {
+        return;
+    }
     terminal_resized = false;
     update_screen_size();
-
-    // Turn keypad on (makes cursor keys work)
-    terminal.put_control_code(terminal.control_codes.keypad_on);
-
-    // Use alternate buffer if possible
-    terminal.put_control_code(terminal.control_codes.cup_mode_on);
-
     editor.mode_ops[editor.input_mode]->update();
 }
 
-static void ui_end(void)
+void ui_start(void)
 {
+    if (editor.status == EDITOR_INITIALIZING) {
+        return;
+    }
+    terminal.put_control_code(terminal.control_codes.init);
+    terminal.put_control_code(terminal.control_codes.keypad_on);
+    terminal.put_control_code(terminal.control_codes.cup_mode_on);
+    ui_resize();
+}
+
+void ui_end(void)
+{
+    if (editor.status == EDITOR_INITIALIZING) {
+        return;
+    }
     terminal.clear_screen();
     terminal.move_cursor(0, terminal.height - 1);
     term_show_cursor();
-
     terminal.put_control_code(terminal.control_codes.cup_mode_off);
     terminal.put_control_code(terminal.control_codes.keypad_off);
-
+    terminal.put_control_code(terminal.control_codes.deinit);
     term_output_flush();
     term_cooked();
 }
@@ -378,7 +382,7 @@ char dialog_prompt(const char *question, const char *choices)
             return (c >= 'a') ? c : 0;
         }
         if (terminal_resized) {
-            resize();
+            ui_resize();
             term_hide_cursor();
             show_dialog(question);
             show_message(question, false);
@@ -413,7 +417,7 @@ char status_prompt(const char *question, const char *choices)
             return (c >= 'a') ? c : 0;
         }
         if (terminal_resized) {
-            resize();
+            ui_resize();
             term_hide_cursor();
             show_message(question, false);
             restore_cursor();
@@ -481,7 +485,7 @@ void main_loop(void)
 {
     while (editor.status == EDITOR_RUNNING) {
         if (terminal_resized) {
-            resize();
+            ui_resize();
         }
 
         KeyCode key;

src/editor.h
@@ -39,8 +39,6 @@ typedef struct {
     PointerArray search_history;
     PointerArray command_history;
     const char *const version;
-    void (*resize)(void);
-    void (*ui_end)(void);
 } EditorState;
 
 extern EditorState editor;
@@ -64,5 +62,8 @@ void normal_update(void);
 void handle_sigwinch(int signum);
 void suspend(void);
 void main_loop(void);
+void ui_resize(void);
+void ui_start(void);
+void ui_end(void);
 
 #endif

src/spawn.c
@@ -177,7 +177,7 @@ static int handle_child_error(pid_t pid)
 static void yield_terminal(void)
 {
     editor.child_controls_terminal = true;
-    editor.ui_end();
+    ui_end();
 }
 
 static void resume_terminal(bool prompt)
@@ -186,7 +186,7 @@ static void resume_terminal(bool prompt)
     if (prompt) {
         any_key();
     }
-    editor.resize();
+    ui_start();
     editor.child_controls_terminal = false;
 }
 

test/config.c
@@ -117,8 +117,6 @@ void init_headless_mode(void)
     ;
 
     MEMZERO(&terminal.control_codes);
-    editor.resize = &no_op;
-    editor.ui_end = &no_op;
     exec_builtin_rc();
     window = new_window();
     root_frame = new_root_frame(window);

commit 29bcf20e5fada6efb0452982852a2ffe8859ab03

Update screen if -t and/or -c command-line options are used at startup
This fixes a bug that was introduced in commit 043413ca6401be0b.

src/main.c
@@ -442,6 +442,10 @@ loop_break:
         remove_view(window->views.ptrs[0]);
     }
 
+    if (command || tag) {
+        normal_update();
+    }
+
     main_loop();
 
     terminal.restore_title();