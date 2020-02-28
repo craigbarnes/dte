903e5b74ac152e608c04e5617e576a0aad8597ff
Craig Barnes <Cr@igBarn.es>
2020-02-28 21:31:05 +0000

    Remove GCC "const" attribute from u_is_unprintable() function
    It's no longer "const", since it was changed a while back to inspect
    the value of GlobalOptions::display_invisible.

--- src/util/unicode.h
+++ src/util/unicode.h
@@ -31,7 +31,7 @@ static inline CodePoint u_to_lower(CodePoint u)
 
 bool u_is_breakable_whitespace(CodePoint u) CONST_FN;
 bool u_is_word_char(CodePoint u) CONST_FN;
-bool u_is_unprintable(CodePoint u) CONST_FN;
+bool u_is_unprintable(CodePoint u);
 bool u_is_special_whitespace(CodePoint u) CONST_FN;
 bool u_is_zero_width(CodePoint u);
 unsigned int u_char_width(CodePoint uch) CONST_FN;
