c50d005de5f41424d5bdac0092cb3ba4f235570c
Craig Barnes <Cr@igBarn.es>
2020-03-03 14:07:58 +0000

    Add auto-completion for "macro cancel"

--- src/completion.c
+++ src/completion.c
@@ -335,7 +335,7 @@ static void collect_completions(char **args, size_t argc)
     }
     if (strview_equal_cstring(&cmd_name, "macro")) {
         static const char verbs[][8] = {
-            "record", "stop", "toggle", "run"
+            "record", "stop", "toggle", "cancel", "run"
         };
         if (argc == 1) {
             for (size_t i = 0; i < ARRAY_COUNT(verbs); i++) {
