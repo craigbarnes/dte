292cf55086872aa41cccfac05343a2fc9f4300da
Craig Barnes <Cr@igBarn.es>
2020-06-28 15:16:35 +0100

    Add DragonFly support to mk/nproc.sh script and improve fallback

--- mk/nproc.sh
+++ mk/nproc.sh
@@ -3,12 +3,18 @@
 case "$(uname)" in
 Linux)
     exec getconf _NPROCESSORS_ONLN;;
-FreeBSD|NetBSD|OpenBSD)
+FreeBSD|NetBSD|OpenBSD|DragonFly)
     exec /sbin/sysctl -n hw.ncpu;;
 Darwin)
     exec /usr/sbin/sysctl -n hw.activecpu;;
 SunOS)
     exec /usr/sbin/psrinfo -p;;
 *)
-    echo 1;;
+    (
+        getconf NPROCESSORS_ONLN ||
+        getconf _NPROCESSORS_ONLN ||
+        sysctl -n hw.ncpu ||
+        nproc ||
+        echo 1
+    ) 2>/dev/null;;
 esac
