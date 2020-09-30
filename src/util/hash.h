2c5986c6031fbca682df96c24262310ee6c08192
Craig Barnes <craigbarnes@protonmail.com>
2020-09-30 05:29:28 +0100

    Minor tweaks in src/util/hash.h

--- src/util/hash.h
+++ src/util/hash.h
@@ -1,9 +1,11 @@
 #ifndef UTIL_HASH_H
 #define UTIL_HASH_H
 
+#include <limits.h>
 #include <stddef.h>
 #include <stdint.h>
 #include "ascii.h"
+#include "macros.h"
 
 #define FNV_32_INIT UINT32_C(2166136261)
 #define FNV_64_INIT UINT64_C(14695981039346656037)
@@ -12,12 +14,13 @@
 
 static inline size_t fnv_1a_init(void)
 {
-    return (sizeof(size_t) > 4) ? FNV_64_INIT : FNV_32_INIT;
+    static_assert(8 * CHAR_BIT == 64);
+    return (sizeof(size_t) >= 8) ? FNV_64_INIT : FNV_32_INIT;
 }
 
 static inline size_t fnv_1a_prime(void)
 {
-    return (sizeof(size_t) > 4) ? FNV_64_PRIME : FNV_32_PRIME;
+    return (sizeof(size_t) >= 8) ? FNV_64_PRIME : FNV_32_PRIME;
 }
 
 static inline size_t fnv_1a_hash(const unsigned char *str, size_t n)
