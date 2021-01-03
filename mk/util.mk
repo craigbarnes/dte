a646be6cc5c483cfd48ce09191f3ca1ce9baafa2
Craig Barnes <craigbarnes@protonmail.com>
2021-01-03 13:11:19 +0000

    Remove terminfo/curses related information from "make vars" output
    The editor no longer makes any use of libterminfo or the terminfo
    database.

--- mk/util.mk
+++ mk/util.mk
@@ -17,8 +17,6 @@ ARCH = $(shell uname -m 2>/dev/null)
 NPROC = $(or $(shell sh mk/nproc.sh), 1)
 _POSIX_VERSION = $(shell getconf _POSIX_VERSION 2>/dev/null)
 _XOPEN_VERSION = $(shell getconf _XOPEN_VERSION 2>/dev/null)
-TPUT = $(shell sh -c 'command -v tput')
-TPUT-V = $(if $(TPUT), $(shell $(TPUT) -V 2>/dev/null))
 CC_VERSION = $(shell $(CC) --version 2>/dev/null | head -n1)
 MAKE_S = $(findstring s,$(firstword -$(MAKEFLAGS)))$(filter -s,$(MAKEFLAGS))
 PRINTVAR = printf '\033[1m%15s\033[0m = %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
@@ -30,7 +28,7 @@ AUTOVARS = \
     $(if $(call streq,$(KERNEL),Linux), DISTRO) \
     ARCH NPROC _POSIX_VERSION _XOPEN_VERSION \
     TERM SHELL LANG $(call echo-if-set, LC_CTYPE LC_ALL) \
-    PKGCONFIG TPUT TPUT-V MAKE_VERSION MAKEFLAGS CC_VERSION
+    PKGCONFIG MAKE_VERSION MAKEFLAGS CC_VERSION
 
 vars:
 	@echo
