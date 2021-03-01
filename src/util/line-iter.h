c4bd66fabbe0e84c99a52ef8cffcd4beea52b6b7
Craig Barnes <craigbarnes@protonmail.com>
2021-03-01 02:04:23 +0000

    Make buf_next_line() and buf_slice_next_line() functions static inline

--- src/util/line-iter.h
+++ src/util/line-iter.h
@@ -2,10 +2,38 @@
 #define UTIL_LINE_ITER_H
 
 #include <stddef.h>
+#include <string.h>
+#include "debug.h"
 #include "macros.h"
 #include "string-view.h"
 
-char *buf_next_line(char *buf, size_t *posp, size_t size) NONNULL_ARGS_AND_RETURN;
-StringView buf_slice_next_line(const char *buf, size_t *posp, size_t size) NONNULL_ARGS;
+static inline char *buf_next_line(char *buf, size_t *posp, size_t size)
+{
+    size_t pos = *posp;
+    BUG_ON(pos >= size);
+    size_t avail = size - pos;
+    char *line = buf + pos;
+    char *nl = memchr(line, '\n', avail);
+    if (nl) {
+        *nl = '\0';
+        *posp += nl - line + 1;
+    } else {
+        line[avail] = '\0';
+        *posp += avail;
+    }
+    return line;
+}
+
+static inline StringView buf_slice_next_line(const char *buf, size_t *posp, size_t size)
+{
+    size_t pos = *posp;
+    BUG_ON(pos >= size);
+    size_t avail = size - pos;
+    const char *line = buf + pos;
+    const char *nl = memchr(line, '\n', avail);
+    size_t line_length = nl ? (nl - line + 1) : avail;
+    *posp += line_length;
+    return string_view(line, line_length);
+}
 
 #endif
