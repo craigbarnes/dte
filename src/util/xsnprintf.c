5bedc3e6cbee5a0e9719f0f7c133632f2cb3f2fc
Craig Barnes <craigbarnes@protonmail.com>
2021-08-12 16:12:10 +0100

    Clarify comment in src/util/xsnprintf.c

--- src/util/xsnprintf.c
+++ src/util/xsnprintf.c
@@ -5,7 +5,8 @@
 #include "debug.h"
 
 /*
-Note: ISO C doesn't require vsnprintf(3) to set errno, but POSIX does:
+Note: ISO C doesn't require vsnprintf(3) to set errno on failure, but
+POSIX does:
 
 > If an output error was encountered, these functions shall return
 > a negative value [CX] and set errno to indicate the error.
@@ -13,11 +14,10 @@ Note: ISO C doesn't require vsnprintf(3) to set errno, but POSIX does:
  - https://pubs.opengroup.org/onlinepubs/9699919799/functions/vfprintf.html
  - https://pubs.opengroup.org/onlinepubs/9699919799/functions/snprintf.html
 
-Errors applicable to snprintf(3)/vsnprintf(3):
+The mandated errors of particular interest are:
 
- - EILSEQ: A wide character does not correspond to a valid character
- - EOVERFLOW: The value to be returned is greater than INT_MAX
- - EOVERFLOW: The value of n is greater than INT_MAX
+ - [EOVERFLOW] The value of n is greater than INT_MAX
+ - [EOVERFLOW] The value to be returned is greater than INT_MAX
 */
 
 size_t xvsnprintf(char *restrict s, size_t n, const char *restrict fmt, va_list v)
