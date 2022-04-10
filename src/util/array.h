af3da98202ed64d8f0e1d105bc17cdd2ac3ae8c8
Craig Barnes <craigbarnes@protonmail.com>
2022-04-10 12:49:28 +0100

    Add find_str_idx() function and use for str_to_enum()

--- src/util/array.h
+++ src/util/array.h
@@ -10,27 +10,40 @@
     collect_strings_from_flat_array(*a, ARRAYLEN(a), sizeof(a[0]), ptrs, prefix)
 
 #define STR_TO_ENUM(a, str, off, nfval) \
-    str_to_enum(*a, ARRAYLEN(a), sizeof(a[0]), str, off, nfval)
+    str_to_enum(str, *a, ARRAYLEN(a), sizeof(a[0]), off, nfval)
 
-// Attempt to find `str` in a "flat" array of strings (i.e. an array of
-// type `const char[nr_elements][element_size]`) starting at `base`.
+// This is somewhat similar to lfind(3), but returning an index
+// instead of a pointer and specifically for arrays of type
+// `const char[nmemb][size]`
+static inline ssize_t find_str_idx (
+    const char *str,
+    const char *base,
+    size_t nmemb,
+    size_t size,
+    bool (*equal)(const char *s1, const char *s2)
+) {
+    const char *entry = base;
+    for (size_t i = 0; i < nmemb; i++, entry += size) {
+        if (equal(str, entry)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Attempt to find `str` in a flat array of strings starting at `base`.
 // If found, return the index plus `return_offset`, otherwise return
 // `not_found_val`.
 static inline int str_to_enum (
-    const char *base,
-    size_t nr_elements,
-    size_t element_size,
     const char *str,
+    const char *base,
+    size_t nmemb,
+    size_t size,
     int return_offset,
     int not_found_val
 ) {
-    const char *entry = base;
-    for (size_t i = 0; i < nr_elements; i++, entry += element_size) {
-        if (streq(str, entry)) {
-            return i + return_offset;
-        }
-    }
-    return not_found_val;
+    ssize_t idx = find_str_idx(str, base, nmemb, size, streq);
+    return (idx >= 0) ? idx + return_offset : not_found_val;
 }
 
 void collect_strings_from_flat_array (
