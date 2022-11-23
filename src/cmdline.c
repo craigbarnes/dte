86ef762bb33f9de65728b7f41eac09cbf9203edd
Craig Barnes <craigbarnes@protonmail.com>
2022-11-23 22:42:22 +0000

    Avoid unnecessary work in cmd_search_mode_accept() when not recording

--- src/cmdline.c
+++ src/cmdline.c
@@ -423,36 +423,34 @@ static void cmd_search_mode_accept(EditorState *e, const CommandArgs *a)
         free(original);
     }
 
-    const char *args[5];
-    size_t i = 0;
-    bool backwards = (e->search.direction == SEARCH_BWD);
-    if (c->buf.len > 0) {
-        const char *str = string_borrow_cstring(&c->buf);
-        search_set_regexp(&e->search, str);
-        if (backwards) {
-            args[i++] = "-r";
-        }
-        if (cmdargs_has_flag(a, 'H')) {
-            args[i++] = "-H";
+    if (e->macro.recording) {
+        const char *args[5];
+        size_t i = 0;
+        bool backwards = (e->search.direction == SEARCH_BWD);
+        if (c->buf.len > 0) {
+            const char *str = string_borrow_cstring(&c->buf);
+            search_set_regexp(&e->search, str);
+            if (backwards) {
+                args[i++] = "-r";
+            }
+            if (cmdargs_has_flag(a, 'H')) {
+                args[i++] = "-H";
+            } else {
+                history_add(&e->search_history, str);
+            }
+            if (unlikely(str[0] == '-')) {
+                args[i++] = "--";
+            }
+            args[i++] = str;
         } else {
-            history_add(&e->search_history, str);
+            args[i++] = backwards ? "-p" : "-n";
         }
-        if (unlikely(str[0] == '-')) {
-            args[i++] = "--";
-        }
-        args[i++] = str;
-    } else {
-        args[i++] = backwards ? "-p" : "-n";
+        args[i] = NULL;
+        macro_command_hook(&e->macro, "search", (char**)args);
     }
 
-    args[i] = NULL;
     current_command = NULL;
     search_next(e->view, &e->search, e->options.case_sensitive_search);
-
-    // TODO: avoid calling this function (and constructing args above)
-    // if no macro is being recorded
-    macro_command_hook(&e->macro, "search", (char**)args);
-
     cmdline_clear(c);
     set_input_mode(e, INPUT_NORMAL);
 }
