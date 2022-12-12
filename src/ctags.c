9b765b2a9d9ba7d6926b99f0596bc1b7cd2e414d
Craig Barnes <craigbarnes@protonmail.com>
2022-12-12 08:15:40 +0000

    Reorganize ctags parsing code

--- src/ctags.c
+++ src/ctags.c
@@ -5,42 +5,51 @@
 #include "util/strtonum.h"
 #include "util/xmalloc.h"
 
-static size_t parse_excmd(Tag *t, const char *buf, size_t size)
+static size_t parse_ex_pattern(char **escaped, const char *buf, size_t size)
+{
+    BUG_ON(size == 0);
+    BUG_ON(buf[0] != '/' && buf[0] != '?');
+
+    // The search pattern is not a real regular expression; special characters
+    // need to be escaped
+    char *pattern = xmalloc(size * 2);
+    char open_delim = buf[0];
+    for (size_t i = 1, j = 0; i < size; i++) {
+        if (buf[i] == '\\' && i + 1 < size) {
+            i++;
+            if (buf[i] == '\\') {
+                pattern[j++] = '\\';
+            }
+            pattern[j++] = buf[i];
+            continue;
+        }
+        if (buf[i] == open_delim) {
+            if (i + 2 < size && buf[i + 1] == ';' && buf[i + 2] == '"') {
+                i += 2;
+            }
+            pattern[j] = '\0';
+            *escaped = pattern;
+            return i + 1;
+        }
+        char c = buf[i];
+        if (c == '*' || c == '[' || c == ']') {
+            pattern[j++] = '\\';
+        }
+        pattern[j++] = buf[i];
+    }
+
+    free(pattern);
+    return 0;
+}
+
+static size_t parse_ex_cmd(Tag *t, const char *buf, size_t size)
 {
     if (unlikely(size == 0)) {
         return 0;
     }
 
-    const char open_delim = buf[0];
-    if (open_delim == '/' || open_delim == '?') {
-        // The search pattern is not a real regular expression; special
-        // characters need to be escaped
-        char *pattern = xmalloc(size * 2);
-        for (size_t i = 1, j = 0; i < size; i++) {
-            if (buf[i] == '\\' && i + 1 < size) {
-                i++;
-                if (buf[i] == '\\') {
-                    pattern[j++] = '\\';
-                }
-                pattern[j++] = buf[i];
-                continue;
-            }
-            if (buf[i] == open_delim) {
-                if (i + 2 < size && buf[i + 1] == ';' && buf[i + 2] == '"') {
-                    i += 2;
-                }
-                pattern[j] = '\0';
-                t->pattern = pattern;
-                return i + 1;
-            }
-            char c = buf[i];
-            if (c == '*' || c == '[' || c == ']') {
-                pattern[j++] = '\\';
-            }
-            pattern[j++] = buf[i];
-        }
-        free(pattern);
-        return 0;
+    if (buf[0] == '/' || buf[0] == '?') {
+        return parse_ex_pattern(&t->pattern, buf, size);
     }
 
     unsigned long lineno;
@@ -48,9 +57,11 @@ static size_t parse_excmd(Tag *t, const char *buf, size_t size)
     if (i == 0) {
         return 0;
     }
+
     if (i + 1 < size && buf[i] == ';' && buf[i + 1] == '"') {
         i += 2;
     }
+
     t->lineno = lineno;
     return i;
 }
@@ -70,7 +81,7 @@ bool parse_ctags_line(Tag *t, const char *line, size_t line_len)
     }
 
     // excmd can contain tabs
-    size_t len = parse_excmd(t, line + pos, line_len - pos);
+    size_t len = parse_ex_cmd(t, line + pos, line_len - pos);
     if (len == 0) {
         return false;
     }
@@ -89,7 +100,7 @@ bool parse_ctags_line(Tag *t, const char *line, size_t line_len)
      * typeref:struct:NAME::MEMBER_TYPE   MEMBER_TYPE is type of the tag
      */
     if (line[pos++] != '\t') {
-        free_tag(t); // free `pattern` allocated by parse_excmd()
+        free_tag(t); // free `pattern` allocated by parse_ex_cmd()
         return false;
     }
 
