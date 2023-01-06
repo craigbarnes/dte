98eb2f3a7fbf61473b4f890928e7837927cb6967
Craig Barnes <craigbarnes@protonmail.com>
2023-01-06 14:28:31 +0000

    Add descriptive comment at the top of mk/dev.mk

--- mk/dev.mk
+++ mk/dev.mk
@@ -1,3 +1,7 @@
+# This makefile contains various targets that are only useful for
+# development purposes (e.g. linting, coverage, etc.) and is NOT
+# included in release tarballs.
+
 RELEASE_VERSIONS = 1.10 1.9.1 1.9 1.8.2 1.8.1 1.8 1.7 1.6 1.5 1.4 1.3 1.2 1.1 1.0
 RELEASE_DIST = $(addprefix dte-, $(addsuffix .tar.gz, $(RELEASE_VERSIONS)))
 DISTVER = $(VERSION)
