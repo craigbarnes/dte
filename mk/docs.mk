fa85c369a1f95055a4d393101ee65465e4915f9a
Craig Barnes <craigbarnes@protonmail.com>
2023-01-24 14:32:35 +0000

    Use pandoc "gfm" format for generating man pages and HTML pages
    Contrary to the previous commit message, newer versions of pandoc do
    seem to support most of the useful extensions. The "auto_identifiers"
    extension isn't supported, but "gfm_auto_identifiers" is and appears
    to be enabled by default for gfm.
    
    Note: this commit also makes some minor changes to docs/dte.md, since
    adjacent double and single asterisks seem to be interpreted differently
    when using the gfm format.

--- mk/docs.mk
+++ mk/docs.mk
@@ -1,8 +1,7 @@
 PANDOC = pandoc
-PANDOC_FLAGS = -f markdown_github+definition_lists+auto_identifiers+yaml_metadata_block-hard_line_breaks
+PANDOC_FLAGS = -f gfm+definition_lists+yaml_metadata_block-hard_line_breaks
 PDMAN = $(PANDOC) $(PANDOC_FLAGS) -t docs/pdman.lua
 PDHTML = $(PANDOC) $(PANDOC_FLAGS) -t html5 --toc --template=docs/template.html -Voutput_basename=$(@F)
-STFU_PANDOC = 2>&1 | sed '/^\[WARNING] Deprecated: markdown_github/d' >&2
 
 man = docs/dte.1 docs/dterc.5 docs/dte-syntax.5
 html-man = public/dte.html public/dterc.html public/dte-syntax.html
@@ -25,11 +24,11 @@ public/dte.html: PANDOC_FLAGS += --indented-code-classes=sh
 
 docs/%.1: docs/%.md docs/pdman.lua
 	$(E) PANDOC $@
-	$(Q) $(PDMAN) -o $@ $< $(STFU_PANDOC)
+	$(Q) $(PDMAN) -o $@ $<
 
 docs/%.5: docs/%.md docs/pdman.lua
 	$(E) PANDOC $@
-	$(Q) $(PDMAN) -o $@ $< $(STFU_PANDOC)
+	$(Q) $(PDMAN) -o $@ $<
 
 public/dte.pdf: $(man) | public/
 	$(E) GROFF $@
@@ -41,7 +40,7 @@ build/docs/index.md: docs/index.sed README.md | build/docs/
 
 $(html): docs/template.html | public/style.css
 	$(E) PANDOC $@
-	$(Q) $(PDHTML) -o $@ $(filter-out %.html %.lua, $^) $(STFU_PANDOC)
+	$(Q) $(PDHTML) -o $@ $(filter-out %.html %.lua, $^)
 
 public/style.css: docs/layout.css docs/style.css | public/
 	$(E) CSSCAT $@
