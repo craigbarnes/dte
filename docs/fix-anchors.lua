dd8bf8c44b7b611f38d8219b9e4a1855b6e4e6e6
Craig Barnes <craigbarnes@protonmail.com>
2023-01-24 13:49:00 +0000

    Disallow "fix-anchors" pandoc filter being used on unexpected files

--- docs/fix-anchors.lua
+++ docs/fix-anchors.lua
@@ -1,34 +1,48 @@
--- This Pandoc filter modifies headings, so that the link anchors
--- for dterc commands don't include trailing junk like flags and
--- parameters. For example "include--bq-file" becomes "include".
+-- This Pandoc filter modifies link anchors for section headings
+-- in public/dterc.html and public/dte-syntax.html, so that they
+-- remain stable over time
 
-local function get_canonical_id(header)
-    return header.content[1].content[1].text
+local allowed_docs = {
+    ["dterc"] = true,
+    ["dte-syntax"] = true,
+}
+
+local h2
+
+local function Pandoc(doc)
+    local title = assert(doc.meta.title[1].text)
+    if not allowed_docs[title] then
+        local msg = "file-specific filter used on unknown document: '%s'"
+        error(msg:format(title))
+    end
 end
-
-local section
 
 local function Header(header)
     local level = assert(header.level)
-    local id = header.attr.identifier
+    local id = assert(header.attr.identifier)
+    assert(level >= 1)
+    assert(#id >= 1)
 
-    if level == 2 then
-        section = assert(id)
+    if level <= 2 then
+        h2 = (level == 2) and id or nil
         return header
     end
 
     -- Uppercase all `<h3>` anchors in the "special variables" section,
     -- so that the one for e.g. `$FILETYPE` doesn't clash with the one
     -- for the `filetype` local option.
-    if level == 3 and section == "special-variables" then
+    if level == 3 and h2 == "special-variables" then
         header.attr.identifier = id:upper()
         return header
     end
 
+    -- Remove trailing junk from other <h3> anchors (like command flags
+    -- and arguments), so that e.g. "include--bq-file" becomes "include".
     if level == 3 and id:find("%-") then
-        local ok, new_id = pcall(get_canonical_id, header)
-        if ok and #new_id < #id and new_id == id:sub(1, #new_id) then
+        local new_id = assert(header.content[1].content[1].text)
+        if #new_id < #id and new_id == id:sub(1, #new_id) then
             header.attr.identifier = new_id
+            return header
         end
     end
 
@@ -36,5 +50,8 @@ local function Header(header)
 end
 
 return {
-    {Header = Header}
+    {
+        Header = Header,
+        Pandoc = Pandoc,
+    }
 }
