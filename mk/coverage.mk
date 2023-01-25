c0ea7d0b9a7fe2c5ef5b2b598fb1a263c70fd934
Craig Barnes <craigbarnes@protonmail.com>
2023-01-25 11:22:58 +0000

    Add "make gcovr-html" target to mk/coverage.mk
    For generating a HTML coverage report with gcovr(1), in addition to
    the existing gcovr XML report and lcov HTML report.

--- mk/coverage.mk
+++ mk/coverage.mk
@@ -7,6 +7,7 @@ LCOVFLAGS ?= --config-file mk/lcovrc
 LCOV_REMOVE = $(foreach PAT, $(2), $(LCOV) -r $(1) -o $(1) '*/$(PAT)';)
 GENHTML ?= genhtml
 GENHTMLFLAGS ?= --config-file mk/lcovrc --title dte
+GZIP_ALL_HTML_CSS = find $(1) -name '*.css' -o -name '*.html' | $(XARGS_P) -- gzip -9kf
 
 EXCLUDE_FROM_COVERAGE = \
     src/lock.c \
@@ -18,22 +19,37 @@ EXCLUDE_FROM_COVERAGE = \
     src/util/debug.c \
     test/test.c
 
-coverage-report: build/docs/lcov.css
+coverage-report: lcov
+gcovr: gcovr-html
+gcovr-html: public/coverage/gcovr.html
+gcovr-xml: build/coverage.xml
+
+lcov: build/docs/lcov.css | public/coverage/
 	$(MAKE) -j$(NPROC) check CFLAGS='-Og -g -pipe --coverage -fno-inline' DEBUG=3 USE_SANITIZER=
 	$(LCOV) $(LCOVFLAGS) -c -b . -d build/ -o build/coverage.info
 	$(call LCOV_REMOVE, build/coverage.info, $(EXCLUDE_FROM_COVERAGE))
 	$(GENHTML) $(GENHTMLFLAGS) -o public/coverage/ build/coverage.info
-	find public/coverage/ -type f -regex '.*\.\(css\|html\)$$' | \
-	  $(XARGS_P) -- gzip -9kf
+	$(call GZIP_ALL_HTML_CSS, '$|')
 
 build/docs/lcov.css: docs/lcov-orig.css docs/lcov-extra.css | build/docs/
 	$(E) CSSCAT $@
 	$(Q) cat $^ > $@
 
-build/coverage.xml: gcovr-xml.cfg | build/
+public/coverage/gcovr.html: gcovr.cfg | public/coverage/
+	$(RM) public/coverage/gcovr*
 	$(MAKE) -j$(NPROC) check CFLAGS='-Og -g -pipe --coverage -fno-inline' DEBUG=3 USE_SANITIZER=
-	$(GCOVR) -j$(NPROC) -s --config $< --xml-pretty --xml $@
+	$(GCOVR) -j$(NPROC) -sp --config '$<' --html-details '$@'
+	$(call GZIP_ALL_HTML_CSS, '$|')
+
+build/coverage.xml: gcovr.cfg | build/
+	$(MAKE) -j$(NPROC) check CFLAGS='-Og -g -pipe --coverage -fno-inline' DEBUG=3 USE_SANITIZER=
+	$(GCOVR) -j$(NPROC) -s --config '$<' --xml-pretty --xml '$@'
+
+public/coverage/: public/
+	$(Q) mkdir -p $@
 
 
-NON_PARALLEL_TARGETS += coverage-report
-.PHONY: coverage-report
+NON_PARALLEL_TARGETS += coverage-report lcov gcovr gcovr-html gcovr-xml
+NON_PARALLEL_TARGETS += public/coverage/gcovr.html build/coverage.xml
+
+.PHONY: coverage-report lcov gcovr gcovr-html gcovr-xml
