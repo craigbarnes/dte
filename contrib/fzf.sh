e4bb4d30698791c5e136a78dbfa9d800b7756883
Craig Barnes <craigbarnes@protonmail.com>
2023-03-14 00:09:50 +0000

    Tweak contrib/fzf.sh, to allow excluding the current window from splits

--- contrib/fzf.sh
+++ contrib/fzf.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # When this is run via the `eval` command, it allows the selection
-# (as created with the Tab key) to be confirmed with Ctrl+h or Ctrl+v,
-# in order to create window splits instead of simply opening files
+# (as created with the Tab key) to be confirmed with Ctrl/Alt + h/v,
+# in order to create window splits instead of simply opening files.
+# The Ctrl variants `open` the first file and `wsplit` the rest, so
+# that the current window is included in the split.
 
 fzf \
     --reverse \
     --multi \
-    --expect=ctrl-v,ctrl-h,enter \
+    --expect=ctrl-v,ctrl-h,alt-v,alt-h,enter \
     --bind='f1:change-prompt(find .>)+reload(find . -type f -not -path "*/\.git/*")' \
     --bind='f2:change-prompt(git ls-files>)+reload(git ls-files)' \
 | awk '
@@ -18,16 +20,19 @@ fzf \
     BEGIN {
         cmds["ctrl-v"] = "wsplit -h"
         cmds["ctrl-h"] = "wsplit"
+        cmds["alt-v"] = "wsplit -h"
+        cmds["alt-h"] = "wsplit"
         cmds["enter"] = "open"
     }
 
     NR == 1 {
         cmd = cmds[$1]
+        first = ($1 ~ /^alt-/) ? cmd : "open"
         next
     }
 
     NR == 2 {
-        print gencmd("open", $0)
+        print gencmd(first, $0)
         next
     }
 
