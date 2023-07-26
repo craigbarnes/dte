3781fb21b6cf5a7fdede6c796eabf61d79dcb249
Craig Barnes <craigbarnes@protonmail.com>
2023-07-26 22:38:57 +0100

    Include `<unistd.h>` in `mk/feature-test/mkostemp.c`, for macOS support

--- mk/feature-test/mkostemp.c
+++ mk/feature-test/mkostemp.c
@@ -1,6 +1,18 @@
 #include "defs.h"
-#include <fcntl.h>
-#include <stdlib.h>
+#include <fcntl.h> // O_CLOEXEC
+#include <stdlib.h> // mkostemp(), glibc
+#include <unistd.h> // mkostemp(), macOS
+
+/*
+ Note: for some reason macOS declares mkostemp() in <unistd.h>, despite
+ the fact it originated in glibc, which declares it in <stdlib.h>.
+
+ See also:
+
+ - https://man7.org/linux/man-pages/man3/mkostemp.3.html
+ - https://www.gnu.org/software/gnulib/manual/html_node/mkostemp.html
+ - https://opensource.apple.com/source/Libc/Libc-1439.40.11/include/unistd.h.auto.html
+*/
 
 int main(void)
 {
