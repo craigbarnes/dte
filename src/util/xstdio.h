249164b6bdb5ab61244bcf3348f7b3faffbad528
Craig Barnes <craigbarnes@protonmail.com>
2023-08-04 08:13:32 +0100

    Split part of xfopen() out into separate helper function

--- src/util/xstdio.h
+++ src/util/xstdio.h
@@ -9,26 +9,30 @@
 #include "macros.h"
 #include "xreadwrite.h"
 
-static inline FILE *xfopen(const char *path, const char *mode, int flags, mode_t mask)
+// Convert fopen(3) mode string to equivalent open(3) flags
+static inline int xfopen_mode_to_flags(const char *mode)
 {
     BUG_ON(mode[0] == '\0');
     bool plus = (mode[1] == '+');
     BUG_ON(mode[plus ? 2 : 1] != '\0');
-    BUG_ON(flags != (flags & (O_CLOEXEC | O_NOCTTY | O_NOFOLLOW)));
 
     switch (mode[0]) {
     case 'a':
-        flags |= (plus ? O_RDWR : O_WRONLY) | O_CREAT | O_APPEND;
-        break;
+        return (plus ? O_RDWR : O_WRONLY) | O_CREAT | O_APPEND;
     case 'r':
-        flags |= (plus ? O_RDWR : O_RDONLY);
-        break;
+        return (plus ? O_RDWR : O_RDONLY);
     case 'w':
-        flags |= (plus ? O_RDWR : O_WRONLY) | O_CREAT | O_TRUNC;
-        break;
-    default:
-        BUG("Unknown fopen() mode string: '%s'", mode);
+        return (plus ? O_RDWR : O_WRONLY) | O_CREAT | O_TRUNC;
     }
+
+    BUG("Unknown fopen() mode string: '%s'", mode);
+    return 0;
+}
+
+static inline FILE *xfopen(const char *path, const char *mode, int flags, mode_t mask)
+{
+    BUG_ON(flags != (flags & (O_CLOEXEC | O_NOCTTY | O_NOFOLLOW)));
+    flags |= xfopen_mode_to_flags(mode);
 
     int fd = xopen(path, flags, mask);
     if (fd < 0) {
