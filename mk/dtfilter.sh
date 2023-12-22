3cec48cdfb525cfb56b36816d27d08f569c40a1b
Craig Barnes <craigbarnes@protonmail.com>
2023-12-22 14:10:36 +0000

    Add descriptive comment to `mk/dtfilter.sh`
    Since its purpose (and relevance to the build system) may not be
    immediately obvious.

--- mk/dtfilter.sh
+++ mk/dtfilter.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
+# This script is used by `make install-desktop-file` to rewrite the
+# Exec/TryExec fields in dte.desktop to use absolute paths. This is
+# done so that the desktop file for each installation executes the
+# appropriate binary, instead of just using $PATH resolution.
+
 exec sed "
     s|^Exec = dte \(.*\)\$|Exec = $1 \1|
     s|^TryExec = dte\$|TryExec = $1|
