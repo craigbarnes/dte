5d34e94d3d3ad87f2e80a1b3bda8e6806022ff04
Craig Barnes <craigbarnes@protonmail.com>
2024-01-26 14:14:35 +0000

    Add comments to `progname()` explaining its purpose

--- src/util/progname.h
+++ src/util/progname.h
@@ -3,8 +3,12 @@
 
 #include "macros.h"
 
+// Get the program name from argv[0] (as passed to main()), or fall back to a
+// pre-defined string if it's missing
 static inline const char *progname(int argc, char *argv[], const char *fallback)
 {
+    // Setting argv[0] properly is under the control of the parent process,
+    // so it's safest to make as few assumptions about it as possible
     if (likely(argc >= 1 && argv && argv[0] && argv[0][0])) {
         return argv[0];
     }
