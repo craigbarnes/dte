514815dea7c5b4fb08d0c7de8165723a6a7c49d3
Craig Barnes <craigbarnes@protonmail.com>
2024-03-21 07:11:23 +0000

    Reformat `tools/get-css-properties.sh` slightly

--- tools/get-css-properties.sh
+++ tools/get-css-properties.sh
@@ -4,16 +4,18 @@ set -eu
 # This is the script used to generate the list of CSS properties
 # in config/syntax/css
 
+filter='
+    [
+        .[]
+        | select(.status | test("^REC|CR|WD$"))
+        | select(.property != "--*")
+    ]
+    | unique_by(.property)
+    | .[].property
+'
+
 curl https://www.w3.org/Style/CSS/all-properties.en.json |
-    jq -r '
-        [
-            .[]
-            | select(.status | test("^REC|CR|WD$"))
-            | select(.property != "--*")
-        ]
-        | unique_by(.property)
-        | .[].property
-    ' |
+    jq -r "$filter" |
     tr '\n' ' ' |
     fold -sw68 |
     sed 's/ *$/ \\/' |
