158ca94700e2ef3b1d57f9e9dcc760fde6cfc078
Craig Barnes <craigbarnes@protonmail.com>
2024-03-28 13:23:10 +0000

    Simplify log message for `DECRPM` query replies

--- src/terminal/parse.c
+++ src/terminal/parse.c
@@ -498,7 +498,7 @@ static KeyCode parse_csi_query_reply(const ControlParams *csi, uint8_t prefix)
         unsigned int mode = csi->params[0][0];
         unsigned int status = csi->params[1][0];
         const char *desc = decrpm_status_to_str(status);
-        LOG_DEBUG("DECRPM reply for mode %u: %u (%s)", mode, status, desc);
+        LOG_DEBUG("DECRPM %u reply: %u (%s)", mode, status, desc);
         if (mode == 2026 && decrpm_is_set_or_reset(status)) {
             return KEYCODE_QUERY_REPLY_BIT | TFLAG_SYNC;
         }
