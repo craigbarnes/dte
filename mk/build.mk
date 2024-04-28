craigbarnes/dte

commit b008d6f8087a9532992b4ac2b647a0e04fd4fb54

Put generated headers in `build/gen/` and use `$(CC) -Ibuild/gen`
Instead of relative `#include` paths.

This allows greater flexibility in substituting generated headers,
for use cases like running linters (e.g. clang-tidy) or mock tests.

mk/build.mk
@@ -81,7 +81,7 @@ feature_tests := $(addprefix build/feature/, $(addsuffix .h, \
     posix_madvise qsort_r ))
 
 all_objects := $(editor_objects) $(test_objects) $(bench_objects)
-build_subdirs := $(filter-out build/, $(sort $(dir $(all_objects)))) build/feature/
+build_subdirs := $(filter-out build/, $(sort $(dir $(all_objects)))) build/feature/ build/gen/
 
 editor_sources := $(patsubst build/%.o, src/%.c, $(editor_objects))
 test_sources := $(patsubst build/test/%.o, test/%.c, $(test_objects))
@@ -156,7 +156,7 @@ endif
 
 BASIC_CFLAGS += $(CSTD) $(CWARNS)
 BASIC_CPPFLAGS += -D_FILE_OFFSET_BITS=64
-$(all_objects): BASIC_CPPFLAGS += -Isrc
+$(all_objects): BASIC_CPPFLAGS += -Isrc -Ibuild/gen
 
 OPTCHECK = mk/optcheck.sh $(if $(MAKE_S),-s)
 
@@ -186,20 +186,20 @@ $(terminal_objects): | build/terminal/
 $(build_subdirs): | build/
 $(feature_tests): mk/feature-test/defs.h build/all.cflags | build/feature/
 build/convert.o: build/convert.cflags
-build/builtin-config.h: build/builtin-config.mk
-build/test/data.h: build/test/data.mk
-build/config.o: build/builtin-config.h
-build/test/config.o: build/test/data.h
-build/main.o: build/version.h
-build/editor.o: build/version.h build/feature.h
-build/load-save.o: build/feature.h
-build/signals.o: build/feature.h
-build/tag.o: build/feature.h
-build/util/fd.o: build/feature.h
-build/util/xmemmem.o: build/feature.h
-build/terminal/ioctl.o: build/feature.h
-build/compat.o: build/feature.h
-src/compat.h: build/feature.h
+build/gen/builtin-config.h: build/builtin-config.mk
+build/gen/test-data.h: build/test/data.mk
+build/config.o: build/gen/builtin-config.h
+build/test/config.o: build/gen/test-data.h
+build/main.o: build/gen/version.h
+build/editor.o: build/gen/version.h build/gen/feature.h
+build/load-save.o: build/gen/feature.h
+build/signals.o: build/gen/feature.h
+build/tag.o: build/gen/feature.h
+build/util/fd.o: build/gen/feature.h
+build/util/xmemmem.o: build/gen/feature.h
+build/terminal/ioctl.o: build/gen/feature.h
+build/compat.o: build/gen/feature.h
+src/compat.h: build/gen/feature.h
 
 CFLAGS_ALL = $(CPPFLAGS) $(CFLAGS) $(BASIC_CPPFLAGS) $(BASIC_CFLAGS)
 LDFLAGS_ALL = $(CFLAGS) $(LDFLAGS) $(BASIC_LDFLAGS)
@@ -223,7 +223,7 @@ build/all.ldflags: FORCE | build/
 build/%.cflags: FORCE | build/
 	@$(OPTCHECK) '$(CC) $(CFLAGS_ALL)' $@
 
-build/version.h: FORCE | build/
+build/gen/version.h: FORCE | build/gen/
 	@$(OPTCHECK) '$(HASH)define VERSION "$(VERSION)"' $@
 
 build/builtin-config.mk: FORCE | build/
@@ -232,15 +232,15 @@ build/builtin-config.mk: FORCE | build/
 build/test/data.mk: FORCE | build/test/
 	@$(OPTCHECK) '$(@:.mk=.h): $(TEST_CONFIGS)' $@
 
-build/builtin-config.h: $(BUILTIN_CONFIGS) mk/config2c.awk | build/
+build/gen/builtin-config.h: $(BUILTIN_CONFIGS) mk/config2c.awk | build/gen/
 	$(E) GEN $@
 	$(Q) $(AWK) -f mk/config2c.awk $(BUILTIN_CONFIGS) > $@
 
-build/test/data.h: $(TEST_CONFIGS) mk/config2c.awk | build/test/
+build/gen/test-data.h: $(TEST_CONFIGS) mk/config2c.awk | build/gen/
 	$(E) GEN $@
 	$(Q) $(AWK) -f mk/config2c.awk $(TEST_CONFIGS) > $@
 
-build/feature.h: mk/feature-test/defs.h $(feature_tests)
+build/gen/feature.h: mk/feature-test/defs.h $(feature_tests) | build/gen/
 	$(E) GEN $@
 	$(Q) cat $^ > $@
 

mk/dev.mk
@@ -22,7 +22,7 @@ SPATCH ?= spatch
 SPATCHFLAGS ?= --very-quiet
 SPATCHFILTER = 2>&1 | sed '/egrep is obsolescent/d'
 CTIDYFLAGS ?= -quiet
-CTIDYCFLAGS ?= -std=gnu11 -Isrc -DDEBUG=3
+CTIDYCFLAGS ?= -std=gnu11 -Isrc -Ibuild/gen -DDEBUG=3
 CTIDYFILTER = 2>&1 | sed -E '/^[0-9]+ warnings? generated\.$$/d' >&2
 FINDLINKS = sed -n 's|^.*\(https\?://[A-Za-z0-9_/.-]*\).*|\1|gp'
 CHECKURL = curl -sSI -w '%{http_code}  @1  %{redirect_url}\n' -o /dev/null @1
@@ -147,9 +147,9 @@ check-clang-tidy-headers:
 	  $(CLANGTIDY) $(CTIDYFLAGS) $(f) -- $(CTIDYCFLAGS) $(CTIDYFILTER); \
 	)
 
-clang-tidy-src/config.c: build/builtin-config.h
-clang-tidy-src/editor.c: build/version.h src/compat.h
-clang-tidy-src/main.c: build/version.h
+clang-tidy-src/config.c: build/gen/builtin-config.h
+clang-tidy-src/editor.c: build/gen/version.h src/compat.h
+clang-tidy-src/main.c: build/gen/version.h
 clang-tidy-src/compat.c: src/compat.h
 clang-tidy-src/load-save.c: src/compat.h
 clang-tidy-src/signals.c: src/compat.h
@@ -157,7 +157,7 @@ clang-tidy-src/tag.c: src/compat.h
 clang-tidy-src/util/fd.c: src/compat.h
 clang-tidy-src/util/xmemmem.c: src/compat.h
 clang-tidy-src/terminal/ioctl.c: src/compat.h
-clang-tidy-test/config.c: build/test/data.h
+clang-tidy-test/config.c: build/gen/test-data.h
 
 
 CLEANFILES += dte-*.tar.gz

src/compat.h
@@ -1,7 +1,7 @@
 #ifndef COMPAT_H
 #define COMPAT_H
 
-#include "../build/feature.h"
+#include "feature.h"
 
 extern const char feature_string[];
 

src/config.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include "config.h"
+#include "builtin-config.h"
 #include "commands.h"
 #include "editor.h"
 #include "error.h"
@@ -11,7 +12,6 @@
 #include "util/debug.h"
 #include "util/readfile.h"
 #include "util/str-util.h"
-#include "../build/builtin-config.h"
 
 // NOLINTNEXTLINE(*-avoid-non-const-global-variables)
 ConfigState current_config;

src/editor.c
@@ -31,7 +31,7 @@
 #include "util/log.h"
 #include "util/xmalloc.h"
 #include "util/xstdio.h"
-#include "../build/version.h"
+#include "version.h"
 
 static void set_and_check_locale(void)
 {

src/main.c
@@ -43,9 +43,9 @@
 #include "util/xmalloc.h"
 #include "util/xreadwrite.h"
 #include "util/xsnprintf.h"
+#include "version.h"
 #include "view.h"
 #include "window.h"
-#include "../build/version.h"
 
 static void term_cleanup(EditorState *e)
 {

test/config.c
@@ -14,6 +14,7 @@
 #include "syntax/state.h"
 #include "syntax/syntax.h"
 #include "terminal/terminal.h"
+#include "test-data.h"
 #include "util/debug.h"
 #include "util/path.h"
 #include "util/readfile.h"
@@ -21,7 +22,6 @@
 #include "util/string-view.h"
 #include "util/xsnprintf.h"
 #include "window.h"
-#include "../build/test/data.h"
 
 static void test_builtin_configs(TestContext *ctx)
 {

commit 08a6cf0175ef6b8f3b7b180310cd6964e30681a1

Fix paths in `mk/build.mk` and `.gitlab-ci.yml` broken by b008d6f8087a9

.github/workflows/test.yml
@@ -16,8 +16,8 @@ jobs:
         steps:
             - uses: actions/checkout@v4
             - run: brew install make
-            - run: gmake build/feature.h
-            - run: cat build/feature.h build/feature/*.log
+            - run: gmake build/gen/feature.h
+            - run: cat build/gen/feature.h build/feature/*.log
             - run: gmake -Otarget check
             - run: gmake bench CFLAGS=-O3 DEBUG=0
 
@@ -28,7 +28,7 @@ jobs:
         runs-on: ${{ matrix.os }}
         steps:
             - uses: actions/checkout@v4
-            - run: make build/feature.h
-            - run: cat build/feature.h build/feature/*.log
+            - run: make build/gen/feature.h
+            - run: cat build/gen/feature.h build/feature/*.log
             - run: make -Otarget check
             - run: make bench CFLAGS=-O3 DEBUG=0

.gitlab-ci.yml
@@ -22,7 +22,7 @@ default:
     needs: []
     script:
         - make
-        - cat build/feature.h
+        - cat build/gen/feature.h
         - ldd dte
         - make check
         - make check LDFLAGS=-static
@@ -69,11 +69,11 @@ debian-stable:
         - make installcheck
 
         # Ensure build works with no detected features (test fallbacks)
-        - cat build/feature.h
-        - sed 's/ 1$/ 0/' build/feature.h > build/feature-tmp.h
-        - mv build/feature-tmp.h build/feature.h
+        - cat build/gen/feature.h
+        - sed 's/ 1$/ 0/' build/gen/feature.h > build/gen/feature-tmp.h
+        - mv build/gen/feature-tmp.h build/gen/feature.h
         - make check
-        - cat build/feature.h
+        - cat build/gen/feature.h
 
         # Build with ARM cross-compilers
         - make CC='ccache arm-linux-gnueabihf-gcc'
@@ -94,7 +94,7 @@ android:
     image: registry.gitlab.com/craigbarnes/dockerfiles/android-ndk
     script:
         - make CC="$CC_AARCH64"
-        - cat build/feature.h
+        - cat build/gen/feature.h
         - make CC="$CC_ARMV7A"
         - make CC="$CC_I686"
         - make CC="$CC_X86_64"

mk/build.mk
@@ -186,8 +186,8 @@ $(terminal_objects): | build/terminal/
 $(build_subdirs): | build/
 $(feature_tests): mk/feature-test/defs.h build/all.cflags | build/feature/
 build/convert.o: build/convert.cflags
-build/gen/builtin-config.h: build/builtin-config.mk
-build/gen/test-data.h: build/test/data.mk
+build/gen/builtin-config.h: build/gen/builtin-config.mk
+build/gen/test-data.h: build/gen/test-data.mk
 build/config.o: build/gen/builtin-config.h
 build/test/config.o: build/gen/test-data.h
 build/main.o: build/gen/version.h
@@ -226,10 +226,10 @@ build/%.cflags: FORCE | build/
 build/gen/version.h: FORCE | build/gen/
 	@$(OPTCHECK) '$(HASH)define VERSION "$(VERSION)"' $@
 
-build/builtin-config.mk: FORCE | build/
+build/gen/builtin-config.mk: FORCE | build/gen/
 	@$(OPTCHECK) '$(@:.mk=.h): $(BUILTIN_CONFIGS)' $@
 
-build/test/data.mk: FORCE | build/test/
+build/gen/test-data.mk: FORCE | build/gen/
 	@$(OPTCHECK) '$(@:.mk=.h): $(TEST_CONFIGS)' $@
 
 build/gen/builtin-config.h: $(BUILTIN_CONFIGS) mk/config2c.awk | build/gen/