caa257c8d518404b42533d45be78e206454efe82
Craig Barnes <craigbarnes@protonmail.com>
2024-06-11 20:58:41 +0100

    Include more compile-time build variables in `feature_string[]`
    This string is logged at startup when `$DTE_LOG` is in effect,
    mostly for debugging purposes.

--- src/compat.c
+++ src/compat.c
@@ -1,8 +1,15 @@
-#include "feature.h"
+#include "feature.h" // HAVE_*
+#include <regex.h> // REG_STARTEND, REG_ENHANCED (macOS)
+#include <sys/stat.h> // S_ISVTX
 #include "compat.h"
+#include "util/macros.h" // ASAN_ENABLED, MSAN_ENABLED
+#include "util/unicode.h" // SANE_WCTYPE
+
+// TODO: ICONV_DISABLE
 
 const char feature_string[] =
     ""
+// Features detected via compilation tests (mk/feature-test/*.c)
 #if HAVE_DUP3
     " dup3"
 #endif
@@ -32,5 +39,25 @@ const char feature_string[] =
 #endif
 #if HAVE_POSIX_MADVISE
     " posix_madvise"
+#endif
+
+// Features detected via cpp(1) macros
+#if defined(REG_STARTEND) && !defined(ASAN_ENABLED) && !defined(MSAN_ENABLED)
+    " REG_STARTEND"
+#endif
+#ifdef REG_ENHANCED
+    " REG_ENHANCED"
+#endif
+#ifdef S_ISVTX
+    " S_ISVTX"
+#endif
+#ifdef SANE_WCTYPE
+    " SANE_WCTYPE"
+#endif
+#ifdef ASAN_ENABLED
+    " ASan"
+#endif
+#ifdef MSAN_ENABLED
+    " MSan"
 #endif
 ;
