8c11c18019eb637b47f33427c67c7fa5d9ed94c6
Craig Barnes <craigbarnes@protonmail.com>
2024-06-17 11:13:28 +0100

    Log uncacheable commands passed to `cached_command_new()`
    When `log_level >= LOG_LEVEL_TRACE`.

--- src/command/cache.c
+++ src/command/cache.c
@@ -3,42 +3,54 @@
 #include "cache.h"
 #include "args.h"
 #include "parse.h"
+#include "util/log.h"
 #include "util/ptr-array.h"
 #include "util/str-util.h"
 #include "util/xmalloc.h"
 
+// Takes a command string and returns a struct containing the resolved
+// Command and pre-parsed arguments, or a NULL Command if uncacheable.
+// In both cases, CachedCommand::cmd_str is filled with a copy of the
+// original string. This caching is done to allow handle_binding() to
+// avoid repeated parsing/allocation each time a key binding is used.
 CachedCommand *cached_command_new(const CommandRunner *runner, const char *cmd_str)
 {
     const size_t cmd_str_len = strlen(cmd_str);
     CachedCommand *cached = xmalloc(sizeof(*cached) + cmd_str_len + 1);
     memcpy(cached->cmd_str, cmd_str, cmd_str_len + 1);
 
+    const char *reason;
     PointerArray array = PTR_ARRAY_INIT;
     if (parse_commands(runner, &array, cmd_str) != CMDERR_NONE) {
+        reason = "parsing failed";
         goto nocache;
     }
 
     ptr_array_trim_nulls(&array);
     size_t n = array.count;
     if (n < 2 || ptr_array_xindex(&array, NULL) != n - 1) {
-        // Only single commands can be cached
+        reason = "multiple commands";
         goto nocache;
     }
 
     const Command *cmd = runner->cmds->lookup(array.ptrs[0]);
     if (!cmd) {
-        // Aliases or non-existent commands can't be cached
+        // Aliases and non-existent commands can't be cached, because the
+        // command they expand to could later be invalidated by cmd_alias().
+        reason = "contains aliases";
         goto nocache;
     }
 
+    // TODO: Make this condition more precise
     if (memchr(cmd_str, '$', cmd_str_len)) {
-        // Commands containing variables can't be cached
+        reason = "may contain variables";
         goto nocache;
     }
 
     free(ptr_array_remove_index(&array, 0));
     CommandArgs cmdargs = cmdargs_new((char**)array.ptrs);
     if (do_parse_args(cmd, &cmdargs) != ARGERR_NONE) {
+        reason = "argument parsing failed";
         goto nocache;
     }
 
@@ -48,6 +60,7 @@ CachedCommand *cached_command_new(const CommandRunner *runner, const char *cmd_s
     return cached;
 
 nocache:
+    LOG_TRACE("skipping command cache (%s): %s", reason, cmd_str);
     ptr_array_free(&array);
     cached->cmd = NULL;
     return cached;
