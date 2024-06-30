ddb2c780dc384f6e15538f6d978fea0eec88e540
Craig Barnes <craigbarnes@protonmail.com>
2024-06-30 22:56:29 +0100

    Clean up `xvsnprintf()` and include more detail in comment above it

--- src/util/xsnprintf.c
+++ src/util/xsnprintf.c
@@ -4,32 +4,39 @@
 #include "debug.h"
 
 /*
-Note: ISO C doesn't require vsnprintf(3) to set errno on failure, but
-POSIX does:
-
-> If an output error was encountered, these functions shall return
-> a negative value [CX] and set errno to indicate the error.
-
- - https://pubs.opengroup.org/onlinepubs/9699919799/functions/vfprintf.html
- - https://pubs.opengroup.org/onlinepubs/9699919799/functions/snprintf.html
-
-The mandated errors of particular interest are:
-
- - [EOVERFLOW] The value of n is greater than INT_MAX
- - [EOVERFLOW] The value to be returned is greater than INT_MAX
-*/
-
+ * ISO C doesn't require vsnprintf(3) to set errno on failure, but
+ * POSIX does:
+ *
+ * "If an output error was encountered, these functions shall return
+ * a negative value and set errno to indicate the error."
+ *
+ * The mandated errors of particular interest are:
+ *
+ * - [EOVERFLOW] The value of n is greater than INT_MAX
+ * - [EOVERFLOW] The value to be returned is greater than INT_MAX
+ *
+ * ISO C11 states:
+ *
+ * "The vsnprintf function returns the number of characters that would
+ * have been written had n been sufficiently large, not counting the
+ * terminating null character, or a negative value if an encoding error
+ * occurred. Thus, the null-terminated output has been completely
+ * written if and only if the returned value is nonnegative and less
+ * than n."
+ *
+ * See also:
+ *
+ * - ISO C11 §7.21.6.12p3
+ * - https://pubs.opengroup.org/onlinepubs/9699919799/functions/vsnprintf.html
+ * - https://pubs.opengroup.org/onlinepubs/9699919799/functions/snprintf.html
+ */
 size_t xvsnprintf(char *restrict s, size_t n, const char *restrict fmt, va_list v)
 {
-    int ret = vsnprintf(s, n, fmt, v);
-    if (unlikely(ret < 0)) {
-        fatal_error(__func__, errno);
+    int r = vsnprintf(s, n, fmt, v);
+    if (unlikely(r < 0 || r >= (int)n)) {
+        fatal_error(__func__, (r < 0) ? errno : ENOBUFS);
     }
-    if (unlikely(ret >= (int)n)) {
-        // Output truncated (insufficient buffer space)
-        fatal_error(__func__, ENOBUFS);
-    }
-    return (size_t)ret;
+    return (size_t)r;
 }
 
 size_t xsnprintf(char *restrict s, size_t n, const char *restrict fmt, ...)
