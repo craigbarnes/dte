a6cbac429cea2cbd88d4583fbcc3b51f781b2029
Craig Barnes <craigbarnes@protonmail.com>
2024-06-30 14:16:29 +0100

    Add descriptive comments to `ReplaceFlags` enum values

--- src/replace.h
+++ src/replace.h
@@ -6,11 +6,11 @@
 #include "view.h"
 
 typedef enum {
-    REPLACE_CONFIRM = 1 << 0,
-    REPLACE_GLOBAL = 1 << 1,
-    REPLACE_IGNORE_CASE = 1 << 2,
-    REPLACE_BASIC = 1 << 3,
-    REPLACE_CANCEL = 1 << 4,
+    REPLACE_CONFIRM = 1 << 0, // Show confirmation prompt for each replacement
+    REPLACE_GLOBAL = 1 << 1, // Allow pattern to match multiple times per line
+    REPLACE_IGNORE_CASE = 1 << 2, // Enable case-insensitive matching
+    REPLACE_BASIC = 1 << 3, // Use basic (POSIX BRE) regex syntax
+    REPLACE_CANCEL = 1 << 4, // Used internally by reg_replace()
 } ReplaceFlags;
 
 bool reg_replace(View *view, const char *pattern, const char *format, ReplaceFlags flags) NONNULL_ARGS;
