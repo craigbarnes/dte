craigbarnes/dte

commit 4df5fce16ceb245b2d195d20cbb417cf1f602199

Move some more code from `main()` to separate functions

src/main.c
@@ -235,6 +235,23 @@ static Buffer *init_std_buffer(EditorState *e, int fds[2])
     return buffer;
 }
 
+static bool write_stdout_buffer(Buffer *buffer, int fd)
+{
+    bool r = true;
+    Block *blk;
+    block_for_each(blk, &buffer->blocks) {
+        if (xwrite_all(fd, blk->data, blk->size) < 0) {
+            error_msg_errno("failed to write (stdout) buffer");
+            r = false;
+            break;
+        }
+    }
+
+    free_blocks(buffer);
+    free(buffer);
+    return r;
+}
+
 static ExitCode init_logging(const char *filename, const char *req_level_str)
 {
     if (!filename || filename[0] == '\0') {
@@ -463,9 +480,7 @@ loop_break:;
     EditorState *e = init_editor_state();
     Terminal *term = &e->terminal;
     term_init(term, term_name, colorterm);
-
     Buffer *std_buffer = init_std_buffer(e, std_fds);
-    bool have_stdout_buffer = std_buffer && std_buffer->stdout_buffer;
 
     // Create this early (needed if "lock-files" is true)
     const char *cfgdir = e->user_config_dir;
@@ -600,25 +615,11 @@ exit:
     frame_remove(e, e->root_frame); // Unlock files and add to file history
     write_history_files(e);
 
-    if (have_stdout_buffer) {
-        int fd = std_fds[STDOUT_FILENO];
-        Block *blk;
-        block_for_each(blk, &std_buffer->blocks) {
-            if (xwrite_all(fd, blk->data, blk->size) < 0) {
-                error_msg_errno("failed to write (stdout) buffer");
-                if (exit_code == EDITOR_EXIT_OK) {
-                    exit_code = EC_IO_ERROR;
-                }
-                break;
-            }
+    if (std_buffer && std_buffer->stdout_buffer) {
+        bool ok = write_stdout_buffer(std_buffer, std_fds[STDOUT_FILENO]);
+        if (!ok && exit_code == EDITOR_EXIT_OK) {
+            exit_code = EC_IO_ERROR;
         }
-        free_blocks(std_buffer);
-        free(std_buffer);
-    }
-
-    size_t n = term->obuf.count;
-    if (n > 0) {
-        LOG_DEBUG("%zu unflushed bytes remaining in terminal output buffer", n);
     }
 
     free_editor_state(e);

src/editor.c
@@ -188,6 +188,11 @@ static void free_mode_handler(ModeHandler *handler)
 
 void free_editor_state(EditorState *e)
 {
+    size_t n = e->terminal.obuf.count;
+    if (n > 0) {
+        LOG_DEBUG("%zu unflushed bytes remaining in terminal output buffer", n);
+    }
+
     free(e->clipboard.buf);
     free_file_options(&e->file_options);
     free_filetypes(&e->filetypes);

commit 0d27067c7dcf35e7f15e534b03e74467c9d06303

Fix use-after-free bug when running e.g. `echo xyz | dte -cquit`
This bug was introduced by commit 4df5fce16ceb245b and was due to
`std_buffer` being fully freed by `free_buffer()`, in between the
calls to `init_std_buffer()` and `buffer_write_blocks_and_free()`,
when `std_buffer->stdout_buffer` was false.

This commit also adds some tests that should prevent regressions
(especially when combined with AddressSanitizer).

src/main.c
@@ -484,6 +484,7 @@ loop_break:;
     Terminal *term = &e->terminal;
     term_init(term, term_name, colorterm);
     Buffer *std_buffer = init_std_buffer(e, std_fds);
+    bool have_stdout_buffer = std_buffer && std_buffer->stdout_buffer;
 
     // Create this early (needed if "lock-files" is true)
     const char *cfgdir = e->user_config_dir;
@@ -629,7 +630,7 @@ exit:
     frame_remove(e, e->root_frame); // Unlock files and add to file history
     write_history_files(e);
 
-    if (std_buffer && std_buffer->stdout_buffer) {
+    if (have_stdout_buffer) {
         bool ok = buffer_write_blocks_and_free(std_buffer, std_fds[STDOUT_FILENO]);
         if (!ok && exit_code == EDITOR_EXIT_OK) {
             exit_code = EC_IO_ERROR;

test/check-opts.sh
@@ -39,6 +39,14 @@ check_str "$($dte -b rc)" "$(cat config/rc)"
 
 $dte -s config/syntax/dte >/dev/null
 
+# TODO: Run these tests unconditionally when main() no longer requires
+# a controlling terminal when started with e.g. `dte -cquit`
+if test -t 0 -a -t 1; then
+    check_str "$(echo xyz | TERM='' $dte -c 'replace y Y; quit' | cat)" 'xYz'
+    printf test | TERM='' $dte -cquit
+    check_exit "$?" 0
+fi
+
 # Check error handling -----------------------------------------------------
 set +e
 