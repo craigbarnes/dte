ab0c64d4573c2f276075153606fac0124f6b3559
Craig Barnes <craigbarnes@protonmail.com>
2024-08-25 07:35:21 +0100

    Move `xargs -P` detection from `mk/util.mk` to `mk/platform.sh`
    This also removes the last uses of the `try-run` and `make-lazy` macros.

--- mk/util.mk
+++ mk/util.mk
@@ -1,19 +1,8 @@
 HASH := \#
 streq = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
 toupper = $(shell echo '$(1)' | tr '[:lower:]' '[:upper:]')
-try-run = $(if $(shell $(1) >/dev/null 2>&1 && echo 1),$(2),$(3))
 prefix-obj = $(addprefix $(1), $(addsuffix .o, $(2)))
 echo-if-set = $(foreach var, $(1), $(if $($(var)), $(var)))
-
-# Note: this doesn't work reliably in make 3.81, due to a bug, but
-# we already check for GNU Make 4.0+ in mk/compat.mk.
-# See also: https://blog.jgc.org/2016/07/lazy-gnu-make-variables.html
-make-lazy = $(eval $1 = $$(eval $1 := $(value $(1)))$$($1))
-
-XARGS = xargs
-XARGS_P_FLAG = $(call try-run, printf "1\n2" | $(XARGS) -P2 -I@ echo '@', -P$(NPROC))
-$(call make-lazy,XARGS_P_FLAG)
-XARGS_P = $(XARGS) $(XARGS_P_FLAG)
 
 MAKEFLAGS += -r
 MAKE_S = $(findstring s,$(firstword -$(MAKEFLAGS)))
