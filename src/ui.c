craigbarnes/dte

commit 9761a6872466b1c405d14d46ab2c0c175492a2c1

Handle dialog drawing in `update_screen()` and use in `dialog_prompt()`
Instead of reimplementing parts of `update_screen()` just to insert an
extra call to `show_dialog()`.

src/ui.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "ui.h"
+#include "error.h"
 #include "frame.h"
 #include "terminal/cursor.h"
 #include "terminal/ioctl.h"
@@ -179,6 +180,11 @@ void update_screen(EditorState *e, const ScreenState *s)
     }
 
     update_command_line(e);
+
+    if (unlikely(flags & UPDATE_DIALOG)) {
+        bool u;
+        show_dialog(term, &e->styles, get_msg(&u));
+    }
 
     if (flags & UPDATE_CURSOR_STYLE) {
         update_cursor_style(e);

src/editor.h
@@ -61,6 +61,8 @@ typedef enum {
 
     UPDATE_ALL_WINDOWS = 1u << 4, // update_all_windows()
     UPDATE_ALL = (UPDATE_ALL_WINDOWS << 1) - 1, // All of the above
+
+    UPDATE_DIALOG = 1u << 30, // show_dialog(); modal dialog for e.g. `quit -p`
 } ScreenUpdateFlags;
 
 typedef struct EditorState {

src/replace.c
@@ -87,6 +87,7 @@ static unsigned int replace_on_line (
         view->cursor = *bi;
 
         if (flags & REPLACE_CONFIRM) {
+            e->screen_update |= UPDATE_CURRENT_BUFFER;
             switch (status_prompt(e, "Replace? [Y/n/a/q]", "ynaq")) {
             case 'y':
                 break;

src/ui-cmdline.c
@@ -19,14 +19,6 @@ static void print_message(Terminal *term, const StyleMap *styles, const char *ms
     }
 }
 
-void show_message(Terminal *term, const StyleMap *styles, const char *msg, bool is_error)
-{
-    term_output_reset(term, 0, term->width, 0);
-    term_move_cursor(&term->obuf, 0, term->height - 1);
-    print_message(term, styles, msg, is_error);
-    term_clear_eol(term);
-}
-
 static size_t print_command(Terminal *term, const StyleMap *styles, const CommandLine *cmdline, char prefix)
 {
     const String *buf = &cmdline->buf;

src/ui-prompt.c
@@ -48,12 +48,8 @@ static char get_choice(Terminal *term, const char *choices, unsigned int esc_tim
     return 0;
 }
 
-static void show_dialog (
-    Terminal *term,
-    const StyleMap *styles,
-    const TermStyle *text_style,
-    const char *question
-) {
+void show_dialog(Terminal *term, const StyleMap *styles, const char *question)
+{
     unsigned int question_width = u_str_width(question);
     unsigned int min_width = question_width + 2;
     if (term->height < 9 || term->width < min_width) {
@@ -66,11 +62,12 @@ static void show_dialog (
     unsigned int bot = top + height;
     unsigned int width = MAX(term->width / 2, min_width);
     unsigned int x = (term->width - width) / 2;
+    TermOutputBuffer *obuf = &term->obuf;
 
     // The "underline" and "strikethrough" attributes should only apply
     // to the text, not the whole dialog background:
-    TermStyle dialog_style = *text_style;
-    TermOutputBuffer *obuf = &term->obuf;
+    TermStyle text_style = styles->builtin[BSE_DIALOG];
+    TermStyle dialog_style = text_style;
     dialog_style.attr &= ~(ATTR_UNDERLINE | ATTR_STRIKETHROUGH);
     set_style(term, styles, &dialog_style);
 
@@ -79,7 +76,7 @@ static void show_dialog (
         term_move_cursor(obuf, x, y);
         if (y == mid) {
             term_set_bytes(term, ' ', (width - question_width) / 2);
-            set_style(term, styles, text_style);
+            set_style(term, styles, &text_style);
             term_put_str(obuf, question);
             set_style(term, styles, &dialog_style);
         }
@@ -89,35 +86,22 @@ static void show_dialog (
 
 char dialog_prompt(EditorState *e, const char *question, const char *choices)
 {
-    const StyleMap *styles = &e->styles;
-    const TermStyle *style = &styles->builtin[BSE_DIALOG];
+    const ScreenState dummyval = {.id = 0};
+    info_msg("%s", question);
+    e->screen_update |= UPDATE_ALL | UPDATE_DIALOG;
+    update_screen(e, &dummyval);
+
     Terminal *term = &e->terminal;
-    TermOutputBuffer *obuf = &term->obuf;
-
-    start_update(term);
-    update_term_title(term, e->buffer, e->options.set_window_title);
-    update_all_windows(e);
-    show_dialog(term, styles, style, question);
-    show_message(term, styles, question, false);
-    term_end_sync_update(term);
-    term_output_flush(obuf);
-
     unsigned int esc_timeout = e->options.esc_timeout;
     char choice;
     while ((choice = get_choice(term, choices, esc_timeout)) == 0) {
-        if (!resized) {
-            continue;
+        if (resized) {
+            e->screen_update |= UPDATE_DIALOG;
+            ui_resize(e);
         }
-        resized = 0;
-        update_screen_size(&e->terminal, e->root_frame);
-        term_begin_sync_update(term);
-        update_all_windows(e);
-        show_dialog(term, styles, style, question);
-        show_message(term, styles, question, false);
-        term_end_sync_update(term);
-        term_output_flush(obuf);
     }
 
+    clear_error();
     e->screen_update |= UPDATE_ALL;
     return (choice >= 'a') ? choice : 0;
 }
@@ -125,7 +109,6 @@ char dialog_prompt(EditorState *e, const char *question, const char *choices)
 char status_prompt(EditorState *e, const char *question, const char *choices)
 {
     const ScreenState dummyval = {.id = 0};
-    e->screen_update |= UPDATE_CURRENT_BUFFER | UPDATE_TERM_TITLE;
     info_msg("%s", question);
     update_screen(e, &dummyval);
 

src/ui.h
@@ -35,7 +35,6 @@ void restore_cursor(EditorState *e);
 
 // ui-cmdline.c
 void update_command_line(EditorState *e);
-void show_message(Terminal *term, const StyleMap *styles, const char *msg, bool is_error);
 
 // ui-tabbar.c
 void print_tabbar(Terminal *term, const StyleMap *styles, Window *window);
@@ -59,5 +58,6 @@ void update_window_separators(EditorState *e);
 // ui-prompt.c
 char status_prompt(EditorState *e, const char *question, const char *choices) NONNULL_ARGS;
 char dialog_prompt(EditorState *e, const char *question, const char *choices) NONNULL_ARGS;
+void show_dialog(Terminal *term, const StyleMap *styles, const char *question) NONNULL_ARGS;
 
 #endif

commit 345aa72418eac9b1f1daffc3e43debb6a57b90e8

Don't unhide cursor in `update_screen()` if `UPDATE_DIALOG` flag is set
This fixes an issue where the cursor was visible inside the dialog,
when positioned under it prior to `dialog_prompt()` being called.
It was also visible in other cases too, but these were much less
noticeable/ugly.

Doing this also avoids unnecessary work in general, since the cursor
will always be restored and shown after `dialog_prompt()` returns.

The issue in question was introduced in commit 9761a6872466b1c4.

src/ui.c
@@ -88,8 +88,6 @@ static void clear_update_tabbar(Window *window, void* UNUSED_ARG(data))
 static void end_update(EditorState *e)
 {
     Terminal *term = &e->terminal;
-    restore_cursor(e);
-    term_show_cursor(term);
     term_end_sync_update(term);
     term_output_flush(&term->obuf);
 
@@ -181,13 +179,16 @@ void update_screen(EditorState *e, const ScreenState *s)
 
     update_command_line(e);
 
+    if (flags & UPDATE_CURSOR_STYLE) {
+        update_cursor_style(e);
+    }
+
     if (unlikely(flags & UPDATE_DIALOG)) {
         bool u;
         show_dialog(term, &e->styles, get_msg(&u));
-    }
-
-    if (flags & UPDATE_CURSOR_STYLE) {
-        update_cursor_style(e);
+    } else {
+        restore_cursor(e);
+        term_show_cursor(term);
     }
 
     e->screen_update = 0;