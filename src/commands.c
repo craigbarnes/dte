craigbarnes/dte

commit 6985f68602003cd7055a917771c9d8c0416b9add

Rename various functions for consistency

src/commands.c
@@ -1759,7 +1759,7 @@ static bool cmd_save(EditorState *e, const CommandArgs *a)
         update_short_filename(buffer, &e->home_dir);
 
         // Filename change is not detected (only buffer_modified() change)
-        mark_buffer_tabbars_changed(buffer);
+        buffer_mark_tabbars_changed(buffer);
     }
     if (!old_mode && streq(buffer->options.filetype, "none")) {
         // New file and most likely user has not changed the filetype
@@ -2067,7 +2067,7 @@ static bool cmd_title(EditorState *e, const CommandArgs *a)
         return error_msg("saved buffers can't be retitled");
     }
     set_display_filename(buffer, xstrdup(a->args[0]));
-    mark_buffer_tabbars_changed(buffer);
+    buffer_mark_tabbars_changed(buffer);
     return true;
 }
 
@@ -2181,10 +2181,10 @@ static bool cmd_wflip(EditorState *e, const CommandArgs *a)
 static bool cmd_wnext(EditorState *e, const CommandArgs *a)
 {
     BUG_ON(a->nr_args);
-    e->window = next_window(e->window);
+    e->window = window_next(e->window);
     set_view(e->window->view);
     mark_everything_changed(e);
-    debug_frame(e->root_frame);
+    frame_debug(e->root_frame);
     return true;
 }
 
@@ -2209,10 +2209,10 @@ static bool cmd_word_fwd(EditorState *e, const CommandArgs *a)
 static bool cmd_wprev(EditorState *e, const CommandArgs *a)
 {
     BUG_ON(a->nr_args);
-    e->window = prev_window(e->window);
+    e->window = window_next(e->window);
     set_view(e->window->view);
     mark_everything_changed(e);
-    debug_frame(e->root_frame);
+    frame_debug(e->root_frame);
     return true;
 }
 
@@ -2258,16 +2258,16 @@ static bool cmd_wresize(EditorState *e, const CommandArgs *a)
             return error_msg("Invalid resize value: %s", arg);
         }
         if (arg[0] == '+' || arg[0] == '-') {
-            add_to_frame_size(window->frame, dir, n);
+            frame_add_to_size(window->frame, dir, n);
         } else {
-            resize_frame(window->frame, dir, n);
+            frame_resize(window->frame, dir, n);
         }
     } else {
-        equalize_frame_sizes(window->frame->parent);
+        frame_equalize_sizes(window->frame->parent);
     }
 
     mark_everything_changed(e);
-    debug_frame(e->root_frame);
+    frame_debug(e->root_frame);
     // TODO: return false if resize failed?
     return true;
 }
@@ -2296,9 +2296,9 @@ static bool cmd_wsplit(EditorState *e, const CommandArgs *a)
 
     Frame *frame;
     if (root) {
-        frame = split_root_frame(e, vertical, before);
+        frame = frame_split_root(e, vertical, before);
     } else {
-        frame = split_frame(e->window, vertical, before);
+        frame = frame_split(e->window, vertical, before);
     }
 
     View *save = e->view;
@@ -2325,13 +2325,13 @@ static bool cmd_wsplit(EditorState *e, const CommandArgs *a)
 
     if (!view) {
         // Open failed, remove new window
-        remove_frame(e, e->window->frame);
+        frame_remove(e, e->window->frame);
         e->view = save;
         e->buffer = save->buffer;
         e->window = save->window;
     }
 
-    debug_frame(e->root_frame);
+    frame_debug(e->root_frame);
     return !!view;
 }
 

src/frame.c
@@ -87,7 +87,7 @@ static void set_size(Frame *frame, int size)
     bool vertical = frame->parent->vertical;
     int w = vertical ? frame->parent->w : size;
     int h = vertical ? size : frame->parent->h;
-    set_frame_size(frame, w, h);
+    frame_set_size(frame, w, h);
 }
 
 static void divide_equally(const Frame *frame)
@@ -291,7 +291,7 @@ static Frame *find_resizable(Frame *frame, ResizeDirection dir)
     return NULL;
 }
 
-void set_frame_size(Frame *frame, int w, int h)
+void frame_set_size(Frame *frame, int w, int h)
 {
     int min_w = get_min_w(frame);
     int min_h = get_min_h(frame);
@@ -302,7 +302,7 @@ void set_frame_size(Frame *frame, int w, int h)
 
     if (frame->window) {
         w -= rightmost_frame(frame) ? 0 : 1; // Separator
-        set_window_size(frame->window, w, h);
+        window_set_size(frame->window, w, h);
         return;
     }
 
@@ -313,14 +313,14 @@ void set_frame_size(Frame *frame, int w, int h)
     }
 }
 
-void equalize_frame_sizes(Frame *parent)
+void frame_equalize_sizes(Frame *parent)
 {
     parent->equal_size = true;
     divide_equally(parent);
     update_window_coordinates(parent);
 }
 
-void resize_frame(Frame *frame, ResizeDirection dir, int size)
+void frame_resize(Frame *frame, ResizeDirection dir, int size)
 {
     frame = find_resizable(frame, dir);
     if (!frame) {
@@ -333,15 +333,15 @@ void resize_frame(Frame *frame, ResizeDirection dir, int size)
     update_window_coordinates(parent);
 }
 
-void add_to_frame_size(Frame *frame, ResizeDirection dir, int amount)
+void frame_add_to_size(Frame *frame, ResizeDirection dir, int amount)
 {
-    resize_frame(frame, dir, get_size(frame) + amount);
+    frame_resize(frame, dir, get_size(frame) + amount);
 }
 
 static void update_frame_coordinates(const Frame *frame, int x, int y)
 {
     if (frame->window) {
-        set_window_coordinates(frame->window, x, y);
+        window_set_coordinates(frame->window, x, y);
         return;
     }
 
@@ -370,7 +370,7 @@ void update_window_coordinates(Frame *frame)
     update_frame_coordinates(get_root_frame(frame), 0, 0);
 }
 
-Frame *split_frame(Window *window, bool vertical, bool before)
+Frame *frame_split(Window *window, bool vertical, bool before)
 {
     Frame *frame = window->frame;
     Frame *parent = frame->parent;
@@ -388,13 +388,13 @@ Frame *split_frame(Window *window, bool vertical, bool before)
     parent->equal_size = true;
 
     // Recalculate
-    set_frame_size(parent, parent->w, parent->h);
+    frame_set_size(parent, parent->w, parent->h);
     update_window_coordinates(parent);
     return frame;
 }
 
 // Doesn't really split root but adds new frame between root and its contents
-Frame *split_root_frame(EditorState *e, bool vertical, bool before)
+Frame *frame_split_root(EditorState *e, bool vertical, bool before)
 {
     Frame *old_root = e->root_frame;
     Frame *new_root = new_frame();
@@ -404,7 +404,7 @@ Frame *split_root_frame(EditorState *e, bool vertical, bool before)
     e->root_frame = new_root;
 
     Frame *frame = add_frame(new_root, new_window(e), before ? 0 : 1);
-    set_frame_size(new_root, old_root->w, old_root->h);
+    frame_set_size(new_root, old_root->w, old_root->h);
     update_window_coordinates(new_root);
     return frame;
 }
@@ -423,7 +423,7 @@ static void free_frame(Frame *frame)
     free(frame);
 }
 
-void remove_frame(EditorState *e, Frame *frame)
+void frame_remove(EditorState *e, Frame *frame)
 {
     Frame *parent = frame->parent;
     if (!parent) {
@@ -454,7 +454,7 @@ void remove_frame(EditorState *e, Frame *frame)
     }
 
     // Recalculate
-    set_frame_size(parent, parent->w, parent->h);
+    frame_set_size(parent, parent->w, parent->h);
     update_window_coordinates(parent);
 }
 
@@ -484,13 +484,13 @@ void dump_frame(const Frame *frame, size_t level, String *str)
 }
 
 #if DEBUG >= 1
-void debug_frame(const Frame *frame)
+void frame_debug(const Frame *frame)
 {
     sanity_check_frame(frame);
     for (size_t i = 0, n = frame->frames.count; i < n; i++) {
         const Frame *c = frame->frames.ptrs[i];
         BUG_ON(c->parent != frame);
-        debug_frame(c);
+        frame_debug(c);
     }
 }
 #endif

src/frame.h
@@ -29,20 +29,20 @@ typedef enum {
 struct EditorState;
 
 Frame *new_root_frame(struct Window *window);
-void set_frame_size(Frame *frame, int w, int h);
-void equalize_frame_sizes(Frame *parent);
-void add_to_frame_size(Frame *frame, ResizeDirection dir, int amount);
-void resize_frame(Frame *frame, ResizeDirection dir, int size);
-void update_window_coordinates(Frame *frame);
-Frame *split_frame(struct Window *window, bool vertical, bool before);
-Frame *split_root_frame(struct EditorState *e, bool vertical, bool before);
-void remove_frame(struct EditorState *e, Frame *frame);
+void frame_set_size(Frame *frame, int w, int h);
+void frame_equalize_sizes(Frame *parent);
+void frame_add_to_size(Frame *frame, ResizeDirection dir, int amount);
+void frame_resize(Frame *frame, ResizeDirection dir, int size);
+Frame *frame_split(struct Window *window, bool vertical, bool before);
+Frame *frame_split_root(struct EditorState *e, bool vertical, bool before);
+void frame_remove(struct EditorState *e, Frame *frame);
 void dump_frame(const Frame *frame, size_t level, String *str);
+void update_window_coordinates(Frame *frame);
 
 #if DEBUG >= 1
-  void debug_frame(const Frame *frame);
+  void frame_debug(const Frame *frame);
 #else
-  static inline void debug_frame(const Frame* UNUSED_ARG(frame)) {}
+  static inline void frame_debug(const Frame* UNUSED_ARG(frame)) {}
 #endif
 
 #endif

src/main.c
@@ -544,7 +544,7 @@ loop_break:;
     set_print_errors_to_stderr(true);
 
     // Unlock files and add to file history
-    remove_frame(e, e->root_frame);
+    frame_remove(e, e->root_frame);
 
     if (load_and_save_history) {
         history_save(&e->command_history);

src/screen-window.c
@@ -25,7 +25,7 @@ static void update_line_numbers(Terminal *term, const ColorScheme *colors, Windo
     size_t lines = view->buffer->nl;
     int x = window->x;
 
-    calculate_line_numbers(window);
+    window_calculate_line_numbers(window);
     long first = view->vy + 1;
     long last = MIN(view->vy + window->edit_h, lines);
 

src/screen.c
@@ -125,7 +125,7 @@ void start_update(Terminal *term)
 
 void update_window_sizes(Terminal *term, Frame *frame)
 {
-    set_frame_size(frame, term->width, term->height - 1);
+    frame_set_size(frame, term->width, term->height - 1);
     update_window_coordinates(frame);
 }
 
@@ -181,7 +181,7 @@ void update_screen(EditorState *e, const ScreenState *s)
             buffer_mark_lines_changed(buffer, s->cy, view->cy);
         }
         if (s->is_modified != buffer_modified(buffer)) {
-            mark_buffer_tabbars_changed(buffer);
+            buffer_mark_tabbars_changed(buffer);
         }
     } else {
         e->window->update_tabbar = true;

src/window.c
@@ -353,7 +353,7 @@ View *window_open_files(Window *window, char **filenames, const Encoding *encodi
     return first;
 }
 
-void mark_buffer_tabbars_changed(Buffer *buffer)
+void buffer_mark_tabbars_changed(Buffer *buffer)
 {
     for (size_t i = 0, n = buffer->views.count; i < n; i++) {
         View *view = buffer->views.ptrs[i];
@@ -390,7 +390,7 @@ static void set_edit_size(Window *window, const GlobalOptions *options)
     window->edit_x = window->x + xo;
 }
 
-void calculate_line_numbers(Window *window)
+void window_calculate_line_numbers(Window *window)
 {
     const GlobalOptions *options = &window->editor->options;
     int w = line_numbers_width(window, options);
@@ -403,7 +403,7 @@ void calculate_line_numbers(Window *window)
     set_edit_size(window, options);
 }
 
-void set_window_coordinates(Window *window, int x, int y)
+void window_set_coordinates(Window *window, int x, int y)
 {
     const GlobalOptions *options = &window->editor->options;
     window->x = x;
@@ -412,11 +412,11 @@ void set_window_coordinates(Window *window, int x, int y)
     window->edit_y = y + edit_y_offset(options);
 }
 
-void set_window_size(Window *window, int w, int h)
+void window_set_size(Window *window, int w, int h)
 {
     window->w = w;
     window->h = h;
-    calculate_line_numbers(window);
+    window_calculate_line_numbers(window);
 }
 
 int window_get_scroll_margin(const Window *window, unsigned int scroll_margin)
@@ -466,7 +466,7 @@ static void find_prev_and_next(Window *window, void *ud)
     data->prev = window;
 }
 
-Window *prev_window(Window *window)
+Window *window_prev(Window *window)
 {
     WindowCallbackData data = {.target = window};
     frame_for_each_window(window->editor->root_frame, find_prev_and_next, &data);
@@ -474,7 +474,7 @@ Window *prev_window(Window *window)
     return data.prev ? data.prev : data.last;
 }
 
-Window *next_window(Window *window)
+Window *window_next(Window *window)
 {
     WindowCallbackData data = {.target = window};
     frame_for_each_window(window->editor->root_frame, find_prev_and_next, &data);
@@ -498,10 +498,10 @@ void window_close(Window *window)
     Window *next_or_prev = data.next ? data.next : data.prev;
     BUG_ON(!next_or_prev);
 
-    remove_frame(e, window->frame);
+    frame_remove(e, window->frame);
     e->window = NULL;
     set_view(next_or_prev->view);
 
     mark_everything_changed(e);
-    debug_frame(e->root_frame);
+    frame_debug(e->root_frame);
 }

src/window.h
@@ -49,20 +49,20 @@ View *window_get_view(Window *window, Buffer *buffer);
 View *window_find_view(Window *window, Buffer *buffer);
 View *window_find_unclosable_view(Window *window);
 void window_free(Window *window);
-size_t remove_view(View *view);
 void window_close(Window *window);
 void window_close_current_view(Window *window);
-void set_view(View *view);
 View *window_open_new_file(Window *window);
 View *window_open_file(Window *window, const char *filename, const Encoding *encoding);
 View *window_open_files(Window *window, char **filenames, const Encoding *encoding);
-void mark_buffer_tabbars_changed(Buffer *buffer);
-void calculate_line_numbers(Window *window);
-void set_window_coordinates(Window *window, int x, int y);
-void set_window_size(Window *window, int w, int h);
+void window_calculate_line_numbers(Window *window);
+void window_set_coordinates(Window *window, int x, int y);
+void window_set_size(Window *window, int w, int h);
 int window_get_scroll_margin(const Window *window, unsigned int scroll_margin);
+Window *window_prev(Window *window);
+Window *window_next(Window *window);
 void frame_for_each_window(const Frame *frame, void (*func)(Window*, void*), void *data);
-Window *prev_window(Window *window);
-Window *next_window(Window *window);
+void buffer_mark_tabbars_changed(Buffer *buffer);
+void set_view(View *view);
+size_t remove_view(View *view);
 
 #endif

commit 5cb3f71eec31f6b0c57fb21b80463feb9f4eb06e

Fix bug causing `wprev` command to be identical to `wnext`
This was caused by a mistake in commit 6985f68602003cd7.

src/commands.c
@@ -2341,7 +2341,7 @@ static bool cmd_word_fwd(EditorState *e, const CommandArgs *a)
 static bool cmd_wprev(EditorState *e, const CommandArgs *a)
 {
     BUG_ON(a->nr_args);
-    e->window = window_next(e->window);
+    e->window = window_prev(e->window);
     set_view(e->window->view);
     e->screen_update |= UPDATE_ALL;
     frame_debug(e->root_frame);