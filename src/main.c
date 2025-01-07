craigbarnes/dte

commit f1ff10b74e76bb7518396286b0036c2b00c1809a

Replace most uses of global `ErrorBuffer` struct with `EditorState::err`

src/main.c
@@ -89,18 +89,21 @@ static ExitCode dump_builtin_config(const char *name)
 static ExitCode lint_syntax(const char *filename, SyntaxLoadFlags flags)
 {
     EditorState *e = init_editor_state(EFLAG_HEADLESS);
-    int err;
     BUG_ON(e->status != EDITOR_INITIALIZING);
+    int err;
     const Syntax *s = load_syntax_file(e, filename, flags | SYN_MUST_EXIST, &err);
+
     if (s) {
         const size_t n = s->states.count;
         const char *plural = (n == 1) ? "" : "s";
         printf("OK: loaded syntax '%s' with %zu state%s\n", s->name, n, plural);
     } else if (err == EINVAL) {
-        error_msg("%s: no default syntax found", filename);
+        error_msg(e->err, "%s: no default syntax found", filename);
     }
+
+    unsigned int nr_errs = e->err->nr_errors;
     free_editor_state(e);
-    return get_nr_errors() ? EC_DATA_ERROR : EC_OK;
+    return nr_errs ? EC_DATA_ERROR : EC_OK;
 }
 
 static ExitCode showkey_loop(unsigned int terminal_query_level)
@@ -234,7 +237,7 @@ static Buffer *init_std_buffer(EditorState *e, int fds[2])
             name = "(stdin)";
             buffer->temporary = true;
         } else {
-            error_msg("Unable to read redirected stdin");
+            error_msg(e->err, "Unable to read redirected stdin");
             buffer_remove_unlock_and_free(&e->buffers, buffer, &e->locks_ctx);
             buffer = NULL;
         }
@@ -259,14 +262,13 @@ static Buffer *init_std_buffer(EditorState *e, int fds[2])
     return buffer;
 }
 
-static bool buffer_write_blocks_and_free(Buffer *buffer, int fd)
+static int buffer_write_blocks_and_free(Buffer *buffer, int fd)
 {
-    bool r = true;
+    int err = 0;
     const Block *blk;
     block_for_each(blk, &buffer->blocks) {
         if (xwrite_all(fd, blk->data, blk->size) < 0) {
-            error_msg_errno("failed to write (stdout) buffer");
-            r = false;
+            err = errno;
             break;
         }
     }
@@ -275,7 +277,7 @@ static bool buffer_write_blocks_and_free(Buffer *buffer, int fd)
     // freed by buffer_unlock_and_free()
     free_blocks(buffer);
     free(buffer);
-    return r;
+    return err;
 }
 
 static ExitCode init_logging(const char *filename, const char *req_level_str)
@@ -442,7 +444,6 @@ int main(int argc, char *argv[])
     bool load_and_save_history = true;
     bool explicit_term_query_level = false;
     unsigned int terminal_query_level = 1;
-    errors_to_stderr(true);
 
     for (int ch; (ch = getopt(argc, argv, optstring)) != -1; ) {
         switch (ch) {
@@ -542,6 +543,7 @@ int main(int argc, char *argv[])
 
     EditorState *e = init_editor_state(headless ? EFLAG_HEADLESS : 0);
     Terminal *term = &e->terminal;
+    e->err->print_to_stderr = true;
 
     if (!headless) {
         term_init(term, getenv("TERM"), getenv("COLORTERM"));
@@ -554,7 +556,7 @@ int main(int argc, char *argv[])
     const char *cfgdir = e->user_config_dir;
     BUG_ON(!cfgdir);
     if (mkdir(cfgdir, 0755) != 0 && errno != EEXIST) {
-        error_msg("Error creating %s: %s", cfgdir, strerror(errno));
+        error_msg(e->err, "Error creating %s: %s", cfgdir, strerror(errno));
         load_and_save_history = false;
         e->options.lock_files = false;
     }
@@ -578,16 +580,16 @@ int main(int argc, char *argv[])
     }
 
     if (!headless) {
-        errors_to_stderr(false);
+        e->err->print_to_stderr = false;
         // Initialize terminal but don't update screen yet
         if (unlikely(!term_raw())) {
             return ec_error("tcsetattr", EC_IO_ERROR);
         }
-        if (get_nr_errors()) {
+        if (e->err->nr_errors) {
             // Display "Press any key to continue" prompt, if there were
             // errors while reading config files
             any_key(term, e->options.esc_timeout);
-            clear_error();
+            clear_error(e->err);
         }
     }
 
@@ -674,7 +676,7 @@ int main(int argc, char *argv[])
     ui_end(e);
 
 exit:
-    errors_to_stderr(true);
+    e->err->print_to_stderr = true;
     frame_remove(e, e->root_frame); // Unlock files and add to file history
     write_history_files(e);
 
@@ -690,9 +692,12 @@ exit:
     }
 
     if (have_stdout_buffer) {
-        bool ok = buffer_write_blocks_and_free(std_buffer, std_fds[STDOUT_FILENO]);
-        if (!ok && exit_code == EDITOR_EXIT_OK) {
-            exit_code = EC_IO_ERROR;
+        int err = buffer_write_blocks_and_free(std_buffer, std_fds[STDOUT_FILENO]);
+        if (err != 0) {
+            error_msg(e->err, "failed to write (stdout) buffer: %s", strerror(err));
+            if (exit_code == EDITOR_EXIT_OK) {
+                exit_code = EC_IO_ERROR;
+            }
         }
     }
 

src/change.c
@@ -3,9 +3,11 @@
 #include "change.h"
 #include "buffer.h"
 #include "edit.h"
+#include "editor.h"
 #include "error.h"
 #include "util/debug.h"
 #include "util/xmalloc.h"
+#include "window.h"
 
 // NOLINTBEGIN(*-avoid-non-const-global-variables)
 static ChangeMergeEnum change_merge;
@@ -232,7 +234,8 @@ bool undo(View *view)
             count++;
         }
         if (count > 1) {
-            info_msg("Undid %lu changes", count);
+            ErrorBuffer *ebuf = view->window->editor->err;
+            info_msg(ebuf, "Undid %lu changes", count);
         }
     } else {
         reverse_change(view, change);
@@ -244,12 +247,13 @@ bool undo(View *view)
 
 bool redo(View *view, unsigned long change_id)
 {
+    ErrorBuffer *ebuf = view->window->editor->err;
     Change *change = view->buffer->cur_change;
     view_reset_preferred_x(view);
     if (!change->prev) {
         // Don't complain if change_id is 0
         if (change_id) {
-            error_msg("Nothing to redo");
+            error_msg(ebuf, "Nothing to redo");
         }
         return false;
     }
@@ -261,14 +265,14 @@ bool redo(View *view, unsigned long change_id)
         change_id = nr_prev - 1;
         if (nr_prev > 1) {
             unsigned long i = change_id + 1;
-            info_msg("Redoing newest (%lu) of %lu possible changes", i, nr_prev);
+            info_msg(ebuf, "Redoing newest (%lu) of %lu possible changes", i, nr_prev);
         }
     } else {
         if (--change_id >= nr_prev) {
             if (nr_prev == 1) {
-                return error_msg("There is only 1 possible change to redo");
+                return error_msg(ebuf, "There is only 1 possible change to redo");
             }
-            return error_msg("There are only %lu possible changes to redo", nr_prev);
+            return error_msg(ebuf, "There are only %lu possible changes to redo", nr_prev);
         }
     }
 
@@ -284,7 +288,7 @@ bool redo(View *view, unsigned long change_id)
             count++;
         }
         if (count > 1) {
-            info_msg("Redid %lu changes", count);
+            info_msg(ebuf, "Redid %lu changes", count);
         }
     } else {
         reverse_change(view, change);

src/command/args.c
@@ -105,33 +105,37 @@ ArgParseError do_parse_args(const Command *cmd, CommandArgs *a)
     return 0;
 }
 
-static bool arg_parse_error_msg(const Command *cmd, const CommandArgs *a, ArgParseError err)
-{
+static bool arg_parse_error_msg (
+    struct EditorState *e,
+    const Command *cmd,
+    const CommandArgs *a,
+    ArgParseError err
+) {
     const char *name = cmd->name;
     switch (err) {
     case ARGERR_INVALID_OPTION:
-        return error_msg_for_cmd(name, "Invalid option -%c", a->flags[0]);
+        return error_msg_for_cmd(e, name, "Invalid option -%c", a->flags[0]);
     case ARGERR_TOO_MANY_OPTIONS:
-        return error_msg_for_cmd(name, "Too many options given");
+        return error_msg_for_cmd(e, name, "Too many options given");
     case ARGERR_OPTION_ARGUMENT_MISSING:
-        return error_msg_for_cmd(name, "Option -%c requires an argument", a->flags[0]);
+        return error_msg_for_cmd(e, name, "Option -%c requires an argument", a->flags[0]);
     case ARGERR_OPTION_ARGUMENT_NOT_SEPARATE:
         return error_msg_for_cmd (
-            name,
+            e, name,
             "Option -%c must be given separately because it"
             " requires an argument",
             a->flags[0]
         );
     case ARGERR_TOO_FEW_ARGUMENTS:
         return error_msg_for_cmd (
-            name,
+            e, name,
             "Too few arguments (got: %zu, minimum: %u)",
             a->nr_args,
             (unsigned int)cmd->min_args
         );
     case ARGERR_TOO_MANY_ARGUMENTS:
         return error_msg_for_cmd (
-            name,
+            e, name,
             "Too many arguments (got: %zu, maximum: %u)",
             a->nr_args,
             (unsigned int)cmd->max_args
@@ -144,8 +148,8 @@ static bool arg_parse_error_msg(const Command *cmd, const CommandArgs *a, ArgPar
     return false;
 }
 
-bool parse_args(const Command *cmd, CommandArgs *a)
+bool parse_args(struct EditorState *e, const Command *cmd, CommandArgs *a)
 {
     ArgParseError err = do_parse_args(cmd, a);
-    return likely(err == ARGERR_NONE) || arg_parse_error_msg(cmd, a, err);
+    return likely(err == ARGERR_NONE) || arg_parse_error_msg(e, cmd, a, err);
 }

src/command/args.h
@@ -96,7 +96,9 @@ static inline unsigned int cmdargs_convert_flags (
     return val;
 }
 
-bool parse_args(const Command *cmd, CommandArgs *a) NONNULL_ARGS WARN_UNUSED_RESULT;
+struct EditorState;
+
+bool parse_args(struct EditorState *e, const Command *cmd, CommandArgs *a) NONNULL_ARGS WARN_UNUSED_RESULT;
 ArgParseError do_parse_args(const Command *cmd, CommandArgs *a) NONNULL_ARGS WARN_UNUSED_RESULT;
 
 #endif

src/command/run.c
@@ -18,15 +18,17 @@ static bool run_command(CommandRunner *runner, char **av)
 {
     const CommandSet *cmds = runner->cmds;
     const Command *cmd = cmds->lookup(av[0]);
+    struct EditorState *e = runner->e;
+
     if (!cmd) {
         const char *name = av[0];
         if (!runner->lookup_alias) {
-            return error_msg("No such command: %s", name);
+            return error_msg_for_cmd(e, NULL, "No such command: %s", name);
         }
 
-        const char *alias_value = runner->lookup_alias(runner->e, name);
+        const char *alias_value = runner->lookup_alias(e, name);
         if (unlikely(!alias_value)) {
-            return error_msg("No such command or alias: %s", name);
+            return error_msg_for_cmd(e, NULL, "No such command or alias: %s", name);
         }
 
         PointerArray array = PTR_ARRAY_INIT;
@@ -34,7 +36,7 @@ static bool run_command(CommandRunner *runner, char **av)
         if (unlikely(err != CMDERR_NONE)) {
             const char *err_msg = command_parse_error_to_string(err);
             ptr_array_free(&array);
-            return error_msg("Parsing alias %s: %s", name, err_msg);
+            return error_msg_for_cmd(e, NULL, "Parsing alias %s: %s", name, err_msg);
         }
 
         // Remove NULL
@@ -51,13 +53,13 @@ static bool run_command(CommandRunner *runner, char **av)
     }
 
     if (unlikely(current_config.file && !(cmd->cmdopts & CMDOPT_ALLOW_IN_RC))) {
-        return error_msg("Command %s not allowed in config file", cmd->name);
+        return error_msg_for_cmd(e, NULL, "Command %s not allowed in config file", cmd->name);
     }
 
     // Record command in macro buffer, if recording (this needs to be done
     // before parse_args() mutates the array)
     if (runner->allow_recording && runner->cmds->macro_record) {
-        runner->cmds->macro_record(runner->e, cmd, av + 1);
+        runner->cmds->macro_record(e, cmd, av + 1);
     }
 
     // By default change can't be merged with previous one.
@@ -65,7 +67,7 @@ static bool run_command(CommandRunner *runner, char **av)
     begin_change(CHANGE_MERGE_NONE);
 
     CommandArgs a = cmdargs_new(av + 1);
-    bool r = likely(parse_args(cmd, &a)) && command_func_call(runner->e, cmd, &a);
+    bool r = likely(parse_args(e, cmd, &a)) && command_func_call(e, cmd, &a);
 
     end_change();
     return r;
@@ -75,7 +77,7 @@ static bool run_command(CommandRunner *runner, char **av)
 static bool run_commands(CommandRunner *runner, const PointerArray *array)
 {
     if (unlikely(runner->recursion_count > 16)) {
-        return error_msg("alias recursion limit reached");
+        return error_msg_for_cmd(runner->e, NULL, "alias recursion limit reached");
     }
 
     void **ptrs = array->ptrs;
@@ -119,7 +121,7 @@ bool handle_command(CommandRunner *runner, const char *cmd)
         BUG_ON(runner->recursion_count != 0);
     } else {
         const char *str = command_parse_error_to_string(err);
-        error_msg("Command syntax error: %s", str);
+        error_msg_for_cmd(runner->e, NULL, "Command syntax error: %s", str);
         r = false;
     }
     ptr_array_free(&array);

src/compiler.c
@@ -60,7 +60,7 @@ bool add_error_fmt (
             }
         }
         if (unlikely(j == n)) {
-            return error_msg("unknown substring name %s", desc[i]);
+            return error_msg_("unknown substring name %s", desc[i]);
         }
     }
 
@@ -77,7 +77,7 @@ bool add_error_fmt (
     if (unlikely(max_idx > f->re.re_nsub)) {
         regfree(&f->re);
         free(f);
-        return error_msg("invalid substring count");
+        return error_msg_("invalid substring count");
     }
 
     Compiler *compiler = find_or_add_compiler(compilers, name);

src/config.c
@@ -94,6 +94,7 @@ const BuiltinConfig *get_builtin_configs_array(size_t *nconfigs)
 
 int do_read_config(CommandRunner *runner, const char *filename, ConfigFlags flags)
 {
+    ErrorBuffer *ebuf = runner->e->err;
     const bool must_exist = flags & CFG_MUST_EXIST;
     const bool builtin = flags & CFG_BUILTIN;
 
@@ -106,6 +107,7 @@ int do_read_config(CommandRunner *runner, const char *filename, ConfigFlags flag
             exec_config(runner, cfg->text);
         } else if (must_exist) {
             error_msg (
+                ebuf,
                 "Error reading '%s': no built-in config exists for that path",
                 filename
             );
@@ -119,7 +121,7 @@ int do_read_config(CommandRunner *runner, const char *filename, ConfigFlags flag
     if (size < 0) {
         int err = errno;
         if (err != ENOENT || must_exist) {
-            error_msg("Error reading %s: %s", filename, strerror(err));
+            error_msg(ebuf, "Error reading %s: %s", filename, strerror(err));
         }
         return err;
     }

commit ebfe5abea37ba6dd32822c1c2789656025d20843

Fix `dte -s file` output, when `file` contains syntax errors
No error messages were being printed, due to a bug introduced by
commit f1ff10b74e76bb7518396286b0036c2b00c1809a.

src/main.c
@@ -89,7 +89,9 @@ static ExitCode dump_builtin_config(const char *name)
 static ExitCode lint_syntax(const char *filename, SyntaxLoadFlags flags)
 {
     EditorState *e = init_editor_state(EFLAG_HEADLESS);
+    e->err->print_to_stderr = true;
     BUG_ON(e->status != EDITOR_INITIALIZING);
+
     int err;
     const Syntax *s = load_syntax_file(e, filename, flags | SYN_MUST_EXIST, &err);
 