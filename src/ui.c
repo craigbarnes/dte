craigbarnes/dte

commit 17f532cc9565b5cc2bbc6a281e3442855f491d1a

Sanitize filenames in `OSC 2` sequences emitted by `update_term_title()`

src/ui.c
@@ -5,6 +5,7 @@
 #include "terminal/cursor.h"
 #include "terminal/ioctl.h"
 #include "util/log.h"
+#include "util/str-util.h"
 
 void set_style(Terminal *term, const StyleMap *styles, const TermStyle *style)
 {
@@ -55,20 +56,30 @@ static void update_cursor_style (
     }
 }
 
-void update_term_title(Terminal *term, const Buffer *buffer, bool set_window_title)
+void update_term_title(Terminal *term, const char *filename, bool is_modified)
 {
-    if (!set_window_title || !(term->features & TFLAG_SET_WINDOW_TITLE)) {
+    if (!(term->features & TFLAG_SET_WINDOW_TITLE)) {
         return;
     }
 
-    // FIXME: title must not contain control characters
-    TermOutputBuffer *obuf = &term->obuf;
-    const char *filename = buffer_filename(buffer);
-    term_put_literal(obuf, "\033]2;");
-    term_put_bytes(obuf, filename, strlen(filename));
-    term_put_byte(obuf, ' ');
-    term_put_byte(obuf, buffer_modified(buffer) ? '+' : '-');
-    term_put_literal(obuf, " dte\033\\");
+    // https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Miscellaneous:~:text=OSC%202%20ST
+    static const char prefix[] = "\033]2;"; // OSC 2
+    char suffix[] = " + dte\033\\"; // ST
+    suffix[1] = is_modified ? '+' : '-';
+
+    size_t filename_len = strlen(filename);
+    size_t print_max = U_MAKE_PRINTABLE_MAXLEN(filename_len);
+    size_t extra_len = sizeof(prefix) + sizeof(suffix) - 2;
+    size_t reserved = MIN(print_max + extra_len, TERM_OUTBUF_SIZE);
+    char *buf = term_output_reserve_space(&term->obuf, reserved);
+
+    // Using u_make_printable() here ensures that there are no control
+    // characters or invalid UTF-8 sequences in the emitted OSC string
+    size_t i = copystrn(buf, prefix, sizeof(prefix) - 1);
+    i += u_make_printable(filename, filename_len, buf + i, print_max, 0);
+    i += copystrn(buf + i, suffix, sizeof(suffix) - 1);
+    BUG_ON(i >= reserved);
+    term->obuf.count += i;
 }
 
 static void restore_cursor (
@@ -184,8 +195,8 @@ void update_screen(EditorState *e, const ScreenState *s)
         }
     }
 
-    if (flags & UPDATE_TERM_TITLE) {
-        update_term_title(term, buffer, options->set_window_title);
+    if ((flags & UPDATE_TERM_TITLE) && options->set_window_title) {
+        update_term_title(term, buffer_filename(buffer), buffer_modified(buffer));
     }
 
     bool is_normal_mode = (e->mode->cmds == &normal_commands);

src/options.c
@@ -139,7 +139,8 @@ static void set_window_title_changed(EditorState *e, bool global)
     Terminal *term = &e->terminal;
     if (e->options.set_window_title) {
         if (e->status == EDITOR_RUNNING) {
-            update_term_title(term, e->buffer, e->options.set_window_title);
+            const Buffer *b = e->buffer;
+            update_term_title(term, buffer_filename(b), buffer_modified(b));
         }
     } else {
         term_restore_title(term);

src/terminal/output.c
@@ -63,7 +63,7 @@ void term_output_free(TermOutputBuffer *obuf)
     };
 }
 
-static char *obuf_reserve_space(TermOutputBuffer *obuf, size_t count)
+char *term_output_reserve_space(TermOutputBuffer *obuf, size_t count)
 {
     BUG_ON(count > TERM_OUTBUF_SIZE);
     if (unlikely(obuf_avail(obuf) < count)) {
@@ -105,7 +105,7 @@ void term_put_bytes(TermOutputBuffer *obuf, const char *str, size_t count)
         return;
     }
 
-    char *buf = obuf_reserve_space(obuf, count);
+    char *buf = term_output_reserve_space(obuf, count);
     obuf->count += count;
     memcpy(buf, str, count);
 }
@@ -113,7 +113,7 @@ void term_put_bytes(TermOutputBuffer *obuf, const char *str, size_t count)
 static void term_repeat_byte(TermOutputBuffer *obuf, char ch, size_t count)
 {
     while (count) {
-        char *buf = obuf_reserve_space(obuf, 1);
+        char *buf = term_output_reserve_space(obuf, 1);
         size_t avail = obuf_avail(obuf);
         size_t n = MIN(count, avail);
         memset(buf, ch, n);
@@ -132,7 +132,7 @@ static bool ecma48_repeat_byte(TermOutputBuffer *obuf, char ch, size_t count)
     // ECMA-48 REP (CSI Pn b)
     static_assert(ECMA48_REP_MAX == 30000);
     const size_t maxlen = STRLEN("_E[30000b");
-    char *buf = obuf_reserve_space(obuf, maxlen);
+    char *buf = term_output_reserve_space(obuf, maxlen);
     size_t i = 0;
     buf[i++] = ch;
     buf[i++] = '\033';
@@ -173,7 +173,7 @@ TermSetBytesMethod term_set_bytes(Terminal *term, char ch, size_t count)
 // bytes within escape sequences without advancing the cursor position.
 void term_put_byte(TermOutputBuffer *obuf, char ch)
 {
-    char *buf = obuf_reserve_space(obuf, 1);
+    char *buf = term_output_reserve_space(obuf, 1);
     buf[0] = ch;
     obuf->count++;
 }
@@ -366,7 +366,7 @@ void term_move_cursor(TermOutputBuffer *obuf, unsigned int x, unsigned int y)
 {
     // ECMA-48 CUP (CSI Pl ; Pc H)
     const size_t maxlen = STRLEN("E[;H") + (2 * DECIMAL_STR_MAX(x));
-    char *buf = obuf_reserve_space(obuf, maxlen);
+    char *buf = term_output_reserve_space(obuf, maxlen);
     size_t i = copyliteral(buf, "\033[");
     i += buf_uint_to_str(y + 1, buf + i);
 
@@ -471,7 +471,7 @@ static const char *get_tab_str(TermTabOutputMode tab_mode)
 
 static void skipped_too_much(TermOutputBuffer *obuf, CodePoint u)
 {
-    char *buf = obuf_reserve_space(obuf, 7);
+    char *buf = term_output_reserve_space(obuf, 7);
     size_t n = obuf->x - obuf->scroll_x;
     BUG_ON(n == 0);
 
@@ -531,7 +531,7 @@ bool term_put_char(TermOutputBuffer *obuf, CodePoint u)
         return false;
     }
 
-    obuf_reserve_space(obuf, 8);
+    term_output_reserve_space(obuf, 8);
     if (likely(u < 0x80)) {
         if (likely(!ascii_iscntrl(u))) {
             obuf->buf[obuf->count++] = u;
@@ -670,7 +670,7 @@ void term_set_style(Terminal *term, TermStyle style)
 
     const size_t maxcolor = STRLEN(";38;2;255;255;255");
     const size_t maxlen = STRLEN("E[0m") + (2 * maxcolor) + (2 * ARRAYLEN(attr_map));
-    char *buf = obuf_reserve_space(&term->obuf, maxlen);
+    char *buf = term_output_reserve_space(&term->obuf, maxlen);
     size_t pos = copyliteral(buf, "\033[0");
 
     for (size_t i = 0; i < ARRAYLEN(attr_map); i++) {
@@ -703,7 +703,7 @@ void term_set_cursor_style(Terminal *term, TermCursorStyle s)
     obuf->cursor_style = s;
 
     const size_t maxlen = STRLEN("E[7 qE]12;rgb:aa/bb/ccST");
-    char *buf = obuf_reserve_space(obuf, maxlen);
+    char *buf = term_output_reserve_space(obuf, maxlen);
 
     // Set shape with DECSCUSR
     BUG_ON(s.type < 0 || s.type > 9);

src/terminal/output.h
@@ -48,6 +48,7 @@ static inline size_t obuf_avail(TermOutputBuffer *obuf)
 
 void term_output_init(TermOutputBuffer *obuf);
 void term_output_free(TermOutputBuffer *obuf);
+char *term_output_reserve_space(TermOutputBuffer *obuf, size_t count) NONNULL_ARGS_AND_RETURN;
 void term_output_reset(Terminal *term, size_t start_x, size_t width, size_t scroll_x);
 void term_put_byte(TermOutputBuffer *obuf, char ch);
 void term_put_bytes(TermOutputBuffer *obuf, const char *str, size_t count);

src/ui.h
@@ -38,7 +38,7 @@ static inline ScreenState get_screen_state(const View *view)
 
 // ui.c
 void update_screen(EditorState *e, const ScreenState *s);
-void update_term_title(Terminal *term, const Buffer *buffer, bool set_window_title);
+void update_term_title(Terminal *term, const char *filename, bool is_modified);
 void update_window_sizes(Terminal *term, Frame *frame);
 void update_screen_size(Terminal *term, Frame *root_frame);
 void set_style(Terminal *term, const StyleMap *styles, const TermStyle *style);

src/util/utf8.h
@@ -6,6 +6,10 @@
 #include "macros.h"
 #include "unicode.h"
 
+// Minimum `dest_len` value needed by u_make_printable() to guarantee
+// truncation cannot occur
+#define U_MAKE_PRINTABLE_MAXLEN(src_len) ((4 * src_len) + 1)
+
 enum {
     // Longest UTF-8 sequence (in bytes) permitted by RFC 3629
     // (maximum number of bytes written by u_set_char_raw())
@@ -58,7 +62,8 @@ static inline size_t u_char_size(CodePoint u)
  * otherwise byte-wise escaped. This is similar in purpose to the
  * BSD strnvisx(3) function, but produces a truncated string if the
  * destination buffer has insufficient space. If `dest_len` is at
- * least `(4 * src_len) + 1`, truncation can never happen.
+ * least `U_MAKE_PRINTABLE_MAXLEN(src_len)`, truncation can never
+ * happen.
  */
 static inline size_t u_make_printable (
     const char *restrict src,

commit d755b7564e62531d7b4505cec7e89978d42102c1

Fix possible buffer overflow in `update_term_title()`
The previous `dest_len` passed to `u_make_printable()` was potentially
greater than the reserved space (for very long filenames), which could
result in a buffer overflow. The code in question has never been in a
versioned release, however.

Fixes bug introduced by: 17f532cc9565b5cc2bbc6a281e3442855f491d1a

src/ui.c
@@ -76,7 +76,7 @@ void update_term_title(Terminal *term, const char *filename, bool is_modified)
     // Using u_make_printable() here ensures that there are no control
     // characters or invalid UTF-8 sequences in the emitted OSC string
     size_t i = copystrn(buf, prefix, sizeof(prefix) - 1);
-    i += u_make_printable(filename, filename_len, buf + i, print_max, 0);
+    i += u_make_printable(filename, filename_len, buf + i, reserved - extra_len, 0);
     i += copystrn(buf + i, suffix, sizeof(suffix) - 1);
     BUG_ON(i >= reserved);
     term->obuf.count += i;

test/terminal.c
@@ -10,6 +10,7 @@
 #include "terminal/rxvt.h"
 #include "terminal/style.h"
 #include "terminal/terminal.h"
+#include "ui.h" // update_term_title()
 #include "util/bit.h"
 #include "util/str-array.h"
 #include "util/unicode.h"
@@ -1717,6 +1718,36 @@ static void test_term_put_level_1_queries(TestContext *ctx)
     EXPECT_EQ(obuf->x, 0);
 }
 
+static void test_update_term_title(TestContext *ctx)
+{
+    static const char prefix[] = "\033]2;";
+    static const char suffix[] = " - dte\033\\";
+    size_t plen = sizeof(prefix) - 1;
+    size_t slen = sizeof(suffix) - 1;
+    Terminal term = {.obuf = TERM_OUTPUT_INIT, .features = TFLAG_SET_WINDOW_TITLE};
+    TermOutputBuffer *obuf = &term.obuf;
+
+    static const char expected[] = "\033]2;example filename - dte\033\\";
+    update_term_title(&term, "example filename", false);
+    EXPECT_MEMEQ(obuf->buf, obuf->count, expected, sizeof(expected) - 1);
+    EXPECT_EQ(obuf->x, 0);
+    obuf->count = 0;
+
+    // Very long filename, for edge case coverage
+    char filename[TERM_OUTBUF_SIZE];
+    memset(filename, 'a', sizeof(filename));
+    filename[sizeof(filename) - 1] = '\0';
+    update_term_title(&term, filename, false);
+
+    size_t tlen = obuf->count - (plen + slen);
+    EXPECT_EQ(sizeof(filename), 8192);
+    EXPECT_EQ(obuf->count, 8188);
+    EXPECT_EQ(tlen, 8176);
+    EXPECT_MEMEQ(obuf->buf, plen, prefix, plen);
+    EXPECT_MEMEQ(obuf->buf + plen, tlen, filename, tlen);
+    EXPECT_MEMEQ(obuf->buf + plen + tlen, slen, suffix, slen);
+}
+
 static void test_is_newly_detected_feature(TestContext *ctx)
 {
     EXPECT_FALSE(is_newly_detected_feature(1, 1, 1));
@@ -1758,6 +1789,7 @@ static const TestEntry tests[] = {
     TEST(test_term_restore_cursor_style),
     TEST(test_term_begin_sync_update),
     TEST(test_term_put_level_1_queries),
+    TEST(test_update_term_title),
     TEST(test_is_newly_detected_feature),
 };
 