craigbarnes/dte

commit 3654a57ae113c209b44edfb67ad79c1b12ee3619

Run `dte -c` commands in headless mode
This prevents them from reading from or writing to the terminal,
including via commands like e.g. `replace -c`. This is done so that
running e.g. `dte -c make` can't consume/discard responses to the
terminal queries we emit. There are too many edge cases to allow
`dte -c` interfere with the terminal before it's been properly
initialized.

src/main.c
@@ -553,7 +553,11 @@ int main(int argc, char *argv[])
         set_basic_signal_dispositions();
     }
 
-    EditorState *e = init_editor_state(headless ? EFLAG_HEADLESS : 0);
+    // Note that we set EFLAG_HEADLESS here, regardless of whether -C was used,
+    // because the terminal isn't properly initialized until later and we don't
+    // want commands running via -c or -C interacting with it
+    EditorState *e = init_editor_state(EFLAG_HEADLESS);
+
     Terminal *term = &e->terminal;
     e->err.print_to_stderr = true;
 
@@ -629,7 +633,10 @@ int main(int argc, char *argv[])
     View *dview = window->views.count ? NULL : window_open_empty_buffer(window);
 
     set_view(window_get_first_view(window));
-    ui_first_start(e, terminal_query_level);
+
+    if (!headless) {
+        update_screen_size(term, e->root_frame);
+    }
 
     if (nr_commands) {
         for (size_t i = 0; i < nr_commands; i++) {
@@ -665,11 +672,8 @@ int main(int argc, char *argv[])
     set_fatal_signal_handlers();
     set_sigwinch_handler();
 
-    if (nr_commands + nr_tags > 0) {
-        const ScreenState s = get_screen_state(e->view);
-        update_screen(e, &s);
-    }
-
+    e->flags &= ~EFLAG_HEADLESS; // See comment for init_editor_state() call above
+    ui_first_start(e, terminal_query_level);
     main_loop(e);
     term_restore_title(term);
 

src/editor.c
@@ -284,10 +284,7 @@ void ui_start(EditorState *e)
 // ui_start() + term_put_initial_queries() + term_output_flush()).
 void ui_first_start(EditorState *e, unsigned int terminal_query_level)
 {
-    if (e->flags & EFLAG_HEADLESS) {
-        return;
-    }
-
+    BUG_ON(e->flags & EFLAG_HEADLESS);
     Terminal *term = &e->terminal;
 
     // The order of these calls is important; see ui_start()

commit 28d8bed452cc284ab5372d8d556693d565e56a27

Fix recent bug causing history files to not be saved
Fixes bug introduced by: 3654a57ae113c209b44edfb67ad79c1b12ee3619
(from ~90 minutes ago)

src/main.c
@@ -382,7 +382,7 @@ static void exec_user_rc(EditorState *e, const char *filename)
     read_normal_config(e, filename, flags);
 }
 
-static void read_history_files(EditorState *e)
+static void read_history_files(EditorState *e, bool headless)
 {
     const char *dir = e->user_config_dir;
     size_t size_limit = 64u << 20; // 64 MiB
@@ -392,7 +392,7 @@ static void read_history_files(EditorState *e)
 
     // There's not much sense in saving history for headless sessions, but we
     // do still load it (above), to make it available to e.g. `exec -i command`
-    if (!(e->flags & EFLAG_HEADLESS)) {
+    if (!headless) {
         e->flags |= EFLAG_ALL_HIST;
     }
 
@@ -592,7 +592,7 @@ int main(int argc, char *argv[])
     e->root_frame = new_root_frame(window);
 
     if (load_and_save_history) {
-        read_history_files(e);
+        read_history_files(e, headless);
     }
 
     if (!headless) {