8ebb237b5950091d01d75423890d1f3617304f3c
Craig Barnes <craigbarnes@protonmail.com>
2025-05-07 17:29:50 +0100

    Always disable `#embed` usage for now, to work around `ccache` bug
    See also:
    
    * https://github.com/ccache/ccache/issues/1540#issuecomment-2701575061
    * Commit ee282c927c58e7cd2d13f9044883087ecdb79923
    * Commit f6575d4fa303f949f4c0eb29ed632643f39fd018

--- mk/feature-test/embed.c
+++ mk/feature-test/embed.c
@@ -1,3 +1,7 @@
+// ccache currently produces stale objects when #embed is used, so deliberately
+// error out for now
+#error "See: https://github.com/ccache/ccache/issues/1540#issuecomment-2701575061"
+
 #if !__has_embed("config/syntax/dte")
     #error "embed file not found; see '--embed-dir' in mk/compiler.sh"
 #endif
