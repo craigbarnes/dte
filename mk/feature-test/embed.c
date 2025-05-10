cb0730145717a9b9902be3eb4fdc3a5ffbcec5d1
Craig Barnes <craigbarnes@protonmail.com>
2025-05-10 21:29:41 +0100

    Make C23 `#embed` feature test harder on the compiler
    To weed out buggy/incomplete implementations.

--- mk/feature-test/embed.c
+++ mk/feature-test/embed.c
@@ -8,10 +8,11 @@
 
 // See: ISO C23 §6.10.3 "Binary resource inclusion"
 static const char a[] = {
-    #embed "config/syntax/dte" limit(8)
+    #embed "config/syntax/dte" limit(9)
 };
 
 int main(void)
 {
-    return !(sizeof(a) == 8 && a[0] == '1');
+    static_assert(sizeof(a) == 9);
+    return !(a[2] == 'q' && a[8] == 'c');
 }
