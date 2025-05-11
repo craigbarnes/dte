7c734219a33e05078dfc440119d3361d180cf9a2
Craig Barnes <craigbarnes@protonmail.com>
2025-05-11 23:13:56 +0100

    Add comment above `HASH` variable in `mk/util.mk` to clarify its purpose

--- mk/util.mk
+++ mk/util.mk
@@ -1,7 +1,10 @@
-HASH := \#
 streq = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
 prefix-obj = $(addprefix $(1), $(addsuffix .o, $(2)))
 echo-if-set = $(foreach var, $(1), $(if $($(var)), $(var)))
+
+# https://www.gnu.org/software/make/manual/html_node/Makefile-Contents.html#:~:text=e.g.%2C%20%5C%23
+# https://lists.gnu.org/archive/html/make-w32/2020-01/msg00001.html#:~:text=H%20%3A%3D%20%5C%23
+HASH := \#
 
 MAKEFLAGS += -r
 MAKE_S = $(findstring s,$(firstword -$(MAKEFLAGS)))
