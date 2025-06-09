craigbarnes/dte

commit 3f9f84937bfa2c2fa10bd721058b89f99f5ea7b9

Don't enter `main_loop()` if a `dte -c` command has already `quit`
This commit is more or less reverting 2f3e3793d (and thus un-reverting
fec217333), but done in the context of 3654a57ae (which was committed
more recently and helps avoid the edge cases mentioned in 2f3e3793d).

The patch here is somewhat different to fec217333, because some related
parts of `main()` were reorganized for "headless" mode (in 3dcea9ae7).

See also:

* The comment below `fast_exit` added to `src/main.c`
* Commit fec217333bf8e9de1a5c77a8aa851b954d486cac
* Commit 2f3e3793d607c2607d7afa1139fa10c738c9b25f
* Commit 3654a57ae113c209b44edfb67ad79c1b12ee3619
* Commit 3dcea9ae7f6aa62d86e99fe02a20981c4ba212bb

src/main.c
@@ -391,12 +391,19 @@ static void lookup_tags (
 }
 
 static View *open_initial_buffers (
-    Window *window,
+    EditorState *e,
     Buffer *std_buffer,
     char *args[],
-    size_t len
+    size_t len,
+    bool headless
 ) {
+    // Don't print error messages to `stderr`, unless in headless mode
+    const unsigned int nerrs_save = e->err.nr_errors;
+    const bool ptse_save = e->err.print_to_stderr;
+    e->err.print_to_stderr = headless;
+
     // Open files specified as command-line arguments
+    Window *window = e->window;
     for (size_t i = 0, line = 0, col = 0; i < len; i++) {
         const char *str = args[i];
         if (line == 0 && *str == '+' && str_to_filepos(str + 1, &line, &col)) {
@@ -418,8 +425,14 @@ static View *open_initial_buffers (
 
     // Open default buffer, if none were opened above
     View *dview = window->views.count ? NULL : window_open_empty_buffer(window);
-
     set_view(dview ? dview : window_get_first_view(window));
+
+    // Restore `nr_errors` from the value saved above, so that any
+    // errors not printed to stderr are also excluded from the
+    // `any_key()` condition in `main()`.
+    e->err.nr_errors = nerrs_save;
+    e->err.print_to_stderr = ptse_save;
+
     return dview;
 }
 
@@ -573,48 +586,36 @@ int main(int argc, char *argv[])
 
     exec_rc_files(e, rc, read_rc);
 
-    Window *window = new_window(e);
-    e->window = window;
-    e->root_frame = new_root_frame(window);
+    e->window = new_window(e);
+    e->root_frame = new_root_frame(e->window);
 
     if (load_and_save_history) {
         read_history_files(e, headless);
     }
 
-    if (!headless) {
-        e->err.print_to_stderr = false;
-        // Initialize terminal but don't update screen yet
-        if (unlikely(!term_raw())) {
-            return ec_error("tcsetattr", EC_IO_ERROR);
-        }
-        if (e->err.nr_errors) {
-            // Display "Press any key to continue" prompt, if there were
-            // errors while reading config files
-            any_key(term, e->options.esc_timeout);
-            clear_error(&e->err);
-        }
-    }
-
     e->status = EDITOR_RUNNING;
-
-    BUG_ON(optind > argc);
-    argc -= optind;
-    argv += optind;
-    View *dview = open_initial_buffers(window, std_buffer, argv, argc);
+    char **files = argv + optind;
+    size_t nfiles = argc - optind;
+    View *dview = open_initial_buffers(e, std_buffer, files, nfiles, headless);
 
     if (!headless) {
         update_screen_size(term, e->root_frame);
     }
 
-    if (nr_commands) {
-        for (size_t i = 0; i < nr_commands; i++) {
-            handle_normal_command(e, commands[i], false);
-        }
-        // Refresh Window pointer; in case `wsplit` opened a new one
-        window = e->window;
+    for (size_t i = 0; i < nr_commands; i++) {
+        handle_normal_command(e, commands[i], false);
     }
 
-    if (headless) {
+    bool fast_exit = e->status != EDITOR_RUNNING;
+    if (headless || fast_exit) {
+        // Headless mode implicitly exits after commands have finished
+        // running. We may also exit here (without drawing the UI,
+        // emitting terminal queries or entering main_loop()) if a `-c`
+        // command has already quit the editor. This prevents commands
+        // like e.g. `dte -HR -cquit` from emitting terminal queries
+        // and exiting before the responses have been read (which would
+        // typically cause the parent shell process to read them
+        // instead).
         goto exit;
     }
 
@@ -624,6 +625,19 @@ int main(int argc, char *argv[])
     set_fatal_signal_handlers();
     set_sigwinch_handler();
 
+    if (unlikely(!term_raw())) {
+        e->status = ec_error("tcsetattr", EC_IO_ERROR);
+        fast_exit = true;
+        goto exit;
+    }
+
+    if (e->err.nr_errors) {
+        // Display "press any key to continue" prompt for stderr errors
+        any_key(term, e->options.esc_timeout);
+        clear_error(&e->err);
+    }
+
+    e->err.print_to_stderr = false;
     e->flags &= ~EFLAG_HEADLESS; // See comment for init_editor_state() call above
     ui_first_start(e, terminal_query_level);
     main_loop(e);
@@ -646,11 +660,9 @@ exit:
     e->err.print_to_stderr = true;
     frame_remove(e, e->root_frame); // Unlock files and add to file history
     write_history_files(e);
+    int exit_code = headless ? MAX(e->status, EDITOR_EXIT_OK) : e->status;
 
-    int exit_code = e->status;
-    if (headless) {
-        exit_code = MAX(exit_code, EDITOR_EXIT_OK);
-    } else {
+    if (!headless && !fast_exit) {
         // This must be done before calling buffer_write_blocks_and_free(),
         // since output modes need to be restored to get proper line ending
         // translation and std_fds[STDOUT_FILENO] may be a pipe to the

src/editor.c
@@ -196,6 +196,15 @@ void clear_all_messages(EditorState *e)
 
 void free_editor_state(EditorState *e)
 {
+    size_t n = e->terminal.obuf.count;
+    if (n) {
+        LOG_DEBUG("%zu unflushed bytes in terminal output buffer", n);
+    }
+    n = e->terminal.ibuf.len;
+    if (n) {
+        LOG_DEBUG("%zu unprocessed bytes in terminal input buffer", n);
+    }
+
     free(e->clipboard.buf);
     free_file_options(&e->file_options);
     free_filetypes(&e->filetypes);

commit 7fecd9214f667f307ebdba9a0b30ebe3ccb37939

Clean up `main()` and don't call `any_key()` for e.g. `dte -t bad-tag`
Not showing "press any key" for `dte -t` errors had always been the
case until 3f9f84937bfa2c inadvertently changed/regressed it.

src/command/error.c
@@ -39,6 +39,7 @@ static void error_msgv(ErrorBuffer *eb, const char *format, va_list ap)
     if (eb->print_to_stderr) {
         xfputs(eb->buf, stderr);
         xfputc('\n', stderr);
+        eb->stderr_errors_printed = true;
     }
 
     eb->is_error = true;

src/command/error.h
@@ -12,6 +12,7 @@ typedef struct ErrorBuffer {
     unsigned int nr_errors;
     bool is_error;
     bool print_to_stderr;
+    bool stderr_errors_printed;
 } ErrorBuffer;
 
 bool error_msg(ErrorBuffer *eb, const char *format, ...) COLD PRINTF(2) NONNULL_ARGS;

src/main.c
@@ -367,14 +367,8 @@ static View *open_initial_buffers (
     EditorState *e,
     Buffer *std_buffer,
     char *args[],
-    size_t len,
-    bool headless
+    size_t len
 ) {
-    // Don't print error messages to `stderr`, unless in headless mode
-    const unsigned int nerrs_save = e->err.nr_errors;
-    const bool ptse_save = e->err.print_to_stderr;
-    e->err.print_to_stderr = headless;
-
     // Open files specified as command-line arguments
     Window *window = e->window;
     for (size_t i = 0, line = 0, col = 0; i < len; i++) {
@@ -398,14 +392,8 @@ static View *open_initial_buffers (
 
     // Open default buffer, if none were opened above
     View *dview = window->views.count ? NULL : window_open_empty_buffer(window);
-    set_view(dview ? dview : window_get_first_view(window));
 
-    // Restore `nr_errors` from the value saved above, so that any
-    // errors not printed to stderr are also excluded from the
-    // `any_key()` condition in `main()`.
-    e->err.nr_errors = nerrs_save;
-    e->err.print_to_stderr = ptse_save;
-
+    set_view(window_get_first_view(window));
     return dview;
 }
 
@@ -535,12 +523,11 @@ int main(int argc, char *argv[])
     EditorState *e = init_editor_state(EFLAG_HEADLESS);
 
     Terminal *term = &e->terminal;
-    e->err.print_to_stderr = true;
-
     if (!headless) {
         term_init(term, getenv("TERM"), getenv("COLORTERM"));
     }
 
+    e->err.print_to_stderr = true;
     Buffer *std_buffer = init_std_buffer(e, std_fds);
     bool have_stdout_buffer = std_buffer && std_buffer->stdout_buffer;
 
@@ -562,14 +549,15 @@ int main(int argc, char *argv[])
         read_history_files(e, headless);
     }
 
-    e->status = EDITOR_RUNNING;
-    char **files = argv + optind;
-    size_t nfiles = argc - optind;
-    View *dview = open_initial_buffers(e, std_buffer, files, nfiles, headless);
+    e->err.print_to_stderr = headless;
+    View *dview = open_initial_buffers(e, std_buffer, argv + optind, argc - optind);
+    e->err.print_to_stderr = true;
 
     if (!headless) {
         update_screen_size(term, e->root_frame);
     }
+
+    e->status = EDITOR_RUNNING;
 
     for (size_t i = 0; i < nr_commands; i++) {
         handle_normal_command(e, commands[i], false);
@@ -588,6 +576,7 @@ int main(int argc, char *argv[])
         goto exit;
     }
 
+    e->err.print_to_stderr = headless;
     lookup_tags(e, tags, nr_tags, nr_commands ? NULL : dview);
 
     set_fatal_error_cleanup_handler(cleanup_handler, e);
@@ -600,13 +589,12 @@ int main(int argc, char *argv[])
         goto exit;
     }
 
-    if (e->err.nr_errors) {
+    if (e->err.stderr_errors_printed) {
         // Display "press any key to continue" prompt for stderr errors
         any_key(term, e->options.esc_timeout);
         clear_error(&e->err);
     }
 
-    e->err.print_to_stderr = false;
     e->flags &= ~EFLAG_HEADLESS; // See comment for init_editor_state() call above
     ui_first_start(e, terminal_query_level);
     main_loop(e);

test/init.c
@@ -201,6 +201,8 @@ static void test_deinit(TestContext *ctx)
     EXPECT_NULL(e->buffer->abs_filename);
     EXPECT_PTREQ(e->buffer, e->buffers.ptrs[0]);
     EXPECT_FALSE(e->child_controls_terminal);
+    EXPECT_FALSE(e->err.print_to_stderr);
+    EXPECT_FALSE(e->err.stderr_errors_printed);
     EXPECT_EQ(e->flags, EFLAG_HEADLESS);
     EXPECT_EQ(e->include_recursion_count, 0);
     EXPECT_FALSE(e->options.lock_files);