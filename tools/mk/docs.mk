8f3d338be22e60a8af23c1df5c6a8c3e5db37240
Craig Barnes <craigbarnes@protonmail.com>
2025-06-19 21:16:24 +0100

    Add 2 more files to `make html-aux` target
    This is purely for development purposes.

--- tools/mk/docs.mk
+++ tools/mk/docs.mk
@@ -1,15 +1,26 @@
 manpage-names = $(basename $(notdir $(man)))
 manpage-date-targets = $(addprefix manpage-date-, $(manpage-names))
 last-commit-date = git log -1 --format='%ad' --date='format:%B %Y'
-html-aux = public/contributing.html public/TODO.html public/releasing.html
+html-aux-basenames = contributing packaging releasing TODO contrib-scripts
+html-aux = $(foreach f, $(html-aux-basenames), public/$(f).html)
+
+# These are miscellaneous Markdown files dotted around the repo and
+# aren't intended to be part of the public website. `make html-aux`
+# can be used to render them as HTML, but only for proof-reading
+# purposes.
+public/contributing.html: docs/contributing.md
+public/TODO.html: docs/TODO.md
+public/releasing.html: docs/releasing.md
+public/packaging.html: docs/packaging.md
+public/contrib-scripts.html: contrib/README.md
 
 html-aux: $(html-aux)
 htmlgz-aux: $(patsubst %, %.gz, $(html-aux))
 bump-manpage-dates: $(manpage-date-targets)
 
-$(html-aux): public/%.html: docs/%.md docs/template.html build/docs/pdhtml.flags | $(css) $(img_tpl)
+$(html-aux): public/%.html: docs/template.html build/docs/pdhtml.flags | $(css) $(img_tpl)
 	$(E) PANDOC $@
-	$(Q) $(PDHTML) --metadata title='dte - $(basename $(notdir $@))' -o $@ $<
+	$(Q) $(PDHTML) --metadata title='dte - $(basename $(notdir $@))' -o $@ $(filter %.md, $^)
 
 # Note that sed -i'' isn't specified by POSIX, but it's supported by
 # at least GNU sed, busybox sed and *BSD sed
