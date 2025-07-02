47dcd01bd570c3c7ce6fff7c92f1f891de0a2466
Craig Barnes <craigbarnes@protonmail.com>
2025-07-02 12:06:17 +0100

    Include POSIX specification link in `fexecve(3)` feature test

--- mk/feature-test/fexecve.c
+++ mk/feature-test/fexecve.c
@@ -7,6 +7,7 @@
  Standardized by: POSIX 2008
 
  See also:
+ • https://pubs.opengroup.org/onlinepubs/9799919799/functions/fexecve.html#:~:text=fexecve()%20function%20is%20added
  • https://man7.org/linux/man-pages/man3/fexecve.3.html
  • https://man.freebsd.org/cgi/man.cgi?query=fexecve#:~:text=int-,fexecve
  • https://man.netbsd.org/fexecve.2#:~:text=int-,fexecve
