fe9c84bb336bc4d6e8fb3df1394a3a857e979dc2
Craig Barnes <craigbarnes@protonmail.com>
2025-07-14 11:10:44 +0100

    Add `make iwyu` and `make iwyu-remove-only`, for `#include` linting
    The `make iwyu` target runs `include-what-you-use(1)` on all C source
    files in the repository and dumps the raw output to stdout, whereas
    `make iwyu-remove-only` filters the output to show only the "should be
    removed" items (in a format suitable for dte's built-in `make` alias).
    
    See also:
    
    * https://include-what-you-use.org/
    * https://github.com/include-what-you-use/include-what-you-use

---
+++ tools/iwyu-filter.awk
@@ -0,0 +1,17 @@
+#!/usr/bin/awk -f
+
+/^(include-what-you-use|.*ERROR):/ {
+    print > "/dev/stderr"
+}
+
+/ should remove these lines:$/ {
+    file = $1
+}
+
+/^- *#include .* \/\/ lines [0-9]+-[0-9]+$/ {
+    ref = "// lines "
+    start = index($0, ref) + length(ref)
+    end = index(substr($0, start), "-") - 1
+    lineno = substr($0, start, end)
+    print file ":" lineno ": " $3 " include should be removed"
+}
--- tools/mk/lint.mk
+++ tools/mk/lint.mk
@@ -8,6 +8,10 @@ GAWK ?= gawk
 AWKLINT = >/dev/null $(GAWK) --lint=fatal --posix
 SPATCH ?= spatch
 SPATCHFLAGS ?= --very-quiet
+IWYU ?= include-what-you-use
+IWYU_FILTER = cat
+IWYUFLAGS ?= -Xiwyu --max_line_length=120
+IWYUCFLAGS = -Isrc -Itools/mock-headers -Wno-pointer-sign
 GITATTRS = $(shell git ls-files --error-unmatch $(foreach A, $(1), ':(attr:$A)'))
 DOCFILES = $(call GITATTRS, xml markdown)
 SHELLSCRIPTS = $(call GITATTRS, shell)
@@ -15,14 +19,16 @@ SPACE_INDENTED_FILES = $(call GITATTRS, space-indent)
 MK_FILES = $(call GITATTRS, make)
 # TODO: Re-enable `wrap`, after diagnosing/fixing the extreme slowness
 SPATCH_NAMES = arraylen minmax tailcall perf pitfalls stat-mtime staticbuf
+
 check_coccinelle_targets = $(addprefix check-coccinelle-, $(SPATCH_NAMES))
 check_awk_targets = $(addprefix check-awk-, $(AWK_FILES))
+iwyu_targets = $(addprefix iwyu-, $(all_sources))
 
 AWK_FILES = \
     config/script/longest-line.awk \
     mk/config2c.awk \
     tools/git-hooks/commit-msg \
-    $(foreach f, cat gcovr-txt hdrcheck mkcheck wscheck, tools/$(f).awk)
+    $(foreach f, cat gcovr-txt hdrcheck iwyu-filter mkcheck wscheck, tools/$(f).awk)
 
 check-awk-config/script/longest-line.awk: src/msg.c
 check-awk-mk/config2c.awk: config/rc
@@ -30,23 +36,29 @@ check-awk-tools/cat.awk: src/msg.c src/msg.h
 check-awk-tools/gcovr-txt.awk: tools/test/gcovr-report.txt
 check-awk-tools/git-hooks/commit-msg: tools/test/COMMIT_EDITMSG
 check-awk-tools/hdrcheck.awk: src/msg.c src/msg.h
+check-awk-tools/iwyu-filter.awk: tools/test/iwyu.txt
 check-awk-tools/mkcheck.awk: mk/build.mk
 check-awk-tools/wscheck.awk: src/msg.c src/msg.h
+iwyu-remove-only: IWYU_FILTER = tools/iwyu-filter.awk >&2
 
 check-all: check-source check-aux check distcheck check-clang-tidy
 check-source: $(addprefix check-, whitespace headers makefiles codespell shell awk)
 check-aux: check-desktop-file check-appstream
 check-awk: $(check_awk_targets)
 check-coccinelle: $(check_coccinelle_targets)
+iwyu iwyu-remove-only: $(iwyu_targets)
 
 $(check_awk_targets): check-awk-%:
 	$(E) AWKLINT '$*'
-	$(Q) $(AWKLINT) -f '$*' $^
+	$(Q) $(AWKLINT) -f '$*' $(or $^, $(error No input for target: $@))
 
 $(check_coccinelle_targets): check-coccinelle-%:
 	$(E) SPATCH 'tools/coccinelle/$*.cocci'
 	$(Q) $(SPATCH) $(SPATCHFLAGS) --sp-file 'tools/coccinelle/$*.cocci' \
 	     $(all_sources) 2>&1 | sed '/egrep is obsolescent/d'
+
+$(iwyu_targets): iwyu-%:
+	$(Q) $(IWYU) $(IWYUFLAGS) $(IWYUCFLAGS) '$*' 2>&1 | $(IWYU_FILTER)
 
 check-shell:
 	$(E) SHCHECK '*.sh *.bash $(filter-out %.sh %.bash, $(SHELLSCRIPTS))'
@@ -81,7 +93,7 @@ check-appstream:
 
 
 .PHONY: \
-    $(check_awk_targets) $(check_coccinelle_targets) \
+    $(check_awk_targets) $(check_coccinelle_targets) $(iwyu_targets) \
     $(addprefix check-, all source aux coccinelle shell awk whitespace) \
     $(addprefix check-, headers makefiles codespell typos desktop-file) \
     $(addprefix check-, appstream)
