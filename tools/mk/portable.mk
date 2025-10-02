dcbe5d20d86752b77c5189b82f7b32c00f896b1e
Craig Barnes <craigbarnes@protonmail.com>
2025-10-02 11:50:48 +0100

    Generate portable build for AArch64, as part of GitLab CI `pages` job
    In addition to the existing build for x86_64.
    
    Note that the "Portable Builds for Linux" section of `CHANGELOG.md`
    (and the "Releases" page of the website) will be updated in follow-up
    commits.

--- tools/mk/portable.mk
+++ tools/mk/portable.mk
@@ -1,11 +1,43 @@
-MUSLGCC ?= ccache musl-gcc
+# This makefile is used to generate the "Portable Builds" mentioned
+# in CHANGELOG.md (which is also the "Releases" page on the website)
 
-portable: private TARBALL = dte-$(VERSION)-$(shell uname -sm | tr 'A-Z ' a-z-).tar.gz
-portable: private MAKEFLAGS += --no-print-directory
-portable: $(man)
-	$(Q) $(MAKE) CC='$(MUSLGCC)' CFLAGS='-O2 -pipe -flto' LDFLAGS='-static -s' NO_CONFIG_MK=1
+# These 2 compilers can be found in the `musl` and `musl-aarch64`
+# packages on Arch Linux
+MUSLGCC ?= ccache musl-gcc
+MUSLGCC_AARCH64 ?= ccache aarch64-linux-musl-gcc
+
+PORTABLE_MAKEFLAGS = \
+    --no-print-directory \
+    CFLAGS='-O2 -pipe -flto' \
+    LDFLAGS='-static -s' \
+    NO_CONFIG_MK=1 \
+    SANE_WCTYPE=1 \
+    DEBUG=0
+
+define PORTABLE_SYMLINK
+	$(E) SYMLINK 'dte-master-linux-$(1).tar.gz'
+	$(Q) ln -sf 'dte-$(VERSION)-linux-$(1).tar.gz' 'dte-master-linux-$(1).tar.gz'
+endef
+
+portable: portable-x86_64 portable-aarch64
+	$(call PORTABLE_SYMLINK,x86_64)
+	$(call PORTABLE_SYMLINK,aarch64)
+
+portable-x86_64: private TARBALL = dte-$(VERSION)-linux-x86_64.tar.gz
+portable-x86_64: $(man)
+	$(Q) test "`uname -sm`" = 'Linux x86_64'
+	$(Q) $(MAKE) vvars all $(PORTABLE_MAKEFLAGS) CC='$(MUSLGCC)'
 	$(E) ARCHIVE '$(TARBALL)'
 	$(Q) tar -czf '$(TARBALL)' $(dte) $^
 
 
-.PHONY: portable
+portable-aarch64: private TARBALL = dte-$(VERSION)-linux-aarch64.tar.gz
+portable-aarch64: $(man)
+	$(Q) test "`uname -s`" = Linux
+	$(Q) $(MAKE) vvars all $(PORTABLE_MAKEFLAGS) CC='$(MUSLGCC_AARCH64)'
+	$(E) ARCHIVE '$(TARBALL)'
+	$(Q) tar -czf '$(TARBALL)' $(dte) $^
+
+
+NON_PARALLEL_TARGETS += portable portable-x86_64 portable-aarch64
+.PHONY: portable portable-x86_64 portable-aarch64
