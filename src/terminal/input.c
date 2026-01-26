craigbarnes/dte

commit 6061d49d12505ff45fe1c54dc6fc166542f6cf86

Simplify `term_parse_sequence()` return value

src/terminal/input.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include "input.h"
 #include "output.h"
+#include "parse.h"
 #include "trace.h"
 #include "util/ascii.h"
 #include "util/bit.h"
@@ -102,20 +103,15 @@ static KeyCode read_special(Terminal *term)
 {
     TermInputBuffer *input = &term->ibuf;
     KeyCode key;
-    ssize_t len = term->parse_input(input->buf, input->len, &key);
-    if (likely(len > 0)) {
-        // Match
-        consume_input(input, len);
-        return key;
+    size_t len = term->parse_input(input->buf, input->len, &key);
+
+    if (unlikely(len == TPARSE_PARTIAL_MATCH)) {
+        bool retry = input->can_be_truncated && fill_buffer(input);
+        return retry ? read_special(term) : KEY_NONE;
     }
 
-    if (len < 0 && input->can_be_truncated && fill_buffer(input)) {
-        // Possibly truncated
-        return read_special(term);
-    }
-
-    // No match
-    return KEY_NONE;
+    consume_input(input, len);
+    return key;
 }
 
 /*

src/terminal/linux.c
@@ -2,7 +2,7 @@
 #include "parse.h"
 #include "util/xstring.h"
 
-ssize_t linux_parse_key(const char *buf, size_t length, KeyCode *k)
+size_t linux_parse_key(const char *buf, size_t length, KeyCode *k)
 {
     if (length < 3 || !mem_equal(buf, "\033[[", 3)) {
         return term_parse_sequence(buf, length, k);

src/terminal/linux.h
@@ -1,10 +1,10 @@
 #ifndef TERMINAL_LINUX_H
 #define TERMINAL_LINUX_H
 
-#include <sys/types.h>
+#include <stddef.h>
 #include "key.h"
 #include "util/macros.h"
 
-ssize_t linux_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT;
+size_t linux_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT WRITEONLY(3);
 
 #endif

src/terminal/parse.c
@@ -240,7 +240,7 @@ static KeyCode normalize_csi_27_tilde_keycode(KeyCode mods, KeyCode key)
     return mods | key;
 }
 
-static ssize_t parse_ss3(const char *buf, size_t length, size_t i, KeyCode *k)
+static size_t parse_ss3(const char *buf, size_t length, size_t i, KeyCode *k)
 {
     if (unlikely(i >= length)) {
         return TPARSE_PARTIAL_MATCH;
@@ -413,7 +413,7 @@ exit_loop:
     return i;
 }
 
-static ssize_t parse_csi(const char *buf, size_t len, size_t i, KeyCode *k)
+static size_t parse_csi(const char *buf, size_t len, size_t i, KeyCode *k)
 {
     TermControlParams csi = {.nparams = 0};
     bool maybe_query_reply = (i < len && buf[i] >= '<' && buf[i] <= '?');
@@ -548,7 +548,7 @@ ignore:
     return i;
 }
 
-static ssize_t parse_osc(const char *buf, size_t len, size_t i, KeyCode *k)
+static size_t parse_osc(const char *buf, size_t len, size_t i, KeyCode *k)
 {
     char data[4096];
     for (size_t pos = 0; i < len; ) {
@@ -578,7 +578,7 @@ static ssize_t parse_osc(const char *buf, size_t len, size_t i, KeyCode *k)
     return TPARSE_PARTIAL_MATCH;
 }
 
-static ssize_t parse_dcs(const char *buf, size_t len, size_t i, KeyCode *k)
+static size_t parse_dcs(const char *buf, size_t len, size_t i, KeyCode *k)
 {
     char data[4096];
     for (size_t pos = 0; i < len; ) {
@@ -617,11 +617,15 @@ static ssize_t parse_dcs(const char *buf, size_t len, size_t i, KeyCode *k)
  *
  * See also: rxvt.c and linux.c
  */
-ssize_t term_parse_sequence(const char *buf, size_t length, KeyCode *k)
+size_t term_parse_sequence(const char *buf, size_t length, KeyCode *k)
 {
-    if (unlikely(length == 0 || buf[0] != '\033')) {
-        return 0;
-    } else if (unlikely(length == 1)) {
+    if (unlikely(length >= 1 && buf[0] != '\033')) {
+        // Unreachable, except via edge case tests in test_term_parse_sequence()
+        *k = KEY_IGNORE;
+        return 1;
+    }
+
+    if (unlikely(length <= 1)) {
         return TPARSE_PARTIAL_MATCH;
     }
 
@@ -630,9 +634,11 @@ ssize_t term_parse_sequence(const char *buf, size_t length, KeyCode *k)
     case 'P': return parse_dcs(buf, length, 2, k);
     case '[': return parse_csi(buf, length, 2, k);
     case ']': return parse_osc(buf, length, 2, k);
-    // String Terminator (https://vt100.net/emu/dec_ansi_parser#STESC)
-    case '\\': *k = KEY_IGNORE; return 2;
+    // String Terminator (no-op; see: https://vt100.net/emu/dec_ansi_parser#STESC)
+    case '\\': break;
     }
 
-    return 0;
+    // ESC followed by unhandled byte; consume and ignore
+    *k = KEY_IGNORE;
+    return 2;
 }

src/terminal/parse.h
@@ -2,8 +2,8 @@
 #define TERMINAL_PARSE_H
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
-#include <sys/types.h>
 #include "key.h"
 #include "util/macros.h"
 
@@ -28,10 +28,10 @@ enum {
     // Returned by term_parse_sequence() if buf contains a valid prefix
     // for a known sequence but isn't terminated within the length bound
     // (e.g. because another read(3) call is required to fill the buffer)
-    TPARSE_PARTIAL_MATCH = -1,
+    TPARSE_PARTIAL_MATCH = 0,
 };
 
-ssize_t term_parse_sequence(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT;
-size_t term_parse_csi_params(const char *buf, size_t len, size_t i, TermControlParams *csi) WARN_UNUSED_RESULT;
+size_t term_parse_sequence(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT NONNULL_ARGS WRITEONLY(3);
+size_t term_parse_csi_params(const char *buf, size_t len, size_t i, TermControlParams *csi) WARN_UNUSED_RESULT NONNULL_ARGS;
 
 #endif

src/terminal/rxvt.c
@@ -8,7 +8,7 @@
  the ECMA-48 spec (notably, the ones ending with '$', which isn't a
  valid final byte).
 */
-ssize_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
+size_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
 {
     KeyCode extra_mods = 0;
     size_t extra_bytes = 0;
@@ -49,7 +49,7 @@ ssize_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
     }
 
     KeyCode key;
-    ssize_t n;
+    size_t n;
     switch (buf[3]) {
     case '~': key = 0; break;
     case '^': key = MOD_CTRL; break;
@@ -73,7 +73,7 @@ ssize_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
 
 generic:
     n = term_parse_sequence(buf, length, &key);
-    if (n <= 0) {
+    if (n == TPARSE_PARTIAL_MATCH) {
         return n;
     }
     if (unlikely(key == KEY_IGNORE && extra_mods)) {

src/terminal/rxvt.h
@@ -1,10 +1,10 @@
 #ifndef TERMINAL_RXVT_H
 #define TERMINAL_RXVT_H
 
-#include <sys/types.h>
+#include <stddef.h>
 #include "key.h"
 #include "util/macros.h"
 
-ssize_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT;
+size_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT WRITEONLY(3);
 
 #endif

src/terminal/terminal.h
@@ -2,8 +2,8 @@
 #define TERMINAL_TERMINAL_H
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
-#include <sys/types.h>
 #include "feature.h"
 #include "key.h"
 #include "style.h"
@@ -64,7 +64,7 @@ typedef struct {
     unsigned int width; // Terminal width (in columns)
     unsigned int height; // Terminal height (in rows)
     unsigned int ncv_attributes; // See "no_color_video" terminfo(5) capability
-    ssize_t (*parse_input)(const char *buf, size_t length, KeyCode *key);
+    size_t (*parse_input)(const char *buf, size_t length, KeyCode *key);
     TermOutputBuffer obuf;
     TermInputBuffer ibuf;
 } Terminal;

commit 00445d8197886e603f7b813ff66a44124b0380d8

Fix input handling for Alt-modified key combos in legacy terminals
Where "legacy terminals" means those that don't support the Kitty
keyboard protocol and/or XTerm's `modifyOtherKeys` protocol (e.g.
the Linux console).

This manually reverts most of the following 2 commits:

* 6061d49d12505ff45fe1c54dc6fc166542f6cf86, which broke e.g. Alt+x
  (`ESC x`) and other Alt+letter combos
* ad229b37d68bb4c5d1381b441aa552b4812612c1, which broke Alt+Shift+o
  (`ESC O`, AKA `SS3`) and Alt+Shift+p (`ESC P`, AKA `DCS`)

The code added by commit f4b88e01f6e56735cb10626312b907e627b37d2e was
then also adjusted to handle `TPARSE_NO_MATCH` (which was previously
just `0`, prior to commit 6061d49d12505ff45fe1c54dc6fc166542f6cf86).

src/terminal/input.c
@@ -40,6 +40,7 @@ static void consume_input(TermInputBuffer *input, size_t len)
     input->len -= len;
     if (input->len) {
         memmove(input->buf, input->buf + len, input->len);
+        input->can_be_truncated = true; // Keys sent faster than we can read
     }
 }
 
@@ -47,6 +48,10 @@ static bool fill_buffer(TermInputBuffer *input)
 {
     if (unlikely(input->len == TERM_INBUF_SIZE)) {
         return false;
+    }
+
+    if (!input->len) {
+        input->can_be_truncated = false;
     }
 
     size_t avail = TERM_INBUF_SIZE - input->len;
@@ -90,8 +95,7 @@ static bool input_get_byte(TermInputBuffer *input, unsigned char *ch)
 static KeyCode read_special(TermInputBuffer *input, TermFeatureFlags features)
 {
     KeyCode key;
-    size_t len;
-
+    ssize_t len;
     if (unlikely(features & TFLAG_LINUX)) {
         len = linux_parse_key(input->buf, input->len, &key);
     } else if (unlikely(features & TFLAG_RXVT)) {
@@ -100,10 +104,17 @@ static KeyCode read_special(TermInputBuffer *input, TermFeatureFlags features)
         len = term_parse_sequence(input->buf, input->len, &key);
     }
 
-    if (unlikely(len == TPARSE_PARTIAL_MATCH)) {
-        return fill_buffer(input) ? read_special(input, features) : KEY_NONE;
+    switch (len) {
+    case TPARSE_PARTIAL_MATCH:
+        if (input->can_be_truncated && fill_buffer(input)) {
+            return read_special(input, features);
+        }
+        return KEY_NONE;
+    case TPARSE_NO_MATCH:
+        return KEY_NONE;
     }
 
+    BUG_ON(len < 1);
     consume_input(input, len);
     return key;
 }
@@ -204,7 +215,7 @@ static KeyCode term_read_input_legacy(Terminal *term, unsigned int esc_timeout_m
         return read_simple(term);
     }
 
-    if (input->len > 1) {
+    if (input->len > 1 || input->can_be_truncated) {
         KeyCode key = read_special(input, term->features);
         if (unlikely(key & KEYCODE_QUERY_REPLY_BIT)) {
             TermFeatureFlags features = key & ~KEYCODE_QUERY_REPLY_BIT;
@@ -295,8 +306,18 @@ static KeyCode term_read_input_modern(Terminal *term)
 
     KeyCode key;
     while (1) {
-        size_t len = term_parse_sequence(input->buf, input->len, &key);
+        ssize_t len = term_parse_sequence(input->buf, input->len, &key);
         if (len != TPARSE_PARTIAL_MATCH) {
+            if (unlikely(len == TPARSE_NO_MATCH)) {
+                // This return code only exists for term_read_input_legacy().
+                // When using the Kitty protocol, this branch shouldn't ever
+                // be taken (in practice), so we handle it as an edge case
+                // by simply consuming (and ignoring) the ESC byte and the
+                // byte that follows it.
+                BUG_ON(input->len < 2); // Implied by the conditions above
+                len = 2;
+                key = KEY_IGNORE;
+            }
             consume_input(input, len);
             break;
         }

src/terminal/linux.c
@@ -11,7 +11,7 @@
  $TERM is set to "linux", input is first passed through this function to
  handle this special case.
 */
-size_t linux_parse_key(const char *buf, size_t length, KeyCode *k)
+ssize_t linux_parse_key(const char *buf, size_t length, KeyCode *k)
 {
     if (length < 3 || !mem_equal(buf, "\033[[", 3)) {
         return term_parse_sequence(buf, length, k);

src/terminal/linux.h
@@ -1,10 +1,10 @@
 #ifndef TERMINAL_LINUX_H
 #define TERMINAL_LINUX_H
 
-#include <stddef.h>
+#include <sys/types.h>
 #include "key.h"
 #include "util/macros.h"
 
-size_t linux_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT WRITEONLY(3);
+ssize_t linux_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT WRITEONLY(3);
 
 #endif

src/terminal/parse.c
@@ -240,7 +240,7 @@ static KeyCode normalize_csi_27_tilde_keycode(KeyCode mods, KeyCode key)
     return mods | key;
 }
 
-static size_t parse_ss3(const char *buf, size_t length, size_t i, KeyCode *k)
+static ssize_t parse_ss3(const char *buf, size_t length, size_t i, KeyCode *k)
 {
     if (unlikely(i >= length)) {
         return TPARSE_PARTIAL_MATCH;
@@ -413,7 +413,7 @@ exit_loop:
     return i;
 }
 
-static size_t parse_csi(const char *buf, size_t len, size_t i, KeyCode *k)
+static ssize_t parse_csi(const char *buf, size_t len, size_t i, KeyCode *k)
 {
     TermControlParams csi = {.nparams = 0};
     bool maybe_query_reply = (i < len && buf[i] >= '<' && buf[i] <= '?');
@@ -548,7 +548,7 @@ ignore:
     return i;
 }
 
-static size_t parse_osc(const char *buf, size_t len, size_t i, KeyCode *k)
+static ssize_t parse_osc(const char *buf, size_t len, size_t i, KeyCode *k)
 {
     char data[4096];
     for (size_t pos = 0; i < len; ) {
@@ -578,7 +578,7 @@ static size_t parse_osc(const char *buf, size_t len, size_t i, KeyCode *k)
     return TPARSE_PARTIAL_MATCH;
 }
 
-static size_t parse_dcs(const char *buf, size_t len, size_t i, KeyCode *k)
+static ssize_t parse_dcs(const char *buf, size_t len, size_t i, KeyCode *k)
 {
     char data[4096];
     for (size_t pos = 0; i < len; ) {
@@ -617,12 +617,11 @@ static size_t parse_dcs(const char *buf, size_t len, size_t i, KeyCode *k)
  *
  * See also: rxvt.c and linux.c
  */
-size_t term_parse_sequence(const char *buf, size_t length, KeyCode *k)
+ssize_t term_parse_sequence(const char *buf, size_t length, KeyCode *k)
 {
     if (unlikely(length >= 1 && buf[0] != '\033')) {
         // Unreachable, except via edge case tests in test_term_parse_sequence()
-        *k = KEY_IGNORE;
-        return 1;
+        return TPARSE_NO_MATCH;
     }
 
     if (unlikely(length <= 1)) {
@@ -634,11 +633,11 @@ size_t term_parse_sequence(const char *buf, size_t length, KeyCode *k)
     case 'P': return parse_dcs(buf, length, 2, k);
     case '[': return parse_csi(buf, length, 2, k);
     case ']': return parse_osc(buf, length, 2, k);
-    // String Terminator (no-op; see: https://vt100.net/emu/dec_ansi_parser#STESC)
-    case '\\': break;
+    case '\\': // String Terminator
+        // No-op; see: https://vt100.net/emu/dec_ansi_parser#STESC
+        *k = KEY_IGNORE;
+        return 2;
     }
 
-    // ESC followed by unhandled byte; consume and ignore
-    *k = KEY_IGNORE;
-    return 2;
+    return TPARSE_NO_MATCH;
 }

src/terminal/parse.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <sys/types.h>
 #include "key.h"
 #include "util/macros.h"
 
@@ -28,10 +29,15 @@ enum {
     // Returned by term_parse_sequence() if buf contains a valid prefix
     // for a known sequence but isn't terminated within the length bound
     // (e.g. because another read(3) call is required to fill the buffer)
-    TPARSE_PARTIAL_MATCH = 0,
+    TPARSE_PARTIAL_MATCH = -1,
+
+    // Returned by term_parse_sequence() to mean "escape sequence not
+    // handled by this function", which makes term_read_input_legacy()
+    // then perform some additional, legacy-specific handling
+    TPARSE_NO_MATCH = 0,
 };
 
-size_t term_parse_sequence(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT NONNULL_ARGS WRITEONLY(3);
+ssize_t term_parse_sequence(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT NONNULL_ARGS WRITEONLY(3);
 size_t term_parse_csi_params(const char *buf, size_t len, size_t i, TermControlParams *csi) WARN_UNUSED_RESULT NONNULL_ARGS;
 
 #endif

src/terminal/rxvt.c
@@ -8,7 +8,7 @@
  the ECMA-48 spec (notably, the ones ending with '$', which isn't a
  valid final byte).
 */
-size_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
+ssize_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
 {
     KeyCode extra_mods = 0;
     size_t extra_bytes = 0;
@@ -49,7 +49,7 @@ size_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
     }
 
     KeyCode key;
-    size_t n;
+    ssize_t n;
     switch (buf[3]) {
     case '~': key = 0; break;
     case '^': key = MOD_CTRL; break;
@@ -73,7 +73,7 @@ size_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k)
 
 generic:
     n = term_parse_sequence(buf, length, &key);
-    if (n == TPARSE_PARTIAL_MATCH) {
+    if (n <= TPARSE_NO_MATCH) {
         return n;
     }
     if (unlikely(key == KEY_IGNORE && extra_mods)) {

src/terminal/rxvt.h
@@ -1,10 +1,10 @@
 #ifndef TERMINAL_RXVT_H
 #define TERMINAL_RXVT_H
 
-#include <stddef.h>
+#include <sys/types.h>
 #include "key.h"
 #include "util/macros.h"
 
-size_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT WRITEONLY(3);
+ssize_t rxvt_parse_key(const char *buf, size_t length, KeyCode *k) WARN_UNUSED_RESULT WRITEONLY(3);
 
 #endif

src/terminal/terminal.h
@@ -2,8 +2,8 @@
 #define TERMINAL_TERMINAL_H
 
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdint.h>
+#include <sys/types.h>
 #include "feature.h"
 #include "key.h"
 #include "style.h"
@@ -55,6 +55,7 @@ typedef struct {
 
 typedef struct {
     unsigned int len;
+    bool can_be_truncated;
     char buf[TERM_INBUF_SIZE];
 } TermInputBuffer;
 

test/terminal.c
@@ -57,22 +57,23 @@ static void expect_parse_seq (
     const char *file,
     int line,
     const char *seq,
-    size_t expected_length,
+    ssize_t expected_length,
     KeyCode expected_key
 ) {
     KeyCode key = 0x18;
-    size_t parsed_length = term_parse_sequence(seq, strlen(seq), &key);
+    ssize_t parsed_length = term_parse_sequence(seq, strlen(seq), &key);
     if (unlikely(parsed_length != expected_length)) {
         test_fail (
             ctx, file, line,
-            "term_parse_sequence() returned %zu; expected %zu",
+            "term_parse_sequence() returned %zd; expected %zd",
             parsed_length, expected_length
         );
         return;
     }
 
-    static_assert(TPARSE_PARTIAL_MATCH == 0);
-    if (expected_length == TPARSE_PARTIAL_MATCH) {
+    static_assert(TPARSE_PARTIAL_MATCH == -1);
+    static_assert(TPARSE_NO_MATCH == 0);
+    if (expected_length <= TPARSE_NO_MATCH) {
         test_pass(ctx);
         return;
     }
@@ -97,8 +98,8 @@ static void expect_parse_seq (
 
         test_fail (
             ctx, file, line,
-            "Parsing truncated sequence of %zu bytes returned %zu; expected %zu",
-            n, parsed_length, (size_t)TPARSE_PARTIAL_MATCH
+            "Parsing truncated sequence of %zu bytes returned %zd; expected %zd",
+            n, parsed_length, (ssize_t)TPARSE_PARTIAL_MATCH
         );
 
         return;
@@ -614,9 +615,9 @@ static void test_term_parse_csi_params(TestContext *ctx)
 
 static void test_term_parse_sequence(TestContext *ctx)
 {
-    EXPECT_PARSE_SEQ("", 0);
-    EXPECT_PARSE_SEQN("x", 1, KEY_IGNORE);
-    EXPECT_PARSE_SEQN("\033q", 2, KEY_IGNORE);
+    EXPECT_PARSE_SEQN("", TPARSE_PARTIAL_MATCH, 0);
+    EXPECT_PARSE_SEQN("x", TPARSE_NO_MATCH, 0);
+    EXPECT_PARSE_SEQN("\033q", TPARSE_NO_MATCH, 0);
     EXPECT_PARSE_SEQ("\033\\", KEY_IGNORE);
 
     EXPECT_PARSE_SEQ("\033[Z", MOD_SHIFT | KEY_TAB);
@@ -1034,7 +1035,7 @@ static void test_term_parse_sequence2(TestContext *ctx)
                 templates[i].final_byte
             );
             KeyCode key = 24;
-            size_t parsed_length = term_parse_sequence(seq, seq_length, &key);
+            ssize_t parsed_length = term_parse_sequence(seq, seq_length, &key);
             KeyCode mods = modifiers[j].mask;
             KeyCode expected_key = mods | (mods == KEY_IGNORE ? 0 : templates[i].key);
             IEXPECT_EQ(parsed_length, seq_length);
@@ -1092,7 +1093,7 @@ static void test_rxvt_parse_key(TestContext *ctx)
             KeyCode key;
 
             // Double ESC
-            size_t parsed_length = rxvt_parse_key(seq, seq_length, &key);
+            ssize_t parsed_length = rxvt_parse_key(seq, seq_length, &key);
             EXPECT_EQ(parsed_length, seq_length);
             EXPECT_EQ(key, modifiers[j].mask | templates[i].key | MOD_META);
 
@@ -1147,7 +1148,7 @@ static void test_rxvt_parse_key(TestContext *ctx)
         const char *seq = tests[i].seq;
         const size_t seq_length = tests[i].seq_length;
         KeyCode key = 0x18;
-        size_t parsed_length = rxvt_parse_key(seq, seq_length, &key);
+        ssize_t parsed_length = rxvt_parse_key(seq, seq_length, &key);
         KeyCode expected_key = (parsed_length > 0) ? tests[i].expected_key : 0x18;
         IEXPECT_EQ(parsed_length, tests[i].expected_length);
         IEXPECT_KEYCODE_EQ(key, expected_key, seq, seq_length);
@@ -1179,7 +1180,7 @@ static void test_linux_parse_key(TestContext *ctx)
         const char *seq = tests[i].seq;
         const size_t seq_length = tests[i].seq_length;
         KeyCode key = 0x18;
-        size_t parsed_length = linux_parse_key(seq, seq_length, &key);
+        ssize_t parsed_length = linux_parse_key(seq, seq_length, &key);
         KeyCode expected_key = (parsed_length > 0) ? tests[i].expected_key : 0x18;
         IEXPECT_EQ(parsed_length, tests[i].expected_length);
         IEXPECT_KEYCODE_EQ(key, expected_key, seq, seq_length);