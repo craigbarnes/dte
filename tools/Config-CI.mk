craigbarnes/dte

commit 8a36d692af0de18fc8dbf783b72f385946331366

Use GNU Make's `-Otarget` option for all CI test runners
See also:

* Commit 1859233b2cf363e9d3f682816240cb2d471acb78
* https://www.gnu.org/software/make/manual/make.html#index-_002d_002doutput_002dsync-1
* https://www.gnu.org/software/make/manual/make.html#Parallel-Output:~:text=target,-Output%20from

.gitlab-ci.yml
@@ -3,7 +3,7 @@ stages: [test, dist]
 default:
     interruptible: true
     before_script:
-        - export DEBUG=3 WERROR=1 V=1 TESTFLAGS=-ct
+        - ln -s tools/Config-CI.mk Config.mk
         - export CC="ccache ${CC:-gcc}"
         # Ensure upstream "v*" tags are present, regardless of whether
         # this job is running for a fork or the upstream repo

tools/Config-CI.mk
@@ -1,0 +1,14 @@
+# These variables are used by all CI test runners.
+# See also: `mk/prelude.mk` and `docs/packaging.md`
+
+# Group build output by target, so that it doesn't get get interleaved
+# (by parallel builds) in ways that make it hard to read.
+# See also:
+# • https://www.gnu.org/software/make/manual/make.html#index-_002d_002doutput_002dsync-1
+# • https://www.gnu.org/software/make/manual/make.html#Parallel-Output:~:text=target,-Output%20from
+MAKEFLAGS += -Otarget
+
+DEBUG = 3 # Full debug assertions
+WERROR = 1 # All compiler warnings are fatal
+V = 1 # Verbose build output (print executed commands, instead of short messages)
+TESTFLAGS = -ct # Enable colors and timing info in test runner output

.github/workflows/test.yml
@@ -1,12 +1,6 @@
 name: Test
 on: [push]
 
-env:
-    V: 1
-    DEBUG: 3
-    WERROR: 1
-    TESTFLAGS: -ct
-
 jobs:
     macos:
         strategy:
@@ -16,6 +10,7 @@ jobs:
         steps:
             - uses: actions/checkout@v4
               with: {filter: "tree:0", fetch-depth: 0}
+            - run: ln -s tools/Config-CI.mk Config.mk
             - run: brew install make
             - run: gmake vvars
             - run: gmake build/gen/feature.h
@@ -31,6 +26,7 @@ jobs:
         steps:
             - uses: actions/checkout@v4
               with: {filter: "tree:0", fetch-depth: 0}
+            - run: ln -s tools/Config-CI.mk Config.mk
             - run: make vvars
             - run: make build/gen/feature.h
             - run: cat build/gen/feature.h build/feature/*.log

commit 3e88392d362e5c5908f478c61da811cdd15584e4

Set `V=1` for all `make` commands in GitLab CI `pages` job
As was done previously, before it was regressed by commit
8a36d692af0de18fc8dbf783b72f385946331366 (for the reason
mentioned in the updated comment).

.gitlab-ci.yml
@@ -127,9 +127,10 @@ android:
         expire_in: 60 days
         name: dte-$CI_JOB_NAME-$CI_COMMIT_SHORT_SHA
     script:
+        - export V=1
         - make docs htmlgz-aux
         - make gcovr-html
-        - make portable
+        - make portable WERROR=1
         - ls -l public/dte-*.tar.gz
         - tree public/
 

tools/Config-CI.mk
@@ -1,5 +1,10 @@
-# These variables are used by all CI test runners.
-# See also: `mk/prelude.mk` and `docs/packaging.md`
+# These variables are used by all CI test runners, except when running
+# Makefile targets that explicitly set `NO_CONFIG_MK=1`.
+# See also:
+# • mk/prelude.mk
+# • docs/packaging.md
+# • tools/mk/coverage.mk (`make gcovr-*`)
+# • tools/mk/portable.mk (`make portable*`)
 
 # Group build output by target, so that it doesn't get get interleaved
 # (by parallel builds) in ways that make it hard to read.